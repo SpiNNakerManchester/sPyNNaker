817fbf0cdd71e87a5ef10a3b5dcc1f2b70e2c769
Petrut Antoniu Bogdan <petrutantoniu8@gmail.com>
2014-07-30 16:35:41 +0100

    Fixed probability now stops including a connection to the same neuron in the same population if allow_self_connection = False

--- spynnaker/pyNN/models/neural_projections/connectors/fixed_probability.py
+++ spynnaker/pyNN/models/neural_projections/connectors/fixed_probability.py
@@ -44,16 +44,20 @@ class FixedProbabilityConnector(AbstractConnector):
 
         if not 0 <= self._p_connect <= 1:
             raise ConfigurationException("The probability should be between 0"
-                                         " and 1")
+                                         " and 1 (inclusive)")
         
     def generate_synapse_list(self, prevertex, postvertex, delay_scale,
                               synapse_type):
         rows = list()
-        for _ in range(0, prevertex.n_atoms):
+        for pre_atom in range(0, prevertex.n_atoms):
             present = numpy.random.rand(postvertex.n_atoms) <= self._p_connect
+            if not self._allow_self_connections and prevertex == postvertex:
+                present[pre_atom] = 0
+
             n_present = numpy.sum(present)
-            
+
             ids = numpy.where(present)[0]
+
             delays = (generate_parameter_array(self._delays, n_present, present)
                       * delay_scale)
             weights = generate_parameter_array(self._weights, n_present,
