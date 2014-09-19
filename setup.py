589dbe04bbc9a7ec036bffe4f8795d50be7e6b75
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2014-09-19 08:02:37 +0100

    Added setup files

---
+++ setup.py
@@ -0,0 +1,30 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name="sPyNNaker",
+    version="0.1-SNAPSHOT",
+    description="Spinnaker implementation of PyNN",
+    url="https://github.com/SpiNNakerManchester/SpyNNaker",
+    packages=['spynnaker',
+              'spynnaker.pyNN',
+              'spynnaker.pyNN.data_storage',
+              'spynnaker.pyNN.models',
+              'spynnaker.pyNN.models.abstract_models',
+              'spynnaker.pyNN.models.neural_models',
+              'spynnaker.pyNN.models.neural_projections',
+              'spynnaker.pyNN.models.neural_projections.connectors',
+              'spynnaker.pyNN.models.neural_properties',
+              'spynnaker.pyNN.models.neural_properties.synapse_dynamics',
+              'spynnaker.pyNN.models.spike_source',
+              'spynnaker.pyNN.models.utility_models',
+              'spynnaker.pyNN.overridden_pacman_functions',
+              'spynnaker.pyNN.utilities',
+              'spynnaker.pyNN.utilities.conf',
+              'spynnaker.pyNN.visualiser_package',
+              'spynnaker.pyNN.visualiser_package.visualiser_pages'],
+    install_requires=['SpiNNMachine', 'SpiNNMan', 'PACMAN',
+            'DataSpecification', 'pyNN']
+)
