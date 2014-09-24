5cccd816a9f5aecd35b90f281520803fafca7914
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2014-09-24 16:07:16 +0100

    Fixed the parameter to be n rather than n_pre to match PyNN spec

--- spynnaker/pyNN/models/neural_projections/connectors/fixed_number_pre_connector.py
+++ spynnaker/pyNN/models/neural_projections/connectors/fixed_number_pre_connector.py
@@ -12,38 +12,38 @@ from spynnaker.pyNN.exceptions import ConfigurationException
 
 class FixedNumberPreConnector(AbstractConnector):
     """
-    Connects a fixed number of pre-synaptic neurons selected at randoom, 
+    Connects a fixed number of pre-synaptic neurons selected at randoom,
     to all post-synaptic neurons
 
-    :param `int` n_pre:
+    :param `int` n:
         number of random pre-synaptic neurons connected to output
     :param `bool` allow_self_connections:
         if the connector is used to connect a
         Population to itself, this flag determines whether a neuron is
         allowed to connect to itself, or only to other neurons in the
         Population.
-    :param weights: 
+    :param weights:
         may either be a float, a !RandomDistribution object, a list/
         1D array with at least as many items as connections to be
         created. Units nA.
-    :param delays: 
+    :param delays:
         If `None`, all synaptic delays will be set
         to the global minimum delay.
-    :param `pyNN.Space` space: 
+    :param `pyNN.Space` space:
         a Space object, needed if you wish to specify distance-
         dependent weights or delays - not implemented
     """
-    def __init__(self, n_pre, weights=0.0, delays=1,
+    def __init__(self, n, weights=0.0, delays=1,
                  allow_self_connections=True):
         """
         Creates a new FixedNumberPreConnector
         """
-        self._n_pre = int(n_pre)
+        self._n_pre = int(n)
         self._weights = float(weights)
         self._delays = int(delays)
         self._allow_self_connections = allow_self_connections
-        
-    def generate_synapse_list(self, prevertex, postvertex, delay_scale, 
+
+    def generate_synapse_list(self, prevertex, postvertex, delay_scale,
                               synapse_type):
         id_lists = list()
         weight_lists = list()
@@ -59,9 +59,9 @@ class FixedNumberPreConnector(AbstractConnector):
             raise ConfigurationException("Sample size has to be a number less "
                                          "than the size of the population but"
                                          "greater than zero")
-        pre_synaptic_neurons = random.sample(range(0, prevertex.n_atoms), 
+        pre_synaptic_neurons = random.sample(range(0, prevertex.n_atoms),
                                              self._n_pre)
- 
+
         for pre_atom in pre_synaptic_neurons:
 
             present = numpy.ones(postvertex.n_atoms, dtype=numpy.uint32)
@@ -70,7 +70,7 @@ class FixedNumberPreConnector(AbstractConnector):
                 n_present = postvertex.n_atoms - 1
             else:
                 n_present = postvertex.n_atoms
-            
+
             id_lists[pre_atom] = numpy.where(present)[0]
             weight_lists[pre_atom] = \
                 generate_parameter_array(self._weights, n_present, present)
@@ -82,9 +82,9 @@ class FixedNumberPreConnector(AbstractConnector):
             type_lists[pre_atom]=\
                 generate_parameter_array(synapse_type, n_present, present)
 
-        
-        connection_list = [SynapseRowInfo(id_lists[i], weight_lists[i], 
+
+        connection_list = [SynapseRowInfo(id_lists[i], weight_lists[i],
                                           delay_lists[i], type_lists[i])
                            for i in range(0, prevertex.n_atoms)]
-        
+
         return SynapticList(connection_list)
