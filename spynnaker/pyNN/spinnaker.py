c46445a096b34b798485131c32b39ee1c4be8e92
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2014-12-03 15:32:45 +0000

    Fixes to make get_weights work

--- spynnaker/pyNN/spinnaker.py
+++ spynnaker/pyNN/spinnaker.py
@@ -702,9 +702,9 @@ class Spinnaker(SpynnakerConfiguration, SpynnakerCommsFunctions):
                                                       router_table.y)
                     self._txrx.clear_router_diagnostic_counters(router_table.x,
                                                                 router_table.y)
-                    self._txrx.\
-                        clear_router_diagnostic_non_default_positioned_filters(
-                            router_table.x, router_table.y)
+                    #self._txrx.\
+                    #    clear_router_diagnostic_non_default_positioned_filters(
+                    #        router_table.x, router_table.y)
             self._txrx.send_signal(app_id, SCPSignal.STOP)
         if conf.config.getboolean("Visualiser", "enable"):
             self._visualiser.stop()
