85781d80ba4d82958f6be5a099c2b5deb741b003
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2015-07-20 11:57:37 +0100

    Added some safety checks, which shouldn't affect the runtime too much.

--- neural_modelling/src/neuron/plasticity/synapse_dynamics_static_impl.c
+++ neural_modelling/src/neuron/plasticity/synapse_dynamics_static_impl.c
@@ -18,7 +18,7 @@ void synapse_dynamics_process_post_synaptic_event(
 }
 
 //---------------------------------------
-void synapse_dynamics_process_plastic_synapses(address_t plastic_region_address,
+bool synapse_dynamics_process_plastic_synapses(address_t plastic_region_address,
         address_t fixed_region_address, weight_t *ring_buffer, uint32_t time) {
     use(plastic_region_address);
     use(fixed_region_address);
@@ -26,6 +26,7 @@ void synapse_dynamics_process_plastic_synapses(address_t plastic_region_address,
     use(time);
 
     log_error("There should be no plastic synapses!");
+    return false;
 }
 
 //---------------------------------------
--- neural_modelling/src/neuron/spike_processing.c
+++ neural_modelling/src/neuron/spike_processing.c
@@ -25,8 +25,11 @@ typedef struct dma_buffer {
   // (used to allow row data to be re-used for multiple spikes)
   spike_t originating_spike;
   
+  uint32_t n_bytes_transferred;
+  
   // Row data
   uint32_t *row;
+  
 } dma_buffer;
 
 extern uint32_t time;
@@ -42,6 +45,8 @@ static uint32_t next_buffer_to_fill;
 
 // The index of the buffer currently being filled by a DMA read
 static uint32_t buffer_being_read;
+
+static uint32_t max_n_words;
 
 /* PRIVATE FUNCTIONS - static for inlining */
 
@@ -61,13 +66,14 @@ static inline void _setup_synaptic_dma_read() {
                 &n_bytes_to_transfer)) {
 
             // **HACK** doesn't copy enough data for plastic rows so add some words!
-            n_bytes_to_transfer += (5 * sizeof(uint32_t));
+            // n_bytes_to_transfer += (5 * sizeof(uint32_t));
             
             // Write the SDRAM address of the plastic region and the
             // Key of the originating spike to the beginning of dma buffer
             dma_buffer *next_buffer = &dma_buffers[next_buffer_to_fill];
-            next_buffer->sdram_writeback_address = row_address + 1;
+            next_buffer->sdram_writeback_address = row_address;
             next_buffer->originating_spike = spike;
+			next_buffer->n_bytes_transferred = n_bytes_to_transfer;
 
             // Start a DMA transfer to fetch this synaptic row into current
             // buffer
@@ -99,11 +105,11 @@ static inline void _setup_synaptic_dma_write(uint32_t dma_buffer_index) {
     size_t n_plastic_region_bytes = synapse_row_plastic_size(buffer->row) * sizeof(uint32_t);
     
     log_debug("Writing back %u bytes of plastic region to %08x",
-              n_plastic_region_bytes, buffer->sdram_writeback_address);
+              n_plastic_region_bytes, buffer->sdram_writeback_address + 1);
 
     // Start transfer
     spin1_dma_transfer(
-        DMA_TAG_WRITE_PLASTIC_REGION, buffer->sdram_writeback_address,
+        DMA_TAG_WRITE_PLASTIC_REGION, buffer->sdram_writeback_address + 1,
         synapse_row_plastic_region(buffer->row),
         DMA_WRITE, n_plastic_region_bytes);
 }
@@ -167,9 +173,25 @@ void _dma_complete_callback(uint unused, uint tag) {
 
             // Process synaptic row, writing it back if it's the last time
             // it's going to be processed
-            synapses_process_synaptic_row(time, current_buffer->row,
+            if (!synapses_process_synaptic_row(time, current_buffer->row,
                                           !subsequent_spikes,
-                                          current_buffer_index);
+                                          current_buffer_index)) {
+                log_error("Error processing spike 0x%.8x for address 0x%.8x (local=0x%.8x)",
+                          current_buffer->originating_spike, 
+                          current_buffer->sdram_writeback_address,
+						  current_buffer->row);
+				for (uint32_t i = 0; i < (current_buffer->n_bytes_transferred >> 2); i++) {
+				    log_error("%u: 0x%.8x", i, current_buffer->row[i]);
+				}
+                vic[VIC_DISABLE] = 
+				     (1 << CC_RDY_INT)   |
+                     (1 << TIMER1_INT)   |
+                     (1 << SOFTWARE_INT) |
+                     (1 << DMA_ERR_INT)  |
+                     (1 << DMA_DONE_INT);
+			    sark.vcpu->rt_code = RTE_SWERR;
+				sark_cpu_state(CPU_STATE_RTE);
+            }
         } while (subsequent_spikes);
 
     } else if (tag == DMA_TAG_WRITE_PLASTIC_REGION) {
@@ -200,6 +222,7 @@ bool spike_processing_initialise(size_t row_max_n_words) {
     dma_busy = false;
     next_buffer_to_fill = 0;
     buffer_being_read = N_DMA_BUFFERS;
+	max_n_words = row_max_n_words;
 
     // Allocate incoming spike buffer
     if (!in_spikes_initialize_spike_buffer(N_INCOMING_SPIKES)) {
