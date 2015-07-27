caffa8e5d97178e49fdfdd19cb50de9892411637
alan-stokes <alan.barry.stokes@gmail.com>
2015-07-27 15:12:38 +0100

    fixes from jamies review

--- spynnaker/pyNN/models/pynn_population.py
+++ spynnaker/pyNN/models/pynn_population.py
@@ -178,7 +178,7 @@ class Population(object):
 
             if not self._spinnaker.has_ran:
                 raise local_exceptions.SpynnakerException(
-                    "The simulation has not yet ran, therefore spikes cannot"
+                    "The simulation has not yet run, therefore spikes cannot"
                     " be retrieved. Please execute the simulation before"
                     " running this command")
             if conf.config.getboolean("Reports", "outputTimesForSections"):
@@ -215,7 +215,7 @@ class Population(object):
 
             if not self._spinnaker.has_ran:
                 raise local_exceptions.SpynnakerException(
-                    "The simulation has not yet ran, therefore gsyn cannot"
+                    "The simulation has not yet run, therefore gsyn cannot"
                     " be retrieved. Please execute the simulation before"
                     " running this command")
             timer = None
@@ -255,7 +255,7 @@ class Population(object):
 
             if not self._spinnaker.has_ran:
                 raise local_exceptions.SpynnakerException(
-                    "The simulation has not yet ran, therefore v cannot"
+                    "The simulation has not yet run, therefore v cannot"
                     " be retrieved. Please execute the simulation before"
                     " running this command")
 
