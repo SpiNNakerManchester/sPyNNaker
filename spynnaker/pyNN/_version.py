c64d27794e76e263ce374b713a195d90d167eeed
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2015-08-25 15:49:45 +0100

    Updating versions

--- spynnaker/pyNN/_version.py
+++ spynnaker/pyNN/_version.py
@@ -1,4 +1,4 @@
-__version__ = "2015.004"
-__version_month__ = "April"
+__version__ = "2015.005-rc-01"
+__version_month__ = "August"
 __version_year__ = "2015"
-__version_name__ = "Little-Rascal"
+__version_name__ = "Arbitrary"
