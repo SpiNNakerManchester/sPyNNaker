46e995c24b01c30fc71b8ad0f21a2a17d20a746b
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2016-03-17 16:59:47 +0000

    Further updates for when nothing has run

--- spynnaker/pyNN/spinnaker.py
+++ spynnaker/pyNN/spinnaker.py
@@ -1260,39 +1260,43 @@ class Spinnaker(object):
             if clear_tags is None:
                 clear_tags = config.getboolean("Machine", "clear_tags")
 
-            self._txrx.enable_reinjection(multicast=False)
+            if self._txrx is not None:
 
-            # if stopping on machine, clear iptags and
-            if clear_tags:
-                for ip_tag in self._tags.ip_tags:
-                    self._txrx.clear_ip_tag(
-                        ip_tag.tag, board_address=ip_tag.board_address)
-                for reverse_ip_tag in self._tags.reverse_ip_tags:
-                    self._txrx.clear_ip_tag(
-                        reverse_ip_tag.tag,
-                        board_address=reverse_ip_tag.board_address)
+                self._txrx.enable_reinjection(multicast=False)
 
-            # if clearing routing table entries, clear
-            if clear_routing_tables:
-                for router_table in self._router_tables.routing_tables:
-                    if not self._machine.get_chip_at(router_table.x,
-                                                     router_table.y).virtual:
-                        self._txrx.clear_multicast_routes(router_table.x,
-                                                          router_table.y)
+                # if stopping on machine, clear iptags and
+                if clear_tags:
+                    for ip_tag in self._tags.ip_tags:
+                        self._txrx.clear_ip_tag(
+                            ip_tag.tag, board_address=ip_tag.board_address)
+                    for reverse_ip_tag in self._tags.reverse_ip_tags:
+                        self._txrx.clear_ip_tag(
+                            reverse_ip_tag.tag,
+                            board_address=reverse_ip_tag.board_address)
 
-            # clear values
-            self._no_sync_changes = 0
+                # if clearing routing table entries, clear
+                if clear_routing_tables:
+                    for router_table in self._router_tables.routing_tables:
+                        if not self._machine.get_chip_at(
+                                router_table.x, router_table.y).virtual:
+                            self._txrx.clear_multicast_routes(
+                                router_table.x, router_table.y)
 
-            # app stop command
-            self._txrx.stop_application(self._app_id)
+                # clear values
+                self._no_sync_changes = 0
+
+                # app stop command
+                self._txrx.stop_application(self._app_id)
 
             if self._buffer_manager is not None:
                 self._buffer_manager.stop()
 
             # stop the transceiver
-            if turn_off_machine:
-                logger.info("Turning off machine")
-            self._txrx.close(power_off_machine=turn_off_machine)
+            if self._txrx is not None:
+                if turn_off_machine:
+                    logger.info("Turning off machine")
+
+                self._txrx.close(power_off_machine=turn_off_machine)
 
             if self._machine_allocation_controller is not None:
                 self._machine_allocation_controller.close()
