0d23e4b6eba14fb49584f86c15ad939612341317
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2016-04-01 13:02:23 +0100

    Fix to use atom id in place of local neuron index

--- spynnaker/pyNN/models/spike_source/spike_source_poisson.py
+++ spynnaker/pyNN/models/spike_source/spike_source_poisson.py
@@ -247,7 +247,7 @@ class SpikeSourcePoisson(
             spec, ip_tags, [spike_history_region_sz],
             buffer_size_before_receive, self._time_between_requests)
 
-    def _write_poisson_parameters(self, spec, key, num_neurons):
+    def _write_poisson_parameters(self, spec, key, vertex_slice):
         """ Generate Neuron Parameter data for Poisson spike sources
 
         :param spec:
@@ -256,7 +256,7 @@ class SpikeSourcePoisson(
         :return:
         """
         spec.comment("\nWriting Neuron Parameters for {} poisson sources:\n"
-                     .format(num_neurons))
+                     .format(vertex_slice.n_atoms))
 
         # Set the focus to the memory region 2 (neuron parameters):
         spec.switch_write_focus(
@@ -290,14 +290,17 @@ class SpikeSourcePoisson(
         # or fast source
         slow_sources = list()
         fast_sources = list()
-        for i in range(0, num_neurons):
+        for i in range(vertex_slice.n_atoms):
+
+            atom_id = vertex_slice.lo_atom + i
 
             # Get the parameter values for source i:
-            rate_val = generate_parameter(self._rate, i)
-            start_val = generate_parameter(self._start, i)
+            rate_val = generate_parameter(self._rate, atom_id)
+            start_val = generate_parameter(self._start, atom_id)
             end_val = None
             if self._duration is not None:
-                end_val = generate_parameter(self._duration, i) + start_val
+                end_val = generate_parameter(
+                    self._duration, atom_id) + start_val
 
             # Decide if it is a fast or slow source and
             spikes_per_tick = \
@@ -457,7 +460,7 @@ class SpikeSourcePoisson(
                 routing_info.get_keys_and_masks_from_partition(partition)
             key = keys_and_masks[0].key
 
-        self._write_poisson_parameters(spec, key, vertex_slice.n_atoms)
+        self._write_poisson_parameters(spec, key, vertex_slice)
 
         # End-of-Spec:
         spec.end_specification()
