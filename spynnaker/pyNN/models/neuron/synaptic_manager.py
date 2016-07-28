c5800ce0439c0d30f623c64cfc0bf46be28260b9
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2016-07-28 19:23:13 +0100

    Allowed setting of a maximum size for the one-to-one data usage

--- spynnaker/pyNN/models/neuron/synaptic_manager.py
+++ spynnaker/pyNN/models/neuron/synaptic_manager.py
@@ -88,6 +88,10 @@ class SynapticManager(object):
         # the edge the connection is for
         self._pre_run_connection_holders = defaultdict(list)
 
+        # Limit the DTCM used by one-to-one connections
+        self._one_to_one_connection_dtcm_max_bytes = conf.config.getint(
+            "Simulation", "one_to_one_connection_dtcm_max_bytes")
+
     @property
     def synapse_dynamics(self):
         return self._synapse_dynamics
@@ -659,7 +663,9 @@ class SynapticManager(object):
                             get_routing_info_for_edge(machine_edge)
 
                         if (row_length == 1 and isinstance(
-                                synapse_info.connector, OneToOneConnector)):
+                                synapse_info.connector, OneToOneConnector) and
+                                (next_single_start_position * 4) <
+                                 self._one_to_one_connection_dtcm_max_bytes):
                             single_rows = row_data.reshape(-1, 4)[:, 3]
                             single_synapses.append(single_rows)
                             self._population_table_type\
@@ -694,7 +700,9 @@ class SynapticManager(object):
                              pre_vertex_slice.hi_atom)]
 
                         if (delayed_row_length == 1 and isinstance(
-                                synapse_info.connector, OneToOneConnector)):
+                                synapse_info.connector, OneToOneConnector) and
+                                (next_single_start_position * 4) <
+                                 self._one_to_one_connection_dtcm_max_bytes):
                             single_rows = delayed_row_data.reshape(-1, 4)[:, 3]
                             single_synapses.append(single_rows)
                             self._population_table_type\
