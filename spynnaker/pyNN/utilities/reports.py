ac5042c8ebc8d7c9792d54c9ec707efc3cf6fc4d
alan-stokes <alan.barry.stokes@gmail.com>
2016-08-11 09:52:33 +0100

    fixes to get synaptic matrix to operate

--- spynnaker/pyNN/utilities/reports.py
+++ spynnaker/pyNN/utilities/reports.py
@@ -2,14 +2,14 @@ import logging
 import os
 from spinn_machine.utilities.progress_bar import ProgressBar
 
-from spynnaker.pyNN import exceptions, ProjectionPartitionableEdge
+from spynnaker.pyNN import exceptions, ProjectionApplicationEdge
 from spynnaker.pyNN.models.neuron.connection_holder import ConnectionHolder
 
 logger = logging.getLogger(__name__)
 
 
 def generate_synaptic_matrix_reports(
-        common_report_directory, partitionable_graph, partitioned_graph,
+        common_report_directory, application_graph, machine_graph,
         placements, txrx, routing_infos, graph_mapper,
         loaded_application_data_token):
     """converts synaptic matrix for every partitionable edge.
@@ -18,9 +18,9 @@ def generate_synaptic_matrix_reports(
     :param routing_infos: routing infos
     :param placements: placements
     :param txrx: the spinnman instance
-    :param partitionable_graph: the application graph
+    :param application_graph: the application graph
     :param common_report_directory: the location for these reports
-    :param partitioned_graph: the machine graph
+    :param machine_graph: the machine graph
     :param graph_mapper: the mapping between app and machine graphs.
     :return: None
     """
@@ -32,12 +32,12 @@ def generate_synaptic_matrix_reports(
                                     "synaptic_matrix_reports")
     if not os.path.exists(top_level_folder):
         os.mkdir(top_level_folder)
-    for partitionable_edge in partitionable_graph.edges:
+    for application_edge in application_graph.edges:
 
-        if isinstance(partitionable_edge, ProjectionPartitionableEdge):
+        if isinstance(application_edge, ProjectionApplicationEdge):
             file_name = os.path.join(
                 top_level_folder, "synaptic_matrix_for_partitionable_edge_{}"
-                                  .format(partitionable_edge))
+                                  .format(application_edge))
             output = None
             try:
                 output = open(file_name, "w")
@@ -46,28 +46,25 @@ def generate_synaptic_matrix_reports(
                              " {} for writing.".format(file_name))
 
             connection_holder = ConnectionHolder(
-                None, True, partitionable_edge.pre_vertex.n_atoms,
-                partitionable_edge.post_vertex.n_atoms)
+                None, True, application_edge.pre_vertex.n_atoms,
+                application_edge.post_vertex.n_atoms)
 
-            subedges = graph_mapper.\
-                get_partitioned_edges_from_partitionable_edge(
-                    partitionable_edge)
+            machine_edges = graph_mapper.get_machine_edges(application_edge)
 
             progress = ProgressBar(
-                len(subedges),
+                len(machine_edges),
                 "Getting synaptic matrix for projection between {} and {}"
                 .format(
-                    partitionable_edge.pre_vertex.label,
-                    partitionable_edge.post_vertex.label))
+                    application_edge.pre_vertex.label,
+                    application_edge.post_vertex.label))
 
-            for subedge in subedges:
-                placement = placements.get_placement_of_subvertex(
-                    subedge.post_subvertex)
-                connections = partitionable_edge.post_vertex.\
+            for machine_edge in machine_edges:
+                placement = placements.get_placement_of_vertex(
+                    machine_edge.post_vertex)
+                connections = application_edge.post_vertex.\
                     get_connections_from_machine(
-                        txrx, placement, subedge, graph_mapper, routing_infos,
-                        partitionable_edge.synapse_information[0],
-                    partitioned_graph)
+                        txrx, placement, machine_edge, graph_mapper,
+                        routing_infos, application_edge.synapse_information[0])
                 if connections is not None:
                     connection_holder.add_connections(connections)
                 progress.update()
