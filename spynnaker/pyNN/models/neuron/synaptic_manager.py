ca93907ab0d8e7a744c4cffd006c6d93f77b8681
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2016-08-24 21:13:18 +0100

    Updated to do single connections correctly

--- spynnaker/pyNN/models/neuron/synaptic_manager.py
+++ spynnaker/pyNN/models/neuron/synaptic_manager.py
@@ -659,7 +659,7 @@ class SynapticManager(object):
                                     spec, next_single_start_position, 1,
                                     keys_and_masks, master_pop_table_region,
                                     is_single=True)
-                            next_single_start_position += len(single_rows)
+                            next_single_start_position += len(single_rows) * 4
                         else:
                             next_block_start_address = self._write_padding(
                                 spec, synaptic_matrix_region,
@@ -693,7 +693,7 @@ class SynapticManager(object):
                                     spec, next_single_start_position, 1,
                                     keys_and_masks, master_pop_table_region,
                                     is_single=True)
-                            next_single_start_position += len(single_rows)
+                            next_single_start_position += len(single_rows) * 4
                         else:
                             next_block_start_address = self._write_padding(
                                 spec, synaptic_matrix_region,
