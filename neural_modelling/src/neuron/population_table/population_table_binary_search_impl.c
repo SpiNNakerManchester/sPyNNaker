cb214a492f7b799c5f884663cb9cf8e3e4ca3374
alan-stokes <alan.barry.stokes@gmail.com>
2016-11-11 14:15:17 +0000

    fixed issue with ports for dynamic ports

--- neural_modelling/src/neuron/population_table/population_table_binary_search_impl.c
+++ neural_modelling/src/neuron/population_table/population_table_binary_search_impl.c
@@ -52,25 +52,25 @@ static inline uint32_t _get_neuron_id(
 }
 
 static inline void _print_master_population_table() {
-    log_info("master_population\n");
-    log_info("------------------------------------------\n");
+    log_debug("master_population\n");
+    log_debug("------------------------------------------\n");
     for (uint32_t i = 0; i < master_population_table_length; i++) {
         master_population_table_entry entry = master_population_table[i];
         for (uint16_t j = entry.start; j < (entry.start + entry.count); j++) {
-            log_info(
+            log_debug(
                 "index (%d, %d), key: 0x%.8x, mask: 0x%.8x, address: 0x%.8x,"
                 " row_length: %u\n", i, j, entry.key, entry.mask,
                 _get_address(address_list[j]),
                 _get_row_length(address_list[j]));
         }
     }
-    log_info("------------------------------------------\n");
+    log_debug("------------------------------------------\n");
 }
 
 bool population_table_initialise(
         address_t table_address, address_t synapse_rows_address,
         address_t direct_rows_address, uint32_t *row_max_n_words) {
-    log_info("population_table_initialise: starting");
+    log_debug("population_table_initialise: starting");
 
     master_population_table_length = table_address[0];
     log_info("master pop table length is %d\n", master_population_table_length);
@@ -106,10 +106,10 @@ bool population_table_initialise(
         }
     }
 
-    log_info(
+    log_debug(
         "pop table size: %u (%u bytes)", master_population_table_length,
         n_master_pop_bytes);
-    log_info(
+    log_debug(
         "address list size: %u (%u bytes)", address_list_length,
         n_address_list_bytes);
 
@@ -120,10 +120,10 @@ bool population_table_initialise(
         n_address_list_bytes);
 
     // Store the base address
-    log_info(
+    log_debug(
         "the stored synaptic matrix base address is located at: 0x%08x",
         synapse_rows_address);
-    log_info(
+    log_debug(
         "the direct synaptic matrix base address is located at: 0x%08x",
         direct_rows_address);
     synaptic_rows_base_address = synapse_rows_address;
