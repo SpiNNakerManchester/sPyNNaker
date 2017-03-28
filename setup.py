e8f73b0a68f113413251fc85973cdf6bc1363e20
Christian <brenninc@cs.man.ac.uk>
2017-03-28 14:38:56 +0100

    added utils, auto packages and other minor cleanups

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 import os
 from setuptools import setup
+from collections import defaultdict
+
 exec(open("spynnaker/pyNN/_version.py").read())
 
 if os.environ.get('READTHEDOCS', None) == 'True':
@@ -10,45 +12,32 @@ else:
     install_requires = ['SpiNNFrontEndCommon >= 3.0.0, < 4.0.0',
                         'pyNN >= 0.7, < 0.8', 'numpy', 'scipy', 'lxml', 'six']
 
+# Build a list of all project modules, as well as supplementary files
+main_package = "spynnaker"
+data_extensions = {".aplx", ".xml"}
+main_package_dir = os.path.join(os.path.dirname(__file__), main_package)
+start = len(main_package_dir)
+packages = []
+package_data = defaultdict(list)
+for dirname, dirnames, filenames in os.walk(main_package_dir):
+    if '__init__.py' in filenames:
+        package = "{}{}".format(
+            main_package, dirname[start:].replace(os.sep, '.'))
+        packages.append(package)
+    for filename in filenames:
+        _, ext = os.path.splitext(filename)
+        if ext in data_extensions:
+            package = "{}{}".format(
+                main_package, dirname[start:].replace(os.sep, '.'))
+            package_data[package].append("*.{}".format(ext))
+            break
+
 setup(
     name="sPyNNaker",
     version=__version__,
     description="Spinnaker implementation of PyNN",
     url="https://github.com/SpiNNakerManchester/SpyNNaker",
-    packages=['spynnaker',
-              'spynnaker.pyNN',
-              'spynnaker.pyNN.model_binaries',
-              'spynnaker.pyNN.models',
-              'spynnaker.pyNN.models.abstract_models',
-              'spynnaker.pyNN.models.common',
-              'spynnaker.pyNN.models.neural_projections',
-              'spynnaker.pyNN.models.neural_projections.connectors',
-              'spynnaker.pyNN.models.neural_properties',
-              'spynnaker.pyNN.models.neuron',
-              'spynnaker.pyNN.models.neuron.additional_inputs',
-              'spynnaker.pyNN.models.neuron.builds',
-              'spynnaker.pyNN.models.neuron.input_types',
-              'spynnaker.pyNN.models.neuron.master_pop_table_generators',
-              'spynnaker.pyNN.models.neuron.neuron_models',
-              'spynnaker.pyNN.models.neuron.plasticity',
-              'spynnaker.pyNN.models.neuron.plasticity.stdp',
-              'spynnaker.pyNN.models.neuron.plasticity.stdp.common',
-              'spynnaker.pyNN.models.neuron.plasticity.stdp.synapse_structure',
-              'spynnaker.pyNN.models.neuron.plasticity.stdp.timing_dependence',
-              'spynnaker.pyNN.models.neuron.plasticity.stdp.weight_dependence',
-              'spynnaker.pyNN.models.neuron.synapse_dynamics',
-              'spynnaker.pyNN.models.neuron.synapse_io',
-              'spynnaker.pyNN.models.neuron.synapse_types',
-              'spynnaker.pyNN.models.neuron.threshold_types',
-              'spynnaker.pyNN.models.spike_source',
-              'spynnaker.pyNN.models.utility_models',
-              'spynnaker.pyNN.overridden_pacman_functions',
-              'spynnaker.pyNN.utilities',
-              'spynnaker.pyNN.utilities.conf',
-              'spynnaker.pyNN.utilities.random_stats',],
-    package_data={'spynnaker.pyNN.model_binaries': ['*.aplx'],
-                  'spynnaker': ['spynnaker.cfg'],
-                  'spynnaker.pyNN.utilities.conf': ['spynnaker.cfg.template'],
-                  'spynnaker.pyNN.overridden_pacman_functions': ['*.xml']},
+    packages=packages,
+    package_data=package_data,
     install_requires=install_requires
 )
