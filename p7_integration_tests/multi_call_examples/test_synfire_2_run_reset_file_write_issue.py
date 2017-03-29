22341a4b2612e97f3ce618a7bbce96879fe7269e
Christian <brenninc@cs.man.ac.uk>
2017-03-29 10:51:36 +0100

    updated test

--- p7_integration_tests/multi_call_examples/test_synfire_2_run_reset_file_write_issue.py
+++ p7_integration_tests/multi_call_examples/test_synfire_2_run_reset_file_write_issue.py
@@ -8,17 +8,18 @@ import spynnaker.spike_checker as spike_checker
 
 import p7_integration_tests.scripts.synfire_run as synfire_run
 
-
-class Synfire2RunResetFileWriteIssue(unittest.TestCase):
+class Synfire2RunResetNewPopIfCurrExp(unittest.TestCase):
+    @unittest.skip("skipping test_multi_call_examples/"
+                   "test_synfire_2_run_reset_file_write_issue.py")
     def test_run(self):
         nNeurons = 200  # number of neurons in each population
         results = synfire_run.do_run(nNeurons,  runtimes=[1000, 1000],
-                                     extract_between_runs=False, reset=True,
-                                     new_pop=True)
+                                     extract_between_runs=False, reset=True)
         (v, gsyn, spikes) = results
         self.assertEquals(53, len(spikes))
         spike_checker.synfire_spike_checker(spikes, nNeurons)
 
+spikeArray = {'spike_times': [[0, 1050]]}
 
 if __name__ == '__main__':
     nNeurons = 200  # number of neurons in each population
