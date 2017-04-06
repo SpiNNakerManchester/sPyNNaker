e567561d9e5937e485f33082ef4e9aed0f067480
Christian <brenninc@cs.man.ac.uk>
2017-04-06 14:36:30 +0100

    revert back to just the addin files in setup

--- p7_integration_tests/setup.py
+++ p7_integration_tests/setup.py
@@ -1,19 +1,7 @@
-import os
 from setuptools import setup
-from collections import defaultdict
 
-main_package = "p7_integration_tests"
-main_package_dir = os.path.join(os.path.dirname(__file__))
-start = len(main_package_dir)
-packages = []
-package_data = defaultdict(list)
-for dirname, dirnames, filenames in os.walk(main_package_dir):
-    if '__init__.py' in filenames:
-        package = "{}{}".format(
-            main_package, dirname[start:].replace(os.sep, '.'))
-        packages.append(package)
-
-print packages
+packages = ["p7_integration_tests",
+            "p7_integration_tests.scripts"]
 
 setup(
     packages=packages,
