56795100f5f7fe818942548cbaeceaae30187d67
Christian <brenninc@cs.man.ac.uk>
2017-04-06 14:18:51 +0100

    add all p7_integeration_test modules

--- p7_integration_tests/setup.py
+++ p7_integration_tests/setup.py
@@ -1,7 +1,17 @@
+import os
 from setuptools import setup
+from collections import defaultdict
 
-packages = ["p7_integration_tests",
-            "p7_integration_tests.scripts"]
+main_package = "p7_integration_tests"
+main_package_dir = os.path.join(os.path.dirname(__file__), main_package)
+start = len(main_package_dir)
+packages = []
+package_data = defaultdict(list)
+for dirname, dirnames, filenames in os.walk(main_package_dir):
+    if '__init__.py' in filenames:
+        package = "{}{}".format(
+            main_package, dirname[start:].replace(os.sep, '.'))
+        packages.append(package)
 
 setup(
     packages=packages,
