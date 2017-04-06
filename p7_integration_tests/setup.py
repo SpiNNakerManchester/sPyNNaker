d3a2948c00f5f818278e1b8d0b584949a7873150
Christian <brenninc@cs.man.ac.uk>
2017-04-06 14:30:07 +0100

    corrected for bing in a different directory

--- p7_integration_tests/setup.py
+++ p7_integration_tests/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 from collections import defaultdict
 
 main_package = "p7_integration_tests"
-main_package_dir = os.path.join(os.path.dirname(__file__), main_package)
+main_package_dir = os.path.join(os.path.dirname(__file__))
 start = len(main_package_dir)
 packages = []
 package_data = defaultdict(list)
@@ -12,6 +12,8 @@ for dirname, dirnames, filenames in os.walk(main_package_dir):
         package = "{}{}".format(
             main_package, dirname[start:].replace(os.sep, '.'))
         packages.append(package)
+
+print packages
 
 setup(
     packages=packages,
