7583e68eebdd0005cd88ae486c813245279ea65b
Christian Brenninkmeijer <brenninc@cs.man.ac.uk>
2017-04-25 15:31:49 +0100

    added all import test

--- unittests/test_import_all.py
+++ unittests/test_import_all.py
@@ -6,4 +6,4 @@ import spinn_utilities.package_loader as package_loader
 class ImportAllModule(unittest.TestCase):
 
     def test_import_all(self):
-        package_loader.load_module("spynnaker", remove_pyc_files=False)
+        package_loader.load_module("spynnaker", remove_pyc_files=True)
