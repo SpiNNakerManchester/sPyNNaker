f6382c5dd7a511b6d9158fb3fd94cb3e99f67065
Christian <brenninc@cs.man.ac.uk>
2017-05-11 11:53:27 +0100

    Checker can handle none wrap around files

--- spynnaker/spike_checker.py
+++ spynnaker/spike_checker.py
@@ -14,7 +14,17 @@ def synfire_spike_checker(spikes, nNeurons):
             num = 0
 
 
-def synfire_multiple_lines_spike_checker(spikes, nNeurons, lines):
+def synfire_multiple_lines_spike_checker(spikes, nNeurons, lines,
+                                         wrap_around=True):
+    """
+    Checks that there are the expected number of spike lines
+
+    :param spikes: The spikes
+    :param nNeurons: Number of neurons
+    :param lines: Expected number of lines
+    :param wrap_around: If True the lines will wrap around when reaching the
+        last neuron
+    """
     sorted = spikes[spikes[:, 1].argsort()]
     nums = [0] * lines
     used = [False] * lines
@@ -25,7 +35,7 @@ def synfire_multiple_lines_spike_checker(spikes, nNeurons, lines):
             if nums[i] == node:
                 found = True
                 nums[i] += 1
-                if nums[i] >= nNeurons:
+                if nums[i] >= nNeurons and wrap_around:
                     nums[i] = 0
                 used[i] = True
                 break
@@ -41,5 +51,6 @@ def synfire_multiple_lines_spike_checker(spikes, nNeurons, lines):
 
 
 if __name__ == '__main__':
-    spikes = numpy.loadtxt("spikes.csv", delimiter=',')
-    synfire_spike_checker(spikes, 20)
+    spikes = numpy.loadtxt("sorted_spikes.csv", delimiter=',')
+    synfire_multiple_lines_spike_checker(spikes, 200, 10, wrap_around=False)
+    #synfire_spike_checker(spikes, 20)
