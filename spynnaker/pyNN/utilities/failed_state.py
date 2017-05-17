143e39557c2c99de3b6802864e0a6255252f7821
Christian <brenninc@cs.man.ac.uk>
2017-05-17 13:15:11 +0100

    Corrected spelling so bad even Alan noticed.

--- spynnaker/pyNN/utilities/failed_state.py
+++ spynnaker/pyNN/utilities/failed_state.py
@@ -71,79 +71,79 @@ class FailedState(SimulatorInterface):
     def exit():
         raise exceptions.ConfigurationException(
             "You cannot call exit until you have called setup "
-            "or after end/sop has been called.")
+            "or after end/stop has been called.")
 
     @staticmethod
     def stop():
         raise exceptions.ConfigurationException(
             "You cannot call end/stop until you have called setup "
-            "or after end/sop has been called.")
+            "or after end/stop has been called.")
 
     @staticmethod
     def reset(annotations=None):
         raise exceptions.ConfigurationException(
             "You cannot call reset until you have called setup "
-            "or after end/sop has been called.")
+            "or after end/t has been called.")
 
     @staticmethod
     def run_until(time_point, callbacks=None):
         raise exceptions.ConfigurationException(
             "You cannot call run_until until you have called setup "
-            "or after end/sop has been called.")
+            "or after end/stop has been called.")
 
     @staticmethod
     def run_for(simtime, callbacks=None):
         raise exceptions.ConfigurationException(
             "You cannot call run_for until you have called setup "
-            "or after end/sop has been called.")
+            "or after end/stop has been called.")
 
     @staticmethod
     def get_current_time():
         raise exceptions.ConfigurationException(
             "You cannot call get_current_time until you have called setup "
-            "or after end/sop has been called.")
+            "or after end/stop has been called.")
 
     @staticmethod
     def get_time_step():
         raise exceptions.ConfigurationException(
             "You cannot call get_time_step until you have called setup "
-            "or after end/sop has been called.")
+            "or after end/stop has been called.")
 
     @staticmethod
     def get_min_delay():
         raise exceptions.ConfigurationException(
             "You cannot call get_min_delay until you have called setup "
-            "or after end/sop has been called.")
+            "or after end/stop has been called.")
 
     @staticmethod
     def get_max_delay():
         raise exceptions.ConfigurationException(
             "You cannot call get_max_delay until you have called setup "
-            "or after end/sop has been called.")
+            "or after end/stop has been called.")
 
     @staticmethod
     def num_processes():
         raise exceptions.ConfigurationException(
             "You cannot call num_processes until you have called setup "
-            "or after end/sop has been called.")
+            "or after end/stop has been called.")
 
     @staticmethod
     def rank():
         raise exceptions.ConfigurationException(
             "You cannot call rank until you have called setup "
-            "or after end/sop has been called.")
+            "or after end/stop has been called.")
 
     @staticmethod
     def initialize(cells, **initial_values):
         raise exceptions.ConfigurationException(
             "You cannot call initialize until you have called setup"
-            "or after end/sop has been called.")
+            "or after end/stop has been called.")
 
     @staticmethod
     def create(ellclass, cellparams=None, n=1):
         raise exceptions.ConfigurationException(
             "You cannot call create until you have called setup"
-            "or after end/sop has been called.")
+            "or after end/stop has been called.")
 
     @staticmethod
     def connect(
@@ -151,20 +151,20 @@ class FailedState(SimulatorInterface):
             rng=None):
         raise exceptions.ConfigurationException(
             "You cannot call connect until you have called setup"
-            "or after end/sop has been called.")
+            "or after end/stop has been called.")
 
     @staticmethod
     def record(variables, source, filename, sampling_interval=None,
                annotations=None):
         raise exceptions.ConfigurationException(
             "You cannot call record until you have called setup"
-            "or after end/sop has been called.")
+            "or after end/stop has been called.")
 
     @staticmethod
     def min_delay():
         raise exceptions.ConfigurationException(
             "You cannot call min_delay until you have called setup"
-            "or after end/sop has been called.")
+            "or after end/stop has been called.")
 
     @staticmethod
     def _add_socket_address(self, x):
