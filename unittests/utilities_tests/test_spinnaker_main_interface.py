08b5e5d208d911f9511289a93eeeaa17690bba87
Christian <brenninc@cs.man.ac.uk>
2017-05-30 12:54:45 +0100

    fixed broken import

--- unittests/utilities_tests/test_spinnaker_main_interface.py
+++ unittests/utilities_tests/test_spinnaker_main_interface.py
@@ -2,8 +2,8 @@ import unittest
 
 import ConfigParser
 
-from spinn_front_end_common.interface.spinnaker_main_interface import \
-    SpinnakerMainInterface
+from spinn_front_end_common.interface.abstract_spinnaker_base \
+    import AbstractSpinnakerBase
 from spinn_front_end_common.utilities import globals_variables
 from spinn_front_end_common.utilities.utility_objs.executable_finder \
     import ExecutableFinder
@@ -53,10 +53,10 @@ class TestSpinnakerMainInterface(unittest.TestCase):
         return config
 
     def test_min_init(self):
-        SpinnakerMainInterface(self.default_config(), ExecutableFinder())
+        AbstractSpinnakerBase(self.default_config(), ExecutableFinder())
 
     def test_stop_init(self):
-        interface = SpinnakerMainInterface(self.default_config(),
+        interface = AbstractSpinnakerBase(self.default_config(),
                                            ExecutableFinder())
         mock_contoller = Close_Once()
         interface._machine_allocation_controller = mock_contoller
