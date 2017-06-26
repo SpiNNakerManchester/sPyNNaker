aca21114c110362c74383587c5ac630c73415ccb
Christian Brenninkmeijer <brenninc@cs.man.ac.uk>
2017-06-26 09:47:03 +0100

    neuron recorder was missing

---
+++ spynnaker/pyNN/models/common/neuron_recorder.py
@@ -0,0 +1,67 @@
+import logging
+from spynnaker.pyNN.models.common import recording_utils
+from .abstract_uint32_recorder import AbstractUInt32Recorder
+from .abstract_neuron_recordable import AbstractNeuronRecordable
+from spinn_front_end_common.utilities import exceptions as fec_excceptions
+
+logger = logging.getLogger(__name__)
+
+
+class NeuronRecorder(AbstractUInt32Recorder):
+
+    def __init__(self, allowed_variables):
+        AbstractUInt32Recorder.__init__(self)
+        self._record = dict()
+        for variable in allowed_variables:
+            self._record[variable] = False
+
+    def get_recordable_variables(self):
+        return self._record.keys()
+
+    def is_recording(self, variable):
+        print self._record
+        if variable in self._record:
+            return self._record[variable]
+        msg = "Variable {} is not supported. Supported variables include {}" \
+              "".format(variable, self._record.keys())
+        raise fec_excceptions.ConfigurationException(msg)
+
+    @property
+    def recording_variables(self):
+        results = list()
+        for key, value in self._record.iteritems():
+            if value:
+                results.append(key)
+        return results
+
+    def set_recording(self, variable, new_state):
+        if variable == "all":
+            for key in self._record.keys():
+                self._record[key] = new_state
+        elif variable in self._record:
+            self._record[variable] = new_state
+        else:
+            msg = "Variable {} is not supported ".format(variable)
+            raise fec_excceptions.ConfigurationException(msg)
+        print "set", variable, new_state
+        print self._record
+
+    def get_sdram_usage_in_bytes(self, variable, n_neurons, n_machine_time_steps):
+        if self.is_recording(variable):
+            return recording_utils.get_recording_region_size_in_bytes(
+                n_machine_time_steps,  self.N_BYTES_PER_NEURON * n_neurons)
+        else:
+            return 0
+
+    def get_dtcm_usage_in_bytes(self):
+        print self._record
+        print self.recording_variables
+        print len(self.recording_variables)
+        print sum(self._record.values())
+        assert (len(self.recording_variables) == sum(self._record.values()))
+        return  self.N_BYTES_PER_NEURON * sum(self._record.values())
+
+    def get_n_cpu_cycles(self, n_neurons):
+        return  n_neurons * self.N_CPU_CYCLES_PER_NEURON * \
+                sum(self._record.values())
+
