ce5ebedca1fe66660aa6c8cac372e6e20f9fb756
Christian <brenninc@cs.man.ac.uk>
2017-06-30 11:18:56 +0100

    Travis Default Environment Variables appear not to work as expected

--- unittests/test_import_all.py
+++ unittests/test_import_all.py
@@ -6,6 +6,9 @@ import spinn_utilities.package_loader as package_loader
 class ImportAllModule(unittest.TestCase):
 
     def test_import_all(self):
+        print os.environ.get('CONTINUOUS_INTEGRATION', None)
+        print os.environ.get('TRAVIS', None)
+        print os.environ.get('CI', None)
         if os.environ.get('CONTINUOUS_INTEGRATION', None) == 'True':
             package_loader.load_module("spynnaker", remove_pyc_files=False)
         else:
