d56488f9a2fb2eb888c6ef1a54c7c2fac8ecf2e2
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2017-07-18 07:30:43 +0100

    flake8

--- spynnaker/pyNN/models/neuron/input_types/__init__.py
+++ spynnaker/pyNN/models/neuron/input_types/__init__.py
@@ -3,4 +3,7 @@ from .input_type_conductance import InputTypeConductance
 from .input_type_current import InputTypeCurrent
 from .input_type_current_semd import InputTypeCurrentSEMD
 
-__all__ = ["AbstractInputType", "InputTypeConductance", "InputTypeCurrent", "InputTypeCurrentSEMD"]
+__all__ = [
+    "AbstractInputType", "InputTypeConductance", "InputTypeCurrent",
+    "InputTypeCurrentSEMD"
+]
