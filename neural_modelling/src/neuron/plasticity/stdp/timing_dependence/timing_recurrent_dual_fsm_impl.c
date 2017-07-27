5516451d5b68d4c5b186b310a84392b680524e47
simond68 <simon.davidson@manchester.ac.uk>
2017-07-27 15:11:05 +0100

    Moving extra models code here

--- neural_modelling/src/neuron/plasticity/stdp/timing_dependence/timing_recurrent_dual_fsm_impl.c
+++ neural_modelling/src/neuron/plasticity/stdp/timing_dependence/timing_recurrent_dual_fsm_impl.c
@@ -1,14 +1,20 @@
 #include "timing_recurrent_dual_fsm_impl.h"
+#include "random.h"
 
 //---------------------------------------
 // Globals
 //---------------------------------------
+#define ACCUM_SCALING   10
+
 // Exponential lookup-tables
 uint16_t pre_exp_dist_lookup[STDP_FIXED_POINT_ONE];
 uint16_t post_exp_dist_lookup[STDP_FIXED_POINT_ONE];
+uint16_t pre_exp_dist_lookup_inhib[STDP_FIXED_POINT_ONE];
+uint16_t post_exp_dist_lookup_inhib[STDP_FIXED_POINT_ONE];
 
-// Global plasticity parameter data
-plasticity_trace_region_data_t plasticity_trace_region_data;
+uint32_t recurrentSeed[4];
+
+plasticity_params_recurrent_t recurrent_plasticity_params;
 
 //---------------------------------------
 // Functions
@@ -16,28 +22,41 @@ plasticity_trace_region_data_t plasticity_trace_region_data;
 uint32_t *timing_initialise(address_t address) {
 
     log_info("timing_initialise: starting");
-    log_info("\tRecurrent dual-FSM STDP rule");
+    log_info("\tRecurrent dual-FSM STDP Rule");
 
-    // Copy plasticity region data from address
-    // **NOTE** this seems somewhat safer than relying on sizeof
-    plasticity_trace_region_data.accumulator_depression_plus_one =
-        (int32_t) address[0];
-    plasticity_trace_region_data.accumulator_potentiation_minus_one =
-        (int32_t) address[1];
+    recurrent_plasticity_params.accum_decay_per_ts     = (int32_t) address[0];
+    recurrent_plasticity_params.accum_dep_plus_one[0]  = (int32_t) address[1];
+    recurrent_plasticity_params.accum_pot_minus_one[0] = (int32_t) address[2];
+    recurrent_plasticity_params.pre_window_tc[0]       = (int32_t) address[3];
+    recurrent_plasticity_params.post_window_tc[0]      = (int32_t) address[4];
+    recurrent_plasticity_params.accum_dep_plus_one[1]  = (int32_t) address[5];
+    recurrent_plasticity_params.accum_pot_minus_one[1] = (int32_t) address[6];
+    recurrent_plasticity_params.pre_window_tc[1]       = (int32_t) address[7];
+    recurrent_plasticity_params.post_window_tc[1]      = (int32_t) address[8];
 
-    log_info("\tAccumulator depression=%d, Accumulator potentiation=%d",
-             plasticity_trace_region_data
-             .accumulator_depression_plus_one - 1,
-             plasticity_trace_region_data
-             .accumulator_potentiation_minus_one + 1);
+    log_info("Accum decay per TS: %d", (int)(recurrent_plasticity_params.accum_decay_per_ts>>ACCUM_SCALING));
+    log_info("Thresh dep excit: %d", recurrent_plasticity_params.accum_dep_plus_one[0]-1);
+    log_info("Thresh pot excit: %d", recurrent_plasticity_params.accum_pot_minus_one[0]+1);
+    log_info("Mean pre-win excit:  %d", recurrent_plasticity_params.pre_window_tc[0]);
+    log_info("Mean post-win excit: %d", recurrent_plasticity_params.post_window_tc[0]);
+    log_info("Thresh dep inhib: %d", recurrent_plasticity_params.accum_dep_plus_one[1]-1);
+    log_info("Thresh pot inhib: %d", recurrent_plasticity_params.accum_pot_minus_one[1]+1);
+    log_info("Mean pre-win inhib:  %d", recurrent_plasticity_params.pre_window_tc[1]);
+    log_info("Mean post-win inhib: %d", recurrent_plasticity_params.post_window_tc[1]);
 
     // Copy LUTs from following memory
-    // **HACK** these aren't actually int16_t-based but this function will
-    // still work fine
     address_t lut_address = maths_copy_int16_lut(
-        &address[2], STDP_FIXED_POINT_ONE, (int16_t*) &pre_exp_dist_lookup[0]);
+        &address[9], STDP_FIXED_POINT_ONE, (int16_t*) &pre_exp_dist_lookup[0]);
     lut_address = maths_copy_int16_lut(
         lut_address, STDP_FIXED_POINT_ONE, (int16_t*) &post_exp_dist_lookup[0]);
+    lut_address = maths_copy_int16_lut(
+        lut_address, STDP_FIXED_POINT_ONE, (int16_t*) &pre_exp_dist_lookup_inhib[0]);
+    lut_address = maths_copy_int16_lut(
+        lut_address, STDP_FIXED_POINT_ONE, (int16_t*) &post_exp_dist_lookup_inhib[0]);
+
+    memcpy(recurrentSeed, lut_address, 4 * sizeof(uint32_t));
+    lut_address += 4;
+    validate_mars_kiss64_seed(recurrentSeed);
 
     log_info("timing_initialise: completed successfully");
 
--- neural_modelling/src/neuron/plasticity/stdp/timing_dependence/timing_recurrent_dual_fsm_impl.h
+++ neural_modelling/src/neuron/plasticity/stdp/timing_dependence/timing_recurrent_dual_fsm_impl.h
@@ -9,161 +9,228 @@ typedef uint16_t pre_trace_t;
 
 #include "../synapse_structure/synapse_structure_weight_accumulator_impl.h"
 
-#include "timing.h"
-#include "../weight_dependence/weight_one_term.h"
+#include "neuron/plasticity/stdp/timing_dependence/timing.h"
+#include "neuron/plasticity/stdp/weight_dependence/weight_one_term.h"
+#include "neuron/threshold_types/threshold_type_static.h"
 
 // Include debug header for log_info etc
 #include <debug.h>
 
 // Include generic plasticity maths functions
-#include "../../common/maths.h"
-#include "../../common/stdp_typedefs.h"
-
-#include "random_util.h"
+#include "neuron/plasticity/common/maths.h"
+#include "neuron/plasticity/common/stdp_typedefs.h"
+#include "random.h"
 
 typedef struct {
-    int32_t accumulator_depression_plus_one;
-    int32_t accumulator_potentiation_minus_one;
-} plasticity_trace_region_data_t;
+    int32_t accum_decay_per_ts;
+    int32_t accum_dep_plus_one[2];
+    int32_t accum_pot_minus_one[2];
+    int32_t pre_window_tc[2];
+    int32_t post_window_tc[2];
+} plasticity_params_recurrent_t;
+
+static inline weight_state_t weight_one_term_apply_potentiation_sd(weight_state_t state,
+                                                uint32_t syn_type, int32_t potentiation);
+static inline weight_state_t weight_one_term_apply_depression_sd(weight_state_t state,
+                                                  uint32_t syn_type, int32_t depression);
 
 //---------------------------------------
 // Externals
 //---------------------------------------
 extern uint16_t pre_exp_dist_lookup[STDP_FIXED_POINT_ONE];
 extern uint16_t post_exp_dist_lookup[STDP_FIXED_POINT_ONE];
-extern plasticity_trace_region_data_t plasticity_trace_region_data;
+extern uint16_t pre_exp_dist_lookup_inhib[STDP_FIXED_POINT_ONE];
+extern uint16_t post_exp_dist_lookup_inhib[STDP_FIXED_POINT_ONE];
+extern plasticity_params_recurrent_t recurrent_plasticity_params;
+
+static uint32_t last_event_time;
+
+extern uint32_t last_spike;
+
+extern uint32_t recurrentSeed[4];
+
+extern accum *last_voltage;
+extern accum *voltage_before_last_spike;
+extern threshold_type_pointer_t threshold_type_array;
+
+#define ACCUM_SCALING	10
+// With cycle time 35ms, timestep 0.2ms and goal of forgetting an accum update in 6 cycles,
+// this means accum must drain in 210 ms, or 1050 timesteps, so set one step for accum to 1024
+// to approximate this value.
 
 //---------------------------------------
 // Timing dependence inline functions
 //---------------------------------------
 static inline post_trace_t timing_get_initial_post_trace() {
-    return 0;
+    return -9999;
 }
 
 //---------------------------------------
 static inline post_trace_t timing_add_post_spike(
         uint32_t time, uint32_t last_time, post_trace_t last_trace) {
+    return 0; // Return window_length;
+}
+
+//---------------------------------------
+static inline pre_trace_t timing_add_pre_spike_sd( uint32_t time, uint32_t last_time, 
+                  pre_trace_t last_trace, uint32_t syn_type) {
     use(&time);
     use(&last_time);
     use(&last_trace);
 
-    // Pick random number and use to draw from exponential distribution
-    uint32_t random = mars_kiss_fixed_point();
-    uint16_t window_length = post_exp_dist_lookup[random];
-    log_debug("\t\tResetting post-window: random=%d, window_length=%u", random,
-              window_length);
+    uint16_t window_length;
+    last_event_time = last_time;
 
+    // Pick random number and use to draw from exponential distribution
+    uint32_t random = mars_kiss64_seed(recurrentSeed) & (STDP_FIXED_POINT_ONE - 1);
+    if (syn_type == 0) {
+       window_length = pre_exp_dist_lookup[random];       // Excit. synapse
+    }
+    else {
+       window_length = pre_exp_dist_lookup_inhib[random]; // Inhib. synapse
+    }
     // Return window length
     return window_length;
 }
 
 //---------------------------------------
-static inline pre_trace_t timing_add_pre_spike(
-        uint32_t time, uint32_t last_time, pre_trace_t last_trace) {
-    use(&time);
-    use(&last_time);
-    use(&last_trace);
+// This performs three functions:
+// 1) Decay the accumulator value. Long periods with no spikes should cause the state to forget as this
+//    will not correspond to a complete set of pattern repeats.
+// 2) Set the flag for pre_waiting_post (we've got a pre-spike so now waiting for a post -pike)
+// 3) Check if there was a post-spike window open at the time that this pre-spike was detected
+//    in which case we decrement the accumulator and perhaps perform synaptic depression.
 
-    // Pick random number and use to draw from exponential distribution
-    uint32_t random = mars_kiss_fixed_point();
-    uint16_t window_length = pre_exp_dist_lookup[random];
-    log_debug("\t\tResetting pre-window: random=%d, window_length=%u", random,
-              window_length);
-
-    // Return window length
-    return window_length;
-}
-
-//---------------------------------------
-static inline update_state_t timing_apply_pre_spike(
+static inline update_state_t timing_apply_pre_spike_sd(
         uint32_t time, pre_trace_t trace, uint32_t last_pre_time,
         pre_trace_t last_pre_trace, uint32_t last_post_time,
-        post_trace_t last_post_trace, update_state_t previous_state) {
+        post_trace_t last_post_trace, update_state_t previous_state, uint32_t syn_type, uint32_t postNeuronIndex) {
     use(&trace);
     use(&last_pre_time);
     use(&last_pre_trace);
 
-    // Get time of event relative to last post-synaptic event
-    uint32_t time_since_last_post = time - last_post_time;
+	// Decay accum value so that long periods without spikes cause it to forget:
+    uint32_t time_since_last_event = time - last_event_time;
 
-    log_debug("\t\t\ttime_since_last_post:%u, post_window_length:%u",
-              time_since_last_post, last_post_trace);
-
-    // If spikes don't coincide
-    if (time_since_last_post > 0) {
-
-        // If this pre-spike has arrived within the last post window
-        if (time_since_last_post < last_post_trace) {
-
-            if (previous_state.accumulator >
-                    plasticity_trace_region_data
-                    .accumulator_depression_plus_one) {
-
-                // If accumulator's not going to hit depression limit,
-                // decrement it
-                previous_state.accumulator--;
-                log_debug("\t\t\t\tDecrementing accumulator=%d",
-                          previous_state.accumulator);
-            } else {
-
-                // Otherwise, reset accumulator and apply depression
-                log_debug("\t\t\t\tApplying depression");
-
-                previous_state.accumulator = 0;
-                previous_state.weight_state = weight_one_term_apply_depression(
-                    previous_state.weight_state, STDP_FIXED_POINT_ONE);
-            }
+    int32_t acc_change = (recurrent_plasticity_params.accum_decay_per_ts * time_since_last_event)>>ACCUM_SCALING;
+    if (previous_state.accumulator > 0) {
+        previous_state.accumulator -= acc_change;
+        if (previous_state.accumulator < 0) {
+            previous_state.accumulator = 0;
+        }
+    } else if (previous_state.accumulator < 0) {
+        previous_state.accumulator += acc_change;
+        if (previous_state.accumulator > 0) {
+            previous_state.accumulator = 0;
         }
     }
+    // Check if there was a post window open when this pre arrived and if so,
+    // trigger an accum decrement (a step towards synaptic depression):
+    if ((time > last_post_time) && (time < previous_state.longest_post_pre_window_closing_time)) {
+       // The pre-spike has occurred inside a post window.
+       // Get time of event relative to last post-synaptic event
+       uint32_t time_since_last_post = time - last_post_time;
+
+       if (previous_state.accumulator >
+          recurrent_plasticity_params.accum_dep_plus_one[syn_type]<<ACCUM_SCALING){
+             // If accumulator's not going to hit depression limit, decrement it
+             previous_state.accumulator = previous_state.accumulator - (1<<ACCUM_SCALING);
+          } else {
+             // Otherwise, reset accumulator and apply depression
+             previous_state.accumulator = 0;
+             previous_state.weight_state = weight_one_term_apply_depression_sd( previous_state.weight_state,
+                                                                                syn_type, STDP_FIXED_POINT_ONE);
+            }
+       }
+       // Set the post window to be just before this pre-spike. This is the only way I've found to
+       // reset it. It means that the first window length will be garbage.
+       previous_state.longest_post_pre_window_closing_time = time - 1;
+       previous_state.pre_waiting_post = true;
 
     return previous_state;
 }
 
-//---------------------------------------
-static inline update_state_t timing_apply_post_spike(
-        uint32_t time, post_trace_t trace, uint32_t last_pre_time,
-        pre_trace_t last_pre_trace, uint32_t last_post_time,
-        post_trace_t last_post_trace, update_state_t previous_state) {
-    use(&trace);
-    use(&last_post_time);
-    use(&last_post_trace);
+// This routine has two major responsibilities:
+// 1) Generate the window size for this post spike and extend the window closure time
+// if this is beyond the current value. This is used by a following pre-spike for depression
+// 2) Check if there is currently a pre-window open and then check if the post-spike is within
+//    it. If so:
+//               a) increment the accumulator 
+//               b) perform potentiation and reset accumulator if it has reached threshold
+//               c) set the pre_found_post flag, equivalent to clearing the pore_waiting_post 
+//                  state machine back to idle (later post spikes will not cause an accum increment
+//                  until a new pre-spike has arrived).
+static inline update_state_t timing_apply_post_spike_sd(
+   uint32_t time, post_trace_t trace, uint32_t last_pre_time,
+   pre_trace_t last_pre_trace, uint32_t last_post_time,
+   post_trace_t last_post_trace, update_state_t previous_state, uint32_t syn_type, uint32_t postNeuronIndex) {
+   use(&trace);
+   use(&last_post_time);
+   use(&last_post_trace);
 
-    // Get time of event relative to last pre-synaptic event
-    uint32_t time_since_last_pre = time - last_pre_time;
+   // Generate a windw size for this post-spike and extend the post window if it is
+   // beyond the current value:
+   uint32_t random = mars_kiss64_seed(recurrentSeed) & (STDP_FIXED_POINT_ONE - 1);
+   uint16_t window_length;
+   if (syn_type == 0)
+      window_length = post_exp_dist_lookup[random];
+   else
+      window_length = post_exp_dist_lookup_inhib[random];
 
-    log_debug("\t\t\ttime_since_last_pre:%u, pre_window_length:%u",
-              time_since_last_pre, last_pre_trace);
+   uint32_t this_window_close_time = time + window_length;
 
-    // If spikes don't coincide
-    if (time_since_last_pre > 0) {
+   // Check if this post-spike extends the open window:
+   if (previous_state.longest_post_pre_window_closing_time < this_window_close_time) {
+      previous_state.longest_post_pre_window_closing_time = this_window_close_time;
+   }
 
-        // If this post-spike has arrived within the last pre window
-        if (time_since_last_pre < last_pre_trace) {
+   // Get time of event relative to last pre-synaptic event
+   uint32_t time_since_last_pre = time - last_pre_time;
 
+   // If spikes don't coincide:
+   if (previous_state.pre_waiting_post == true && time_since_last_pre > 0) {
+      previous_state.pre_waiting_post = false;
 
-            if (previous_state.accumulator
-                    < plasticity_trace_region_data
-                      .accumulator_potentiation_minus_one) {
+      // Now check if this post spike occurred in the open window created by the previous pre-spike:
+      if (time_since_last_pre < last_pre_trace) {
+         if (previous_state.accumulator < 
+             recurrent_plasticity_params.accum_pot_minus_one[syn_type]<<ACCUM_SCALING){
+             // If accumulator's not going to hit potentiation limit, increment it:
+             previous_state.accumulator = previous_state.accumulator + (1<<ACCUM_SCALING);
+         } else {
+             previous_state.accumulator = 0;
+             previous_state.weight_state = weight_one_term_apply_potentiation_sd(previous_state.weight_state,
+                                                                        syn_type, STDP_FIXED_POINT_ONE);
+         }
+      }
+   }
+   return previous_state;
+}
 
-                // If accumulator's not going to hit potentiation limit,
-                // increment it
-                previous_state.accumulator++;
-                log_debug("\t\t\t\tIncrementing accumulator=%d",
-                          previous_state.accumulator);
-            } else {
+static inline weight_state_t weight_one_term_apply_potentiation_sd(
+   weight_state_t state, uint32_t syn_type, int32_t potentiation) {
 
-                // Otherwise, reset accumulator and apply potentiation
-                log_debug("\t\t\t\tApplying potentiation");
+   int32_t scale = maths_fixed_mul16(
+                   state.weight_region->max_weight - state.weight,
+                   state.weight_region->a2_plus, state.weight_multiply_right_shift);
 
-                previous_state.accumulator = 0;
-                previous_state.weight_state =
-                    weight_one_term_apply_potentiation(
-                        previous_state.weight_state, STDP_FIXED_POINT_ONE);
-            }
-        }
-    }
+   // Multiply scale by potentiation and add
+   // **NOTE** using standard STDP fixed-point format handles format conversion
+   state.weight += STDP_FIXED_MUL_16X16(scale, potentiation);
+   return state;
+}
 
-    return previous_state;
+static inline weight_state_t weight_one_term_apply_depression_sd(
+   weight_state_t state, uint32_t syn_type, int32_t depression) {
+
+   int32_t scale = maths_fixed_mul16(
+                   state.weight - state.weight_region->min_weight,
+                   state.weight_region->a2_minus, state.weight_multiply_right_shift);
+
+    // Multiply scale by depression and subtract
+    // **NOTE** using standard STDP fixed-point format handles format conversion
+    state.weight -= STDP_FIXED_MUL_16X16(scale, depression);
+    return state;
 }
 
 #endif  // _TIMING_RECURRENT_DUAL_FSM_IMPL_H_
