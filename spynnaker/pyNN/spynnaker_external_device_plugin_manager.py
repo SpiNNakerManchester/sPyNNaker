3161ec93adb76d1c06b134fbccd7a09f863fb521
alan-stokes <alan.barry.stokes@gmail.com>
2017-08-02 16:55:03 +0100

    fix

--- spynnaker/pyNN/spynnaker_external_device_plugin_manager.py
+++ spynnaker/pyNN/spynnaker_external_device_plugin_manager.py
@@ -119,7 +119,6 @@ class SpynnakerExternalDevicePluginManager(object):
             use_prefix, key_prefix, prefix_type, message_type, right_shift,
             payload_as_time_stamps, use_payload_prefix, payload_prefix,
             payload_right_shift, number_of_packets_sent_per_time_step,
-            label="LiveSpikeReceiver",
             partition_id=spynnaker_constants.SPIKE_PARTITION_ID)
 
         if notify:
@@ -161,7 +160,7 @@ class SpynnakerExternalDevicePluginManager(object):
             right_shift=0, payload_as_time_stamps=True,
             use_payload_prefix=True, payload_prefix=None,
             payload_right_shift=0, number_of_packets_sent_per_time_step=0,
-            label=None, partition_id=None):
+            partition_id=None):
         """
         adds a edge from a vertex to the LPG object, builds as needed and has
         all the parameters for the creation of the LPG if needed
