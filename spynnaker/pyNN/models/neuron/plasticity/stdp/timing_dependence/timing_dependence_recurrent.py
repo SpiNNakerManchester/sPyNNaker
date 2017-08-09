040acecb8ec89aa674caa20a0ca2717369e8fda9
oliverrhodes <oliver.rhodes@manchester.ac.uk>
2017-08-09 14:09:13 +0100

    Revert to broken state

--- spynnaker/pyNN/models/neuron/plasticity/stdp/timing_dependence/timing_dependence_recurrent.py
+++ spynnaker/pyNN/models/neuron/plasticity/stdp/timing_dependence/timing_dependence_recurrent.py
@@ -17,20 +17,36 @@ class TimingDependenceRecurrent(AbstractTimingDependence):
             self, accum_decay = 10.00,
             accum_dep_thresh_excit=-6, accum_pot_thresh_excit=7,
             pre_window_tc_excit=20.0, post_window_tc_excit=25.0, 
+            accum_dep_thresh_excit2=-6, accum_pot_thresh_excit2=7,
+            pre_window_tc_excit2=20.0, post_window_tc_excit2=25.0, 
             accum_dep_thresh_inhib=-4, accum_pot_thresh_inhib=5,
             pre_window_tc_inhib=35.0, post_window_tc_inhib=45.0, 
+            accum_dep_thresh_inhib2=-4, accum_pot_thresh_inhib2=5,
+            pre_window_tc_inhib2=35.0, post_window_tc_inhib2=45.0, 
             dual_fsm=True, seed=None):
         AbstractTimingDependence.__init__(self)
 
         self.accum_decay               = accum_decay
+
         self.accum_dep_plus_one_excit  = accum_dep_thresh_excit + 1
         self.accum_pot_minus_one_excit = accum_pot_thresh_excit - 1
         self.pre_window_tc_excit = pre_window_tc_excit
         self.post_window_tc_excit = post_window_tc_excit
+
+        self.accum_dep_plus_one_excit2  = accum_dep_thresh_excit2 + 1
+        self.accum_pot_minus_one_excit2 = accum_pot_thresh_excit2 - 1
+        self.pre_window_tc_excit2 = pre_window_tc_excit2
+        self.post_window_tc_excit2 = post_window_tc_excit2
+
         self.accum_dep_plus_one_inhib  = accum_dep_thresh_inhib + 1
         self.accum_pot_minus_one_inhib = accum_pot_thresh_inhib - 1
         self.pre_window_tc_inhib = pre_window_tc_inhib
         self.post_window_tc_inhib = post_window_tc_inhib
+
+        self.accum_dep_plus_one_inhib2  = accum_dep_thresh_inhib2 + 1
+        self.accum_pot_minus_one_inhib2 = accum_pot_thresh_inhib2 - 1
+        self.pre_window_tc_inhib2 = pre_window_tc_inhib2
+        self.post_window_tc_inhib2 = post_window_tc_inhib2
         #self.accumulator_depression_plus_one = accumulator_depression + 1
         #self.accumulator_potentiation_minus_one = accumulator_potentiation - 1
         #self.mean_pre_window = mean_pre_window
@@ -72,8 +88,8 @@ class TimingDependenceRecurrent(AbstractTimingDependence):
 
         # 2 * 32-bit parameters
         # 2 * LUTS with STDP_FIXED_POINT_ONE * 16-bit entries
-        numParams = 9
-        numLUTs   = 2
+        numParams = 17
+        numLUTs   = 4
         numSeeds  = 4
         return (
             (4 * numParams) 
@@ -94,10 +110,21 @@ class TimingDependenceRecurrent(AbstractTimingDependence):
         spec.write_value(data=self.accum_pot_minus_one_excit,  data_type=DataType.INT32)
         spec.write_value(data=self.pre_window_tc_excit,        data_type=DataType.INT32)
         spec.write_value(data=self.post_window_tc_excit,       data_type=DataType.INT32)
+
+        spec.write_value(data=self.accum_dep_plus_one_excit2,   data_type=DataType.INT32)
+        spec.write_value(data=self.accum_pot_minus_one_excit2,  data_type=DataType.INT32)
+        spec.write_value(data=self.pre_window_tc_excit2,        data_type=DataType.INT32)
+        spec.write_value(data=self.post_window_tc_excit2,       data_type=DataType.INT32)
+
         spec.write_value(data=self.accum_dep_plus_one_inhib,   data_type=DataType.INT32)
         spec.write_value(data=self.accum_pot_minus_one_inhib,  data_type=DataType.INT32)
         spec.write_value(data=self.pre_window_tc_inhib,        data_type=DataType.INT32)
         spec.write_value(data=self.post_window_tc_inhib,       data_type=DataType.INT32)
+
+        spec.write_value(data=self.accum_dep_plus_one_inhib2,   data_type=DataType.INT32)
+        spec.write_value(data=self.accum_pot_minus_one_inhib2,  data_type=DataType.INT32)
+        spec.write_value(data=self.pre_window_tc_inhib2,        data_type=DataType.INT32)
+        spec.write_value(data=self.post_window_tc_inhib2,       data_type=DataType.INT32)
 
         # Convert mean times into machine timesteps
         mean_pre_timesteps_excit = (float(self.pre_window_tc_excit) *
