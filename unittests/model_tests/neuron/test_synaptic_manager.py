650e988820247c2675e555b13a678a80ffe1bfc9
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2017-08-17 10:51:47 +0100

    Added testing for reading the direct matrix

--- unittests/model_tests/neuron/test_synaptic_manager.py
+++ unittests/model_tests/neuron/test_synaptic_manager.py
@@ -1,4 +1,5 @@
 import unittest
+import struct
 from spynnaker.pyNN.models.neuron.synaptic_manager import SynapticManager
 from pacman.model.placements.placement import Placement
 from spynnaker.pyNN.abstract_spinnaker_common import AbstractSpiNNakerCommon
@@ -15,20 +16,21 @@ class MockSynapseIO(object):
 
 class MockMasterPopulationTable(object):
 
+    def __init__(self, key_to_entry_map):
+        self._key_to_entry_map = key_to_entry_map
+
     def extract_synaptic_matrix_data_location(
             self, key, master_pop_table_address, transceiver, x, y):
-        return [(1, 0, False)]
+        return self._key_to_entry_map[key]
 
 
-class MockTransceiver(object):
+class MockTransceiverRawData(object):
 
     def __init__(self, data_to_read):
         self._data_to_read = data_to_read
-        self._index = -1
 
     def read_memory(self, x, y, base_address, length):
-        self._index += 1
-        return self._data_to_read[self._index]
+        return self._data_to_read[base_address:base_address + length]
 
 
 class TestSynapticManager(unittest.TestCase):
@@ -41,35 +43,85 @@ class TestSynapticManager(unittest.TestCase):
         config = conf_loader.load_config(
             AbstractSpiNNakerCommon.CONFIG_FILE_NAME, default_config_paths)
 
+        key = 0
+
         synaptic_manager = SynapticManager(
             synapse_type=None, ring_buffer_sigma=5.0, spikes_per_second=100.0,
-            config=config, population_table_type=MockMasterPopulationTable(),
+            config=config,
+            population_table_type=MockMasterPopulationTable(
+                {key: [(1, 0, False)]}),
             synapse_io=MockSynapseIO())
 
-        transceiver = MockTransceiver([
-            bytearray(4), bytearray(5)
-        ])
+        transceiver = MockTransceiverRawData(bytearray(16))
         placement = Placement(None, 0, 0, 1)
 
-        first_block = synaptic_manager._retrieve_synaptic_block(
+        first_block, row_len_1 = synaptic_manager._retrieve_synaptic_block(
             transceiver=transceiver, placement=placement,
             master_pop_table_address=0, indirect_synapses_address=0,
-            direct_synapses_address=0, key=0, n_rows=1, index=0)
-        same_block = synaptic_manager._retrieve_synaptic_block(
+            direct_synapses_address=0, key=key, n_rows=1, index=0)
+        same_block, row_len_1_2 = synaptic_manager._retrieve_synaptic_block(
             transceiver=transceiver, placement=placement,
             master_pop_table_address=0, indirect_synapses_address=0,
-            direct_synapses_address=0, key=0, n_rows=1, index=0)
+            direct_synapses_address=0, key=key, n_rows=1, index=0)
         synaptic_manager.clear_connection_cache()
-        different_block = synaptic_manager._retrieve_synaptic_block(
+        different_block, row_len_2 = synaptic_manager._retrieve_synaptic_block(
             transceiver=transceiver, placement=placement,
             master_pop_table_address=0, indirect_synapses_address=0,
-            direct_synapses_address=0, key=0, n_rows=1, index=0)
+            direct_synapses_address=0, key=key, n_rows=1, index=0)
+
+        # Check that the row lengths are all the same
+        assert row_len_1 == row_len_1_2
+        assert row_len_1 == row_len_2
 
         # Check that the block retrieved twice without reset is cached
-        self.assertEqual(first_block, same_block)
+        assert id(first_block) == id(same_block)
 
         # Check that the block after reset is not a copy
-        self.assertNotEqual(first_block, different_block)
+        assert id(first_block) != id(different_block)
+
+    def test_retrieve_direct_block(self):
+        default_config_paths = os.path.join(
+            os.path.dirname(abstract_spinnaker_common.__file__),
+            AbstractSpiNNakerCommon.CONFIG_FILE_NAME)
+
+        config = conf_loader.load_config(
+            AbstractSpiNNakerCommon.CONFIG_FILE_NAME, default_config_paths)
+
+        key = 0
+        n_rows = 2
+
+        direct_matrix = bytearray(struct.pack("<IIII", 1, 2, 3, 4))
+        direct_matrix_1_expanded = bytearray(
+            struct.pack("<IIIIIIII", 0, 1, 0, 1, 0, 1, 0, 2))
+        direct_matrix_2_expanded = bytearray(
+            struct.pack("<IIIIIIII", 0, 1, 0, 3, 0, 1, 0, 4))
+
+        synaptic_manager = SynapticManager(
+            synapse_type=None, ring_buffer_sigma=5.0, spikes_per_second=100.0,
+            config=config,
+            population_table_type=MockMasterPopulationTable(
+                {key: [(1, 0, True), (1, n_rows * 4, True)]}),
+            synapse_io=MockSynapseIO())
+
+        transceiver = MockTransceiverRawData(direct_matrix)
+        placement = Placement(None, 0, 0, 1)
+
+        data_1, row_len_1 = synaptic_manager._retrieve_synaptic_block(
+            transceiver=transceiver, placement=placement,
+            master_pop_table_address=0, indirect_synapses_address=0,
+            direct_synapses_address=0, key=key, n_rows=n_rows, index=0)
+        data_2, row_len_2 = synaptic_manager._retrieve_synaptic_block(
+            transceiver=transceiver, placement=placement,
+            master_pop_table_address=0, indirect_synapses_address=0,
+            direct_synapses_address=0, key=key, n_rows=n_rows, index=1)
+
+        # Row lengths should be 1
+        assert row_len_1 == 1
+        assert row_len_2 == 1
+
+        # Check the data retrieved
+        assert data_1 == direct_matrix_1_expanded
+        assert data_2 == direct_matrix_2_expanded
 
 
 if __name__ == "__main__":
