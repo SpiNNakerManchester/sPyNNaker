bd2fed768c2b32ff7812ee7297c6c70e61ae2ced
Christian Brenninkmeijer <brenninc@cs.man.ac.uk>
2017-09-21 14:15:18 +0100

    Removed break and unified setup.py to all work the same

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ if os.environ.get('READTHEDOCS', None) != 'True':
 
 # Build a list of all project modules, as well as supplementary files
 main_package = "spynnaker"
-data_extensions = {".aplx", ".xml"}
+data_extensions = {".aplx", ".boot", ".json", ".xml", ".xsd"}
 config_extensions = {".cfg", ".template"}
 main_package_dir = os.path.join(os.path.dirname(__file__), main_package)
 start = len(main_package_dir)
@@ -35,17 +35,19 @@ for dirname, dirnames, filenames in os.walk(main_package_dir):
         package = "{}{}".format(
             main_package, dirname[start:].replace(os.sep, '.'))
         packages.append(package)
+    ext_set = set()
     for filename in filenames:
         _, ext = os.path.splitext(filename)
         if ext in data_extensions:
-            package = "{}{}".format(
-                main_package, dirname[start:].replace(os.sep, '.'))
-            package_data[package].append("*{}".format(ext))
-            break
+            ext_set.add(ext)
         if ext in config_extensions:
             package = "{}{}".format(
                 main_package, dirname[start:].replace(os.sep, '.'))
             package_data[package].append(filename)
+    for ext in ext_set:
+        package = "{}{}".format(
+            main_package, dirname[start:].replace(os.sep, '.'))
+        package_data[package].append("*{}".format(ext))
 
 setup(
     name="sPyNNaker",
