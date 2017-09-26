351063d03a0bc0154dc32280a038dea23f18f775
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2017-09-26 10:59:57 +0100

    unified how package_data is found

--- setup.py
+++ setup.py
@@ -24,8 +24,8 @@ if os.environ.get('READTHEDOCS', None) != 'True':
 
 # Build a list of all project modules, as well as supplementary files
 main_package = "spynnaker"
-data_extensions = {".aplx", ".boot", ".json", ".xml", ".xsd"}
-config_extensions = {".cfg", ".template"}
+extensions = {".aplx", ".boot", ".cfg", ".json", ".sql", ".template", ".xml",
+              ".xsd"}
 main_package_dir = os.path.join(os.path.dirname(__file__), main_package)
 start = len(main_package_dir)
 packages = []
@@ -35,19 +35,12 @@ for dirname, dirnames, filenames in os.walk(main_package_dir):
         package = "{}{}".format(
             main_package, dirname[start:].replace(os.sep, '.'))
         packages.append(package)
-    ext_set = set()
     for filename in filenames:
         _, ext = os.path.splitext(filename)
-        if ext in data_extensions:
-            ext_set.add(ext)
-        if ext in config_extensions:
+        if ext in extensions:
             package = "{}{}".format(
                 main_package, dirname[start:].replace(os.sep, '.'))
             package_data[package].append(filename)
-    for ext in ext_set:
-        package = "{}{}".format(
-            main_package, dirname[start:].replace(os.sep, '.'))
-        package_data[package].append("*{}".format(ext))
 
 setup(
     name="sPyNNaker",
