c3c400541aff6cc69d4160b5244d1a271a8f1b93
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2017-11-14 15:39:28 +0000

    iterator objects

--- spynnaker/pyNN/models/neural_properties/neural_parameter.py
+++ spynnaker/pyNN/models/neural_properties/neural_parameter.py
@@ -1,6 +1,32 @@
 from spinn_utilities.ranged.abstract_list import AbstractList
 
 
+class _List_Iterator(object):
+
+    def __init__(self, iterator, datatype):
+        self._iterator = iterator
+        self._datatype = datatype
+
+    def next(self):
+        return (self._iterator.next(), self._datatype)
+
+
+class _Get_Iterator(object):
+
+    def __init__(self, a_list, datatype, slice_start, slice_stop):
+        self._a_list = a_list
+        self._datatype = datatype
+        self._index = slice_start
+        self._slice_stop = slice_stop
+
+    def next(self):
+        if self._index >= self._slice_stop:
+            raise StopIteration
+        result = self._a_list[self._index]
+        self._index += 1
+        return (result, self._datatype)
+
+
 class NeuronParameter(object):
     def __init__(self, value, datatype):
         self._value = value
@@ -12,12 +38,10 @@ class NeuronParameter(object):
     def get_dataspec_datatype(self):
         return self._datatype
 
-    def start_iterator_by_slice(self, slice_start, slice_stop):
+    def iterator_by_slice(self, slice_start, slice_stop):
         if isinstance(self._value, AbstractList):
-            self._iterator = self._value.iter_by_slice(
-                slice_start=slice_start, slice_stop=slice_stop)
-            return True
-        return False
-
-    def next(self):
-        return self._iterator.next()
+            return _List_Iterator(self._value.iter_by_slice(
+                slice_start=slice_start, slice_stop=slice_stop),
+                self._datatype)
+        return _Get_Iterator(
+            self._value, self._datatype, slice_start, slice_stop)
