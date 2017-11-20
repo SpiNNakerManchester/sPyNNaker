553814ddfa70425763c88d1338efd35eb3f3c37b
oliverrhodes <oliver.rhodes@manchester.ac.uk>
2017-11-20 10:27:34 +0000

    Fix DEFAULT_ATOMS_PER_CORE

--- spynnaker/pyNN/models/neuron/builds/if_curr_comb_exp_4E4I.py
+++ spynnaker/pyNN/models/neuron/builds/if_curr_comb_exp_4E4I.py
@@ -282,7 +282,7 @@ class IFCurrCombExp4E4I(AbstractPopulationVertex):
 
     @staticmethod
     def set_model_max_atoms_per_core(
-            new_value=IFCurrCombExp4E4I._max_feasible_max_atoms_per_core):
+            new_value=DEFAULT_MAX_ATOMS_PER_CORE):
         IFCurrCombExp4E4I._model_based_max_atoms_per_core = new_value
         print "Adjusting neurons per core to: {}".format(new_value)
 
