ad4c09a9b3e38a449345c4a805aa4a1589818e2a
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2017-11-29 17:34:38 +0000

    Use overrides to save comment duplication

--- spynnaker/pyNN/utilities/ranged/spynakker_ranged_list.py
+++ spynnaker/pyNN/utilities/ranged/spynakker_ranged_list.py
@@ -1,15 +1,13 @@
 from spinn_utilities.ranged.ranged_list import RangedList
 from spinn_front_end_common.utilities import globals_variables
+from pacman.model.decorators.overrides import overrides
 
 
 class SpynakkerRangedList(RangedList):
 
     @staticmethod
+    @overrides(RangedList.is_list)
     def is_list(value, size):
-        """ Determines if the value is a list of a given size.
-            An exception is raised if value *is* a list but is shorter\
-            than size
-        """
 
         if globals_variables.get_simulator().is_a_pynn_random(value):
             return True
@@ -17,16 +15,8 @@ class SpynakkerRangedList(RangedList):
         return RangedList.is_list(value, size)
 
     @staticmethod
+    @overrides(RangedList.as_list)
     def as_list(value, size):
-        """
-        Converts if required the value into a list
-
-        Assumes that is_list has been called and returned True
-        So does not repeat the checks there unless required
-
-        :param value:
-        :return:
-        """
 
         if globals_variables.get_simulator().is_a_pynn_random(value):
             return value.next(n=size)
