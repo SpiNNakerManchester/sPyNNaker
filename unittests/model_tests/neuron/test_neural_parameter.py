50896e318de7307b11ad20ae5d8ba5c86d249c7c
Christian Brenninkmeijer <brenninc@cs.man.ac.uk>
2017-12-04 15:43:20 +0000

    removed unused mock

--- unittests/model_tests/neuron/test_neural_parameter.py
+++ unittests/model_tests/neuron/test_neural_parameter.py
@@ -1,16 +1,11 @@
 from spynnaker.pyNN.models.neural_properties import NeuronParameter
 from spynnaker.pyNN.models.neural_properties.neural_parameter \
     import _Range_Iterator, _Get_Iterator, _SingleValue_Iterator
-from spynnaker.pyNN.utilities.spynnaker_failed_state \
-    import SpynnakerFailedState
 from spynnaker.pyNN.utilities.ranged.spynakker_ranged_list \
     import SpynakkerRangedList
 from data_specification.enums import DataType
 from data_specification import DataSpecificationGenerator
 from spinn_storage_handlers.file_data_writer import FileDataWriter
-from spinn_front_end_common.utilities import globals_variables
-
-from unittests.model_tests.neuron.test_synaptic_manager import MockSimulator
 
 import os
 import struct
@@ -30,10 +25,6 @@ def _iterate_parameter_values(iterator, data_type):
 
 
 def test_range_list():
-    simulator = MockSimulator()
-    globals_variables.set_failed_state(SpynnakerFailedState())
-    globals_variables.set_simulator(simulator)
-
     spec_writer = FileDataWriter("test.dat")
     spec = DataSpecificationGenerator(spec_writer, None)
     try:
@@ -55,10 +46,6 @@ def _generator(size):
 
 
 def test_range_list_as_list():
-    simulator = MockSimulator()
-    globals_variables.set_failed_state(SpynnakerFailedState())
-    globals_variables.set_simulator(simulator)
-
     spec_writer = FileDataWriter("test.dat")
     spec = DataSpecificationGenerator(spec_writer, None)
     try:
@@ -74,10 +61,6 @@ def test_range_list_as_list():
 
 
 def test_real_list():
-    simulator = MockSimulator()
-    globals_variables.set_failed_state(SpynnakerFailedState())
-    globals_variables.set_simulator(simulator)
-
     spec_writer = FileDataWriter("test.dat")
     spec = DataSpecificationGenerator(spec_writer, None)
     try:
@@ -93,10 +76,6 @@ def test_real_list():
 
 
 def test_single_value():
-    simulator = MockSimulator()
-    globals_variables.set_failed_state(SpynnakerFailedState())
-    globals_variables.set_simulator(simulator)
-
     spec_writer = FileDataWriter("test.dat")
     spec = DataSpecificationGenerator(spec_writer, None)
     try:
