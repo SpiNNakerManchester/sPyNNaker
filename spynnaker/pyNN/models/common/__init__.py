55f8ecbb4b42f0e30a6d4dca6bf27c6482d621b4
Christian Brenninkmeijer <brenninc@cs.man.ac.uk>
2017-12-28 08:50:27 +0000

    Added global methods to share with spikes

--- spynnaker/pyNN/models/common/__init__.py
+++ spynnaker/pyNN/models/common/__init__.py
@@ -3,13 +3,15 @@ from .abstract_spike_recordable import AbstractSpikeRecordable
 from .eieio_spike_recorder import EIEIOSpikeRecorder
 from .neuron_recorder import NeuronRecorder
 from .multi_spike_recorder import MultiSpikeRecorder
-from .recording_utils import get_buffer_sizes, get_data, \
-    get_recording_region_size_in_bytes, needs_buffering, pull_off_cached_lists
+from .recording_utils import compute_interval, compute_rate, get_buffer_sizes, \
+    get_data, get_recording_region_size_in_bytes, global_parameter, \
+    needs_buffering, pull_off_cached_lists
 from .simple_population_settable import SimplePopulationSettable
 from .spike_recorder import SpikeRecorder
 
 __all__ = ["AbstractNeuronRecordable", "AbstractSpikeRecordable",
            "EIEIOSpikeRecorder", "NeuronRecorder",
            "MultiSpikeRecorder", "SimplePopulationSettable", "SpikeRecorder",
-           "get_buffer_sizes", "get_data", "needs_buffering",
+           "compute_interval", "compute_rate", "get_buffer_sizes", "get_data",
+           "global_parameter", "needs_buffering",
            "get_recording_region_size_in_bytes", "pull_off_cached_lists", ]
