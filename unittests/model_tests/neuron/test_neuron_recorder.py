8e673f68983f7eb4f8638d40a9683339a0dab611
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2018-01-24 15:44:40 +0000

    updated test

--- unittests/model_tests/neuron/test_neuron_recorder.py
+++ unittests/model_tests/neuron/test_neuron_recorder.py
@@ -1,8 +1,21 @@
 from pacman.model.graphs.common import Slice
+from spinn_front_end_common.utilities import globals_variables
 from spynnaker.pyNN.models.common import NeuronRecorder
+from spynnaker.pyNN.utilities.spynnaker_failed_state \
+    import SpynnakerFailedState
+
+
+class MockSimulator(object):
+    @property
+    def machine_time_step(self):
+        return 1000
 
 
 def test_simple_record():
+    simulator = MockSimulator()
+    globals_variables.set_failed_state(SpynnakerFailedState())
+    globals_variables.set_simulator(simulator)
+
     nr = NeuronRecorder(["spikes", "v", "gsyn_exc", "gsyn_inh"], 100)
     assert(["spikes", "v", "gsyn_exc", "gsyn_inh"] ==
            nr.get_recordable_variables())
@@ -11,4 +24,7 @@ def test_simple_record():
     assert(["v"] == nr.recording_variables)
     slice = Slice(0, 50)
     gps = nr.get_global_parameters(slice)
-    assert (gps[0].get_value() == 1)
+    # 4 rates second (index "1") is v
+    assert (gps[1].get_value() == 1)
+    # 4 n_neurons second (index "5") is v
+    assert (gps[5].get_value() == slice.n_atoms)
