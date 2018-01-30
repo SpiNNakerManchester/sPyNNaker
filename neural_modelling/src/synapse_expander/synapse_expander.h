b55bd222cc85a7ab9edeb392cf71b16f9ccd313c
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2018-01-30 16:41:17 +0000

    Starting conversion process

---
+++ neural_modelling/src/synapse_expander/synapse_expander.h
@@ -0,0 +1,14 @@
+#pragma once
+
+typedef enum regions_e {
+    SYSTEM_REGION,           // 0
+    NEURON_PARAMS_REGION,    // 1
+    SYNAPSE_PARAMS_REGION,   // 2
+    POPULATION_TABLE_REGION, // 3
+    SYNAPTIC_MATRIX_REGION,  // 4
+    SYNAPSE_DYNAMICS_REGION, // 5
+    RECORDING_REGION,        // 6
+    PROVENANCE_DATA_REGION,  // 7
+    PROFILING_REGION,        // 8
+    CONNECTOR_BUILDER_REGION // 9
+} regions_e;
