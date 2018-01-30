5266951cf331984b8906da2da761e4ff3793b77d
366anna <366anna@gmail.com>
2018-01-30 16:57:33 +0100

    Logging changes

--- neural_modelling/src/neuron/additional_inputs/additional_input_ca2_concentration_impl.h
+++ neural_modelling/src/neuron/additional_inputs/additional_input_ca2_concentration_impl.h
@@ -30,7 +30,9 @@ static input_t additional_input_get_input_value_as_current(
     //additional_input->I_Ca2 *= additional_input->exp_TauCa;
 	additional_input->I_Ca2 = decay_s1615(additional_input->I_Ca2, additional_input->exp_TauCa);
     log_debug("ca conc from neuron code = %12.6k", additional_input->I_Ca2);
-    log_debug("ca = %12.6k", additional_input->I_Ca2);
+#ifdef _LOG_FIG1_
+    log_info("ca = %12.6k", additional_input->I_Ca2);
+#endif
 
     // Return 0.0 current contribution from the Ca2
     return ZERO;
