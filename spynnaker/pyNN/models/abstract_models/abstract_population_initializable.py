c83c8100a145ea9555f04195cf05402812949a92
christian-B <christian.brenninkmeijer@manchester.ac.uk>
2018-02-20 12:31:30 +0100

    Added a comment

--- spynnaker/pyNN/models/abstract_models/abstract_population_initializable.py
+++ spynnaker/pyNN/models/abstract_models/abstract_population_initializable.py
@@ -29,3 +29,5 @@ class AbstractPopulationInitializable(object):
 
         :return: list of propery names
         """
+        # Note: this will have been none_pynn_default_parameters in the
+        # subclass in previous versions.
