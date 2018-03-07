f58897a59c692cc4bc442adc23c44bf7396a8d86
oliverrhodes <oliver.rhodes@manchester.ac.uk>
2018-03-07 13:58:32 +0000

    Hardcode shift in synaptic manager

--- spynnaker/pyNN/models/neuron/synaptic_manager.py
+++ spynnaker/pyNN/models/neuron/synaptic_manager.py
@@ -534,10 +534,11 @@ class SynapticManager(object):
             machine_time_step):
         # Get the ring buffer shifts and scaling factors
         weight_scale = input_type.get_global_weight_scale()
-        ring_buffer_shifts = self._get_ring_buffer_to_input_left_shifts(
-            machine_vertex, machine_graph, graph_mapper, post_slices,
-            post_slice_index, post_vertex_slice, machine_time_step,
-            weight_scale)
+        ring_buffer_shifts = [12,12]
+#         self._get_ring_buffer_to_input_left_shifts(
+#             machine_vertex, machine_graph, graph_mapper, post_slices,
+#             post_slice_index, post_vertex_slice, machine_time_step,
+#             weight_scale)
 
         spec.switch_write_focus(POPULATION_BASED_REGIONS.SYNAPSE_PARAMS.value)
         write_parameters_per_neuron(
@@ -547,8 +548,12 @@ class SynapticManager(object):
         spec.write_array(ring_buffer_shifts)
 
         weight_scales = numpy.array([
-            self._get_weight_scale(r) * weight_scale
+            #self._get_precision_based_weight_scale() *
+            weight_scale
             for r in ring_buffer_shifts])
+
+#         print weight_scales
+
         return weight_scales
 
     def _write_padding(
