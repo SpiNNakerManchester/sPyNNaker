303a09cbb137d53c065ca78149f97e26a7c14684
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2018-03-14 17:46:21 +0000

    find logs and write on one line

--- neural_modelling/convertor.py
+++ neural_modelling/convertor.py
@@ -5,8 +5,8 @@ import sys
 
 
 class Convertor(object):
-    __slots__ = [
-        "_dest", "_dest_basename", "_src", "_src_basename"]
+    #__slots__ = [
+    #    "_dest", "_dest_basename", "_src", "_src_basename"]
 
     def __init__(self, src, dest):
         self._src = os.path.abspath(src)
@@ -62,6 +62,38 @@ class Convertor(object):
                         self._src_basename, self._dest_basename)
                     dest_f.write(line_dest)
 
+    def _start_log(self, line):
+        self._log_type = None
+        # checking stripped ignores comments and text elsewhere
+        stripped = line.strip()
+        if "log_info(" in stripped:
+            self._log_type = "log_info("
+        if "log_error(" in stripped:
+            self._log_type = "log_error("
+        if "log_debug(" in stripped:
+            self._log_type = "log_debug("
+
+        if self._log_type is None:
+            return False
+        else:
+            self._log_start = line.index(self._log_type)
+            # Empty full and lines as end_log called on first line too
+            self._log_full = ""
+            self._log_lines = 0
+            return True
+
+    def _end_log(self, line, dest_f):
+        self._log_full += line.strip()
+        self._log_lines += 1
+        # may need a mopre complex test but lets start easy
+        if ");" not in self._log_full:
+            return True # Still inside a log
+        else:
+            dest_f.write(" " * self._log_start)
+            dest_f.write(self._log_full)
+            dest_f.write("\n" * (self._log_lines))
+            return False # No longer in log
+
     def convert_c(self, src_path):
         destination = self._check_destination(src_path)
         if destination is None:
@@ -72,6 +104,7 @@ class Convertor(object):
                     "// DO NOT EDIT! THIS FILE WAS GENERATED FROM {}\n\n"
                     .format(src_path))
                 too_many_lines = 2
+                in_log = False
                 for line in src_f:
                     if too_many_lines:
                         # Try to recover the lines added by do not edit
@@ -79,8 +112,12 @@ class Convertor(object):
                         if len(check) == 0 or check == "*":
                             too_many_lines -= 1
                             continue
-                    line_dest = line
-                    dest_f.write(line_dest)
+                    if not in_log:
+                        in_log = self._start_log(line)
+                    if in_log:
+                        in_log = self._end_log(line, dest_f)
+                    else:
+                        dest_f.write(line)
 
         self.copy_if_newer(src_path)
 
