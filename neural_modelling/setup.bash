a9858d35d0c90aab03dd08441344d0bd4b07760e
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2018-03-27 15:19:24 +0100

    We want to export the NEURAL_MODELLING_DIRS variable to subprocesses…

--- neural_modelling/setup.bash
+++ neural_modelling/setup.bash
@@ -5,6 +5,6 @@
 set -a
 
 # Define the location of this library
-NEURAL_MODELLING_DIRS="$( CDPATH= cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export NEURAL_MODELLING_DIRS="$( CDPATH= cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # See https://stackoverflow.com/a/246128/301832 for how this works
 # and https://stackoverflow.com/a/29835459/301832 for why CDPATH is used
