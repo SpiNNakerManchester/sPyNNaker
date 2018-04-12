071c18534b88bd7bfb61784e61f55e78a14bd856
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2018-04-12 16:48:13 +0100

    Moved files to make converting possible

--- neural_modelling/localcommon.mk
+++ neural_modelling/localcommon.mk
@@ -7,25 +7,25 @@ endif
 _MAKEFILE_PATH := $(abspath $(lastword $(MAKEFILE_LIST)))
 _CURRENT_DIR := $(dir $(_MAKEFILE_PATH))
 ifndef APP_OUTPUT_DIR
-    APP_OUTPUT_DIR := $(abspath $(_CURRENT_DIR)../../spynnaker/pyNN/model_binaries/)/
+    APP_OUTPUT_DIR := $(abspath $(_CURRENT_DIR)../spynnaker/pyNN/model_binaries/)/
 endif
 
-# local.mk includes rules for converting in Sources and local files
-include $(SPINN_DIRS)/make/local.mk
-
 # Rules will use the converted version so fthe common files
-COMMON_MODIFIED_DIR := $(NEURAL_MODELLING_DIRS)/src/common_modified/
+COMMON_RAW_DIR := $(NEURAL_MODELLING_DIRS)/common
+COMMON_MODIFIED_DIR := $(NEURAL_MODELLING_DIRS)/common_modified/
+COMMON_DICT_FILE := $(NEURAL_MODELLING_DIRS)/common_modified/common.dict
 # Add to include flag
 CFLAGS += -I $(COMMON_MODIFIED_DIR)
 # Add to list or prerequirements for rule to build o files
 COPIED_DIRS += $(COMMON_MODIFIED_DIR)
 
+# local.mk includes rules for converting in Sources and local files
+include $(SPINN_DIRS)/make/local.mk
+
 # Copy the common files
-RAW_DIR :=  $(NEURAL_MODELLING_DIRS)/src/common/
-CONVERT_DIR := $(COMMON_MODIFIED_DIR)
-SUMMARY_DICT := $(COMMON_MODIFIED_DIR)common.dict
-include $(SPINN_DIRS)/make/convert.mk
+$(COMMON_MODIFIED_DIR) $(COMMON_DICT_FILE): $(COMMON_RAW_DIR)
+	python -m spinn_utilities.make_tools.convertor $(COMMON_RAW_DIR) $(COMMON_MODIFIED_DIR) $(COMMON_DICT_FILE) neural_modelling_common
 
 test:
-	echo $(ALL_TARGETS)
+	echo $(COMMON_MODIFIED_DIR)
 
