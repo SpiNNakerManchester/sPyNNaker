91dcd013b7da6162fb536e01d90fbb684a639ae7
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2018-04-17 14:37:15 +0100

    more make improvements in progress

--- neural_modelling/neuron_make_files/common.mk
+++ neural_modelling/neuron_make_files/common.mk
@@ -59,6 +59,8 @@ endif
 
 ifndef NEURON_MODEL_H
     $(error NEURON_MODEL_H is not set.  Please select a neuron model header file)
+else
+    NEURON_MODEL_H := $(NEURON_MODIFIED_DIR)$(NEURON_MODEL_H)
 endif
 
 ifndef INPUT_TYPE_H
@@ -132,6 +134,8 @@ OBJECTS := $(patsubst $(COMMON_MODIFIED_DIR)%.c,$(COMMON_BUILD_DIR)%.o,$(OBJECTS
 #Build rules
 all: $(APP_OUTPUT_DIR)$(APP).aplx
 
+include $(SPINN_DIRS)/make/Makefile.common
+
 # Copy the common files
 $(COMMON_DICT_FILE): $(COMMON_RAW_DIR)                                                                          # Extra tag as this is a library
 	python -m spinn_utilities.make_tools.convertor $(COMMON_RAW_DIR) $(COMMON_MODIFIED_DIR) $(COMMON_DICT_FILE) neural_modelling_common
@@ -163,80 +167,87 @@ CFLAGS += -I $(COMMON_MODIFIED_DIR)
 INCLUDE_NEURON_HEADERS = -I $(NEURON_MODIFIED_DIR)
 INCLUDE_PLASTICITY_HEADERS = $(INCLUDE_NEURON_HEADERS) -I $(NEURON_MODIFIED_DIR)plasticity
 
-# Make sure build dir exists before building
-$(COMMON_BUILD_DIR):
-	-mkdir -p $(COMMON_BUILD_DIR)
-C_FILES_MODIFIED+= $(COMMON_BUILD_DIR)
-
-$(NEURON_BUILD_DIR):
-	-mkdir -p $(NEURON_BUILD_DIR)
-C_FILES_MODIFIED+= $(NEURON_BUILD_DIR)
-
 # Simple build rules
 $(COMMON_BUILD_DIR)%.o: $(COMMON_MODIFIED_DIR)%.c $(C_FILES_MODIFIED)
 	# Simple
+	-mkdir -p $(dir $@)
 	$(CC) $(CFLAGS) -o $@ $<
 
 $(NEURON_BUILD_DIR)%.o: $(NEURON_MODIFIED_DIR)%.c $(C_FILES_MODIFIED)
 	# Simple
+	-mkdir -p $(dir $@)
 	$(CC) $(CFLAGS) -o $@ $<
 
 # Synapese build rules
-SYNAPSE_TYPE_COMPILE := $(CC) -DLOG_LEVEL=$(SYNAPSE_DEBUG) $(CFLAGS) $(INCLUDE_PLASTICITY_HEADERS) -DSTDP_ENABLED=$(STDP_ENABLED) -include $(SYNAPSE_TYPE_H)
+SYNAPSE_TYPE_COMPILE:= $(CC) -DLOG_LEVEL=$(SYNAPSE_DEBUG) $(CFLAGS) $(INCLUDE_PLASTICITY_HEADERS) -DSTDP_ENABLED=$(STDP_ENABLED) -include $(SYNAPSE_TYPE_H)
 
 $(NEURON_BUILD_DIR)c_main.o: $(NEURON_MODIFIED_DIR)c_main.c $(C_FILES_MODIFIED) 
 	#c_main.c
+	-mkdir -p $(dir $@)
 	$(SYNAPSE_TYPE_COMPILE) -o $@ $<
 
 $(NEURON_BUILD_DIR)synapses.o: $(NEURON_MODIFIED_DIR)synapses.c $(C_FILES_MODIFIED) 
 	#synapses.c
+	-mkdir -p $(dir $@)
 	$(SYNAPSE_TYPE_COMPILE) -o $@ $<
 
 $(NEURON_BUILD_DIR)spike_processing.o: $(NEURON_MODIFIED_DIR)spike_processing.c $(C_FILES_MODIFIED) 
 	#spike_processing.c
+	-mkdir -p $(dir $@)
 	$(SYNAPSE_TYPE_COMPILE) -o $@ $<
 
 $(NEURON_BUILD_DIR)population_table/population_table_fixed_impl.o: $(NEURON_MODIFIED_DIR)population_table/population_table_fixed_impl.c $(C_FILES_MODIFIED) 
 	#population_table/population_table_fixed_impln.c
+	-mkdir -p $(dir $@)
 	$(SYNAPSE_TYPE_COMPILE) -o $@ $<
 
 $(NEURON_BUILD_DIR)population_table/population_table_binary_search_impl.o: $(NEURON_MODIFIED_DIR)population_table/population_table_binary_search_impl.c $(C_FILES_MODIFIED) 
 	#population_table/population_table_binary_search_impl
+	-mkdir -p $(dir $@)
 	$(SYNAPSE_TYPE_COMPILE) -o $@ $<
 
 #STDP Build rules If and only if STDP used
+STDP_ENABLED = 0
+
 ifneq ($(SYNAPSE_DYNAMICS), $(NEURON_MODIFIED_DIR)/plasticity/synapse_dynamics_static_impl.c)
-	STDP_ENABLED = 1
+    STDP_ENABLED = 1
 
-	STDP_INCLUDES:= $(INCLUDE_PLASTICITY_HEADERS) -include $(SYNAPSE_TYPE_H) -include $(WEIGHT_DEPENDENCE_H) -include $$(TIMING_DEPENDENCE_H)
-	STDP_COMPILE:= $(CC) -DLOG_LEVEL=$(PLASTIC_DEBUG) $(CFLAGS) -DSTDP_ENABLED=$(STDP_ENABLED) -DSYNGEN_ENABLED=$(SYNGEN_ENABLED) $(STDP_INCLUDES)
+    STDP_INCLUDES:= $(INCLUDE_PLASTICITY_HEADERS) -include $(SYNAPSE_TYPE_H) -include $(WEIGHT_DEPENDENCE_H) -include $(TIMING_DEPENDENCE_H)
+    STDP_COMPILE = $(CC) -DLOG_LEVEL=$(PLASTIC_DEBUG) $(CFLAGS) -DSTDP_ENABLED=$(STDP_ENABLED) -DSYNGEN_ENABLED=$(SYNGEN_ENABLED) $(STDP_INCLUDES)
 
-	SYNAPSE_DYNAMICS_O := $(patsubst $(NEURON_MODIFIED_DIR)%.c,$(NEURON_BUILD_DIR)%.o,$(SYNAPSE_DYNAMICS))
+    SYNAPSE_DYNAMICS_O := $(patsubst $(NEURON_MODIFIED_DIR)%.c,$(NEURON_BUILD_DIR)%.o,$(SYNAPSE_DYNAMICS))
 
-	$(SYNAPTOGENESIS_DYNAMICS_O): $(SYNAPTOGENESIS_DYNAMICS) $(C_FILES_MODIFIED) 
-		# $(SYNAPTOGENESIS_DYNAMICS)
-		$(STDP_COMPILE) -o $@ $<
+    $(SYNAPSE_DYNAMICS_O): $(SYNAPSE_DYNAMICS) $(C_FILES_MODIFIED) 
+	# SYNAPSE_DYNAMICS_O
+	# $(WEIGHT_DEPENDENCE_H)
+	# $(TIMING_DEPENDENCE_H)
+	-mkdir -p $(dir $@)
+	$(STDP_COMPILE) -o $@ $<
 
-	SYNAPTOGENESIS_DYNAMICS_O := $(patsubst $(NEURON_MODIFIED_DIR)%.c,$(NEURON_BUILD_DIR)%.o,$(SYNAPTOGENESIS_DYNAMICS))
+    SYNAPTOGENESIS_DYNAMICS_O := $(patsubst $(NEURON_MODIFIED_DIR)%.c,$(NEURON_BUILD_DIR)%.o,$(SYNAPTOGENESIS_DYNAMICS))
 
-	$(SYNAPTOGENESIS_DYNAMICS_O): $(SYNAPTOGENESIS_DYNAMICS) $(C_FILES_MODIFIED) 
-		# $(SYNAPTOGENESIS_DYNAMICS) stdp
-		$(STDP_COMPILE) -o $@ $<
+    $(SYNAPTOGENESIS_DYNAMICS_O): $(SYNAPTOGENESIS_DYNAMICS) $(C_FILES_MODIFIED) 
+	# SYNAPTOGENESIS_DYNAMICS_O stdp
+	-mkdir -p $(dir $@)
+	$(STDP_COMPILE) -o $@ $<
 
-	$(NEURON_BUILD_DIR)/plasticity/common/post_events.o: $(NEURON_MODIFIED_DIR)/plasticity/common/post_events.c
-		# plasticity/common/post_events.c
-else
-	STDP_ENABLED = 0
+    $(NEURON_BUILD_DIR)/plasticity/common/post_events.o: $(NEURON_MODIFIED_DIR)/plasticity/common/post_events.c
+	# plasticity/common/post_events.c
+	-mkdir -p $(dir $@)
+	$(STDP_COMPILE) -o $@ $<
 
-	SYNAPTOGENESIS_DYNAMICS_O := $(patsubst $(NEURON_MODIFIED_DIR)%.c,$(NEURON_BUILD_DIR)%.o,$(SYNAPTOGENESIS_DYNAMICS))
+ else
+    SYNAPTOGENESIS_DYNAMICS_O := $(patsubst $(NEURON_MODIFIED_DIR)%.c,$(NEURON_BUILD_DIR)%.o,$(SYNAPTOGENESIS_DYNAMICS))
 
-	$(SYNAPTOGENESIS_DYNAMICS_O): $(SYNAPTOGENESIS_DYNAMICS) $(C_FILES_MODIFIED) 
-		# $(SYNAPTOGENESIS_DYNAMICS) SYNAPSE
-		$(SYNAPSE_TYPE_COMPILE) -o $@ $<
+    $(SYNAPTOGENESIS_DYNAMICS_O): $(SYNAPTOGENESIS_DYNAMICS) $(C_FILES_MODIFIED) 
+	# $(SYNAPTOGENESIS_DYNAMICS) SYNAPSE
+	-mkdir -p $(dir $@)
+	$(SYNAPSE_TYPE_COMPILE) -o $@ $<
 
 endif
 
 $(WEIGHT_DEPENDENCE_O): $(WEIGHT_DEPENDENCE) $(SYNAPSE_TYPE_H)
+	# WEIGHT_DEPENDENCE_O
 	-mkdir -p $(dir $@)
 	$(CC) -D__FILE__=\"$(notdir $*.c)\" -DLOG_LEVEL=$(PLASTIC_DEBUG) $(CFLAGS) \
 	        $(INCLUDE_PLASTICITY_HEADERS) \
@@ -244,6 +255,7 @@ $(WEIGHT_DEPENDENCE_O): $(WEIGHT_DEPENDENCE) $(SYNAPSE_TYPE_H)
 
 $(TIMING_DEPENDENCE_O): $(TIMING_DEPENDENCE) $(SYNAPSE_TYPE_H) \
                         $(WEIGHT_DEPENDENCE_H)
+	# TIMING_DEPENDENCE_O
 	-mkdir -p $(dir $@)
 	$(CC) -D__FILE__=\"$(notdir $*.c)\" -DLOG_LEVEL=$(PLASTIC_DEBUG) $(CFLAGS) \
 	        $(INCLUDE_PLASTICITY_HEADERS) \
@@ -251,12 +263,14 @@ $(TIMING_DEPENDENCE_O): $(TIMING_DEPENDENCE) $(SYNAPSE_TYPE_H) \
 	        -include $(WEIGHT_DEPENDENCE_H) -o $@ $<
 
 $(NEURON_MODEL_O): $(NEURON_MODEL)
+	# NEURON_MODEL_O
 	-mkdir -p $(dir $@)
 	$(CC) -D__FILE__=\"$(notdir $*.c)\" -DLOG_LEVEL=$(NEURON_DEBUG) \
 	        $(CFLAGS) -o $@ $<
 
-$(NEURON_O): $(NEURON_MODEL)/neuron__modified/neuron.c $(NEURON_MODEL_H) \
+$(NEURON_O): $(NEURON_MODIFIED_DIR)neuron.c $(NEURON_MODEL_H) \
                              $(SYNAPSE_TYPE_H)
+	# NEURON_O
 	-mkdir -p $(dir $@)
 	$(CC) -D__FILE__=\"neuron.c\" -DLOG_LEVEL=$(NEURON_DEBUG) $(CFLAGS) \
 	      $(INCLUDE_NEURON_HEADERS) \
@@ -267,8 +281,6 @@ $(NEURON_O): $(NEURON_MODEL)/neuron__modified/neuron.c $(NEURON_MODEL_H) \
 	      -include $(ADDITIONAL_INPUT_H) \
 	      -include $(SYNAPTOGENESIS_DYNAMICS_H) -o $@ $<
 
-include $(SPINN_DIRS)/make/Makefile.common
-
 # Tidy and cleaning dependencies
 clean:
 	$(RM) $(OBJECTS) $(BUILD_DIR)$(APP).elf $(BUILD_DIR)$(APP).txt $(APP_OUTPUT_DIR)$(APP).aplx
@@ -277,7 +289,5 @@ clean:
 	rm -rf $(COMMON_MODIFIED_DIR)
 	rm -rf $(NEURON_MODIFIED_DIR)
 
-test: 
-	echo $(CFLAGS)
 
 
