e52334a6142634f8026938bfdb6cade23c4b872d
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2018-04-23 11:12:24 +0100

    fixed text

--- neural_modelling/src/neuron/builds/common.mk
+++ neural_modelling/src/neuron/builds/common.mk
@@ -1,4 +1,4 @@
 # THIS FILE WAS USED IN AN OLDER MAKE STYLE
 # Make now has to convert c files before building
 # SEE http://spinnakermanchester.github.io/spynnaker/5.0.0/Makefiles.html
-$(error inclusion of Makefile.common)
+$(error inclusion of neural_modelling/src/neuron/builds/common.mk)
