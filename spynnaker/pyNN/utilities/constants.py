1eb8d450dffcac306bd55e2225f0bab3d7926da5
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2018-05-15 13:20:05 +0100

    Add direct matrix region

--- spynnaker/pyNN/utilities/constants.py
+++ spynnaker/pyNN/utilities/constants.py
@@ -56,7 +56,8 @@ POPULATION_BASED_REGIONS = Enum(
            ('RECORDING', 6),
            ('PROVENANCE_DATA', 7),
            ('PROFILING', 8),
-           ('CONNECTOR_BUILDER', 9)])
+           ('CONNECTOR_BUILDER', 9),
+           ('DIRECT_MATRIX', 10)])
 
 # The partition ID used for spike data
 SPIKE_PARTITION_ID = "SPIKE"
