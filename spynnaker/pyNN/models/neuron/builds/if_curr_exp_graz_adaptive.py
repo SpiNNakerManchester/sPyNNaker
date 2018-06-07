590d04c9ecbc7d0fe1963defe0daa649914000b4
oliverrhodes <oliver.rhodes@manchester.ac.uk>
2018-06-07 11:00:19 +0100

    Add skeleton neuron model

---
+++ spynnaker/pyNN/models/neuron/builds/if_curr_exp_graz_adaptive.py
@@ -0,0 +1,104 @@
+from spynnaker.pyNN.models.neuron.neuron_models \
+    import NeuronModelLeakyIntegrateAndFire
+from spynnaker.pyNN.models.neuron.synapse_types import SynapseTypeExponential
+from spynnaker.pyNN.models.neuron.input_types import InputTypeCurrent
+from spynnaker.pyNN.models.neuron.threshold_types import ThresholdTypeStatic
+from spynnaker.pyNN.models.neuron import AbstractPopulationVertex
+
+# global objects
+DEFAULT_MAX_ATOMS_PER_CORE = 255
+_apv_defs = AbstractPopulationVertex.non_pynn_default_parameters
+
+
+class IFCurrExpGrazAdaptive(AbstractPopulationVertex):
+    """ Leaky integrate and fire neuron with an exponentially decaying \
+        current input, and adaptive threshold which increases on spiking
+        before decaying back to a baseline threshold.
+    """
+
+    _model_based_max_atoms_per_core = DEFAULT_MAX_ATOMS_PER_CORE
+
+    default_parameters = {
+        'tau_m': 20.0, 'cm': 1.0, 'v_rest': -65.0, 'v_reset': -65.0,
+
+        # Adaptive threshold parameters
+        'v_thresh': -50.0, 'v_thresh_resting': -50,
+        'v_thresh_tau': 5, 'v_thresh_adaptation': 20,
+
+        'tau_syn_E': 5.0, 'tau_syn_I': 5.0,
+        'tau_refrac': 0.1, 'i_offset': 0, 'isyn_exc': 0.0, 'isyn_inh': 0.0}
+
+    initialize_parameters = {'v_init': None}
+
+    def __init__(
+            self, n_neurons,
+            spikes_per_second=_apv_defs['spikes_per_second'],
+            ring_buffer_sigma=_apv_defs['ring_buffer_sigma'],
+            incoming_spike_buffer_size=_apv_defs['incoming_spike_buffer_size'],
+            constraints=_apv_defs['constraints'],
+            label=_apv_defs['label'],
+            tau_m=default_parameters['tau_m'],
+            cm=default_parameters['cm'],
+            v_rest=default_parameters['v_rest'],
+#             v_reset=default_parameters['v_reset'],
+
+            #Adaptive threshold parameters
+            v_thresh=default_parameters['v_thresh'],
+            v_thresh_resting=default_parameters['v_thresh_resting'],
+            v_thresh_tau=default_parameters['v_thresh_tau'],
+            v_thresh_adaptation=default_parameters['v_thresh_adaptation'],
+
+            tau_syn_E=default_parameters['tau_syn_E'],
+            tau_syn_I=default_parameters['tau_syn_I'],
+            tau_refrac=default_parameters['tau_refrac'],
+            i_offset=default_parameters['i_offset'],
+            v_init=initialize_parameters['v_init'],
+            isyn_exc=default_parameters['isyn_exc'],
+            isyn_inh=default_parameters['isyn_inh']):
+        # pylint: disable=too-many-arguments, too-many-locals
+
+        neuron_model = NeuronModelLeakyIntegrateAndFire(
+            n_neurons, v_init, v_rest, tau_m, cm, i_offset,
+            v_reset, tau_refrac)
+
+        synapse_type = SynapseTypeExponential(
+            n_neurons, tau_syn_E, tau_syn_I, isyn_exc, isyn_inh)
+
+        input_type = InputTypeCurrent()
+
+        threshold_type = ThresholdTypeAdaptive(n_neurons, v_thresh,
+                        v_thresh_resting, v_thresh_tau, v_thresh_adaptation)
+
+        super(IFCurrExpGrazAdaptive, self).__init__(
+            n_neurons=n_neurons, binary="IF_curr_exp_graz_adaptive.aplx", label=label,
+            max_atoms_per_core=IFCurrExpBase._model_based_max_atoms_per_core,
+            spikes_per_second=spikes_per_second,
+            ring_buffer_sigma=ring_buffer_sigma,
+            incoming_spike_buffer_size=incoming_spike_buffer_size,
+            model_name="IF_curr_exp_graz_adaptive", neuron_model=neuron_model,
+            input_type=input_type, synapse_type=synapse_type,
+            threshold_type=threshold_type, constraints=constraints)
+
+    @staticmethod
+    def set_model_max_atoms_per_core(new_value=DEFAULT_MAX_ATOMS_PER_CORE):
+        IFCurrExpGrazAdaptive._model_based_max_atoms_per_core = new_value
+
+    @staticmethod
+    def get_max_atoms_per_core():
+        return IFCurrExpBase._model_based_max_atoms_per_core
+
+    @property
+    def isyn_exc(self):
+        return self.synapse_type.initial_value_exc
+
+    @property
+    def isyn_inh(self):
+        return self.synapse_type.initial_value_inh
+
+    @isyn_exc.setter
+    def isyn_exc(self, new_value):
+        self.synapse_type.initial_value_exc = new_value
+
+    @isyn_inh.setter
+    def isyn_inh(self, new_value):
+        self.synapse_type.initial_value_inh = new_value
--- spynnaker/pyNN/models/neuron/threshold_types/__init__.py
+++ spynnaker/pyNN/models/neuron/threshold_types/__init__.py
@@ -1,6 +1,7 @@
 from .abstract_threshold_type import AbstractThresholdType
 from .threshold_type_static import ThresholdTypeStatic
 from .threshold_type_maass_stochastic import ThresholdTypeMaassStochastic
+from .threshold_type_adaptive import ThresholdTypeAdaptive
 
 __all__ = ["AbstractThresholdType", "ThresholdTypeStatic",
-           "ThresholdTypeMaassStochastic"]
+           "ThresholdTypeMaassStochastic", "ThresholdTypeAdaptive"]
