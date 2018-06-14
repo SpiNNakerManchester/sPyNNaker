e8488026e66d5c7e32dac152ae1c02c8d63e753c
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2018-06-14 19:59:58 +0100

    vera

--- neural_modelling/src/synapse_expander/synapse_expander.h
+++ neural_modelling/src/synapse_expander/synapse_expander.h
@@ -1,15 +1,15 @@
 #pragma once
 
 typedef enum regions_e {
-    SYSTEM_REGION,           // 0
-    NEURON_PARAMS_REGION,    // 1
-    SYNAPSE_PARAMS_REGION,   // 2
-    POPULATION_TABLE_REGION, // 3
-    SYNAPTIC_MATRIX_REGION,  // 4
-    SYNAPSE_DYNAMICS_REGION, // 5
-    RECORDING_REGION,        // 6
-    PROVENANCE_DATA_REGION,  // 7
-    PROFILING_REGION,        // 8
-    CONNECTOR_BUILDER_REGION,// 9
-    DIRECT_MATRIX,           // 10
+    SYSTEM_REGION,            // 0
+    NEURON_PARAMS_REGION,     // 1
+    SYNAPSE_PARAMS_REGION,    // 2
+    POPULATION_TABLE_REGION,  // 3
+    SYNAPTIC_MATRIX_REGION,   // 4
+    SYNAPSE_DYNAMICS_REGION,  // 5
+    RECORDING_REGION,         // 6
+    PROVENANCE_DATA_REGION,   // 7
+    PROFILING_REGION,         // 8
+    CONNECTOR_BUILDER_REGION, // 9
+    DIRECT_MATRIX,            // 10
 } regions_e;
