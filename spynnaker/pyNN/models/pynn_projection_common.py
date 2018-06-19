2484e9a4beceee5301001754f4b43db89877abdf
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2018-06-19 18:21:03 +0100

    Tweaking the documentation strings

--- spynnaker/pyNN/models/pynn_projection_common.py
+++ spynnaker/pyNN/models/pynn_projection_common.py
@@ -330,8 +330,9 @@ class PyNNProjectionCommon(object):
 
     def size(self, gather=True):
         """ Return the total number of connections.
-         - only local connections, if gather is False,
-         - all connections, if gather is True (default)
+
+        :param gather: If False, only get the number of connections locally.\
+            Which means nothing on SpiNNaker...
         """
         # TODO
         raise NotImplementedError
