a58edcca8cea9cc28e719ffcf2bc7f535eb426e0
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2018-07-09 14:35:54 +0100

    Fixed unused import

--- spynnaker/pyNN/models/spike_source/__init__.py
+++ spynnaker/pyNN/models/spike_source/__init__.py
@@ -2,4 +2,4 @@ from .spike_source_array import SpikeSourceArray
 from .spike_source_from_file import SpikeSourceFromFile
 from .spike_source_poisson import SpikeSourcePoisson
 
-__all__ = ["SpikeSourceArray", "SpikeSourceFromFile"]
+__all__ = ["SpikeSourceArray", "SpikeSourceFromFile", "SpikeSourcePoisson"]
