4740592539af0f4c62c8b5ba0c215d24d7ea185c
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2018-09-27 14:14:28 +0100

    Vera

--- neural_modelling/src/neuron/synapses.c
+++ neural_modelling/src/neuron/synapses.c
@@ -224,7 +224,7 @@ static inline void _print_synapse_parameters() {
 
 /* INTERFACE FUNCTIONS */
 bool synapses_initialise(
-        address_t synapse_params_address, address_t direct_matrix_address, 
+        address_t synapse_params_address, address_t direct_matrix_address,
         uint32_t n_neurons_value, uint32_t n_synapse_types_value,
         uint32_t **ring_buffer_to_input_buffer_left_shifts,
         address_t *direct_synapses_address) {
@@ -269,10 +269,10 @@ bool synapses_initialise(
     uint32_t n_neurons_power_2 = n_neurons;
     uint32_t log_n_neurons = 1;
     if (n_neurons != 1) {
-    	if (!is_power_of_2(n_neurons)) {
-    		n_neurons_power_2 = next_power_of_2(n_neurons);
-    	}
-    	log_n_neurons = ilog_2(n_neurons_power_2);
+        if (!is_power_of_2(n_neurons)) {
+            n_neurons_power_2 = next_power_of_2(n_neurons);
+        }
+        log_n_neurons = ilog_2(n_neurons_power_2);
     }
 
     uint32_t n_synapse_types_power_2 = n_synapse_types;
