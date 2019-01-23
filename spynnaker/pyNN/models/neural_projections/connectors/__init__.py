ec6704e56101474c4d0674b9bb7332736fd077a5
Andrew Gait <andrew.gait@manchester.ac.uk>
2019-01-23 14:56:53 +0000

    Move FromFileConnector to sPyNNaker8 repository

--- spynnaker/pyNN/models/neural_projections/connectors/__init__.py
+++ spynnaker/pyNN/models/neural_projections/connectors/__init__.py
@@ -9,7 +9,6 @@ from .distance_dependent_probability_connector import (
 from .fixed_number_post_connector import FixedNumberPostConnector
 from .fixed_number_pre_connector import FixedNumberPreConnector
 from .fixed_probability_connector import FixedProbabilityConnector
-from .from_file_connector import FromFileConnector
 from .from_list_connector import FromListConnector
 from .index_based_probability_connector import IndexBasedProbabilityConnector
 from .multapse_connector import MultapseConnector
@@ -20,6 +19,5 @@ __all__ = ["AbstractConnector", "AbstractGenerateConnectorOnMachine",
            "AllToAllConnector", "ArrayConnector", "CSAConnector",
            "DistanceDependentProbabilityConnector", "FixedNumberPostConnector",
            "FixedNumberPreConnector", "FixedProbabilityConnector",
-           "FromFileConnector", "FromListConnector",
-           "IndexBasedProbabilityConnector", "MultapseConnector",
-           "OneToOneConnector", "SmallWorldConnector", ]
+           "FromListConnector", "IndexBasedProbabilityConnector",
+           "MultapseConnector", "OneToOneConnector", "SmallWorldConnector", ]
