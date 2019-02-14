85c3adf1cfdef8e49fe9301c36599d6d59695b70
oliverrhodes <oliver.rhodes@manchester.ac.uk>
2019-02-14 08:14:01 +0000

    Skeleton Poisson neuron

---
+++ spynnaker/pyNN/models/neuron/builds/poisson_neuron.py
@@ -0,0 +1,31 @@
+from spynnaker.pyNN.models.neuron import AbstractPyNNNeuronModelStandard
+from spynnaker.pyNN.models.defaults import default_initial_values
+from spynnaker.pyNN.models.neuron.neuron_models import (
+    NeuronModelLeakyIntegrateAndFirePoisson)
+from spynnaker.pyNN.models.neuron.synapse_types import SynapseTypeExponential
+from spynnaker.pyNN.models.neuron.input_types import InputTypeCurrent
+from spynnaker.pyNN.models.neuron.threshold_types import ThresholdTypeStatic
+
+
+class PoissonNeuron(AbstractPyNNNeuronModelStandard):
+    """ Leaky integrate and fire neuron which fires poisson spikes with rate
+        set by the neurons membrane potential
+    """
+
+    @default_initial_values({"v", "isyn_exc", "isyn_inh"})
+    def __init__(
+            self, tau_m=20.0, cm=1.0, v_rest=0.0, v_reset=0.0,
+            v_thresh=100, tau_syn_E=5.0, tau_syn_I=5.0, tau_refrac=0.1,
+            i_offset=0.0, v=50, isyn_exc=0.0, isyn_inh=0.0):
+        # pylint: disable=too-many-arguments, too-many-locals
+        neuron_model = NeuronModelLeakyIntegrateAndFirePoisson(
+            v, v_rest, tau_m, cm, i_offset, v_reset, tau_refrac)
+        synapse_type = SynapseTypeExponential(
+            tau_syn_E, tau_syn_I, isyn_exc, isyn_inh)
+        input_type = InputTypeCurrent()
+        threshold_type = ThresholdTypeStatic(v_thresh)
+
+        super(PoissonNeuron, self).__init__(
+            model_name="poisson_neuron", binary="poisson_neuron.aplx",
+            neuron_model=neuron_model, input_type=input_type,
+            synapse_type=synapse_type, threshold_type=threshold_type)
--- spynnaker/pyNN/models/neuron/neuron_models/__init__.py
+++ spynnaker/pyNN/models/neuron/neuron_models/__init__.py
@@ -2,6 +2,9 @@ from .abstract_neuron_model import AbstractNeuronModel
 from .neuron_model_izh import NeuronModelIzh
 from .neuron_model_leaky_integrate_and_fire import (
     NeuronModelLeakyIntegrateAndFire)
+from .neuron_model_leaky_integrate_and_fire_poisson import (
+    NeuronModelLeakyIntegrateAndFirePoisson)
 
 __all__ = ["AbstractNeuronModel", "NeuronModelIzh",
-           "NeuronModelLeakyIntegrateAndFire"]
+           "NeuronModelLeakyIntegrateAndFire",
+           "NeuronModelLeakyIntegrateAndFirePoisson"]
