9af7a1708e864dc836be78ab8fcbc63d2c38d72f
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2019-03-22 13:57:01 +0000

    Sane data specification region API types
    The actual layout of data on disk is identical, of course.

--- neural_modelling/src/spike_source/poisson/spike_source_poisson.c
+++ neural_modelling/src/spike_source/poisson/spike_source_poisson.c
@@ -272,11 +272,12 @@ static bool read_poisson_parameters(address_t address) {
 static bool initialise_recording(){
 
     // Get the address this core's DTCM data starts at from SRAM
-    address_t address = data_specification_get_data_address();
+    data_specification_metadata_t *ds_regions =
+            data_specification_get_data_address();
 
     // Get the system region
     address_t recording_region = data_specification_get_region(
-        SPIKE_HISTORY_REGION, address);
+            SPIKE_HISTORY_REGION, ds_regions);
 
     bool success = recording_initialize(recording_region, &recording_flags);
     log_info("Recording flags = 0x%08x", recording_flags);
@@ -296,22 +297,23 @@ static bool initialize() {
     log_info("Initialise: started");
 
     // Get the address this core's DTCM data starts at from SRAM
-    address_t address = data_specification_get_data_address();
+    data_specification_metadata_t *ds_regions =
+            data_specification_get_data_address();
 
     // Read the header
-    if (!data_specification_read_header(address)) {
+    if (!data_specification_read_header(ds_regions)) {
         return false;
     }
 
     // Get the timing details and set up the simulation interface
     if (!simulation_initialise(
-            data_specification_get_region(SYSTEM, address),
+            data_specification_get_region(SYSTEM, ds_regions),
             APPLICATION_NAME_HASH, &timer_period, &simulation_ticks,
             &infinite_run, SDP, DMA)) {
         return false;
     }
     simulation_set_provenance_data_address(
-        data_specification_get_region(PROVENANCE_REGION, address));
+            data_specification_get_region(PROVENANCE_REGION, ds_regions));
 
     // setup recording region
     if (!initialise_recording()){
@@ -320,12 +322,12 @@ static bool initialize() {
 
     // Setup regions that specify spike source array data
     if (!read_global_parameters(
-            data_specification_get_region(POISSON_PARAMS, address))) {
+            data_specification_get_region(POISSON_PARAMS, ds_regions))) {
         return false;
     }
 
     if (!read_poisson_parameters(
-            data_specification_get_region(POISSON_PARAMS, address))) {
+            data_specification_get_region(POISSON_PARAMS, ds_regions))) {
         return false;
     }
 
@@ -357,10 +359,11 @@ static bool initialize() {
 void resume_callback() {
     recording_reset();
 
-    address_t address = data_specification_get_data_address();
+    data_specification_metadata_t *ds_regions =
+            data_specification_get_data_address();
 
     if (!read_poisson_parameters(
-            data_specification_get_region(POISSON_PARAMS, address))){
+            data_specification_get_region(POISSON_PARAMS, ds_regions))){
         log_error("failed to reread the Poisson parameters from SDRAM");
         rt_error(RTE_SWERR);
     }
@@ -388,20 +391,22 @@ bool store_poisson_parameters() {
     log_info("stored_parameters: starting");
 
     // Get the address this core's DTCM data starts at from SRAM
-    address_t address = data_specification_get_data_address();
-    address = data_specification_get_region(POISSON_PARAMS, address);
+    data_specification_metadata_t *ds_regions =
+            data_specification_get_data_address();
+    address_t param_store =
+            data_specification_get_region(POISSON_PARAMS, ds_regions);
 
     // Copy the global_parameters back to SDRAM
-    spin1_memcpy(address, &global_parameters, sizeof(global_parameters));
+    spin1_memcpy(param_store, &global_parameters, sizeof(global_parameters));
 
     // store spike source parameters into array into SDRAM for reading by
     // the host
     if (global_parameters.n_spike_sources > 0) {
         uint32_t spikes_offset =
-            sizeof(global_parameters) / BYTE_TO_WORD_CONVERTER;
+                sizeof(global_parameters) / BYTE_TO_WORD_CONVERTER;
         spin1_memcpy(
-            &address[spikes_offset], poisson_parameters,
-            global_parameters.n_spike_sources * sizeof(spike_source_t));
+                &param_store[spikes_offset], poisson_parameters,
+                global_parameters.n_spike_sources * sizeof(spike_source_t));
     }
 
     log_info("stored_parameters : completed successfully");
--- neural_modelling/src/synapse_expander/delay_expander.c
+++ neural_modelling/src/synapse_expander/delay_expander.c
@@ -165,11 +165,12 @@ void c_main(void) {
 
     // Get the addresses of the regions
     log_info("Starting To Build Delays");
-    address_t core_address = data_specification_get_data_address();
+    data_specification_metadata_t *ds_regions =
+            data_specification_get_data_address();
     address_t delay_params_address = data_specification_get_region(
-        DELAY_PARAMS, core_address);
+            DELAY_PARAMS, ds_regions);
     address_t params_address = data_specification_get_region(
-        EXPANDER_REGION, core_address);
+            EXPANDER_REGION, ds_regions);
     log_info("\tReading SDRAM delay params at 0x%08x,"
             " expander params at 0x%08x",
             delay_params_address, params_address);
