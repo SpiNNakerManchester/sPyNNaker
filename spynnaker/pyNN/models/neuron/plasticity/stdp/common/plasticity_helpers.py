d5068858c9af9ada6521b5e520fa7f8e49fd2b0c
Chiara Bartolozzi <chiara.bartolozzi@iit.it>
2019-04-29 23:32:35 +0200

    run forever working and correct addresses as output

--- spynnaker/pyNN/models/neuron/plasticity/stdp/common/plasticity_helpers.py
+++ spynnaker/pyNN/models/neuron/plasticity/stdp/common/plasticity_helpers.py
@@ -1,11 +1,9 @@
 import math
 import logging
-<<<<<<< HEAD
+
 import matplotlib.pyplot as plt
 import numpy as np
 
-=======
->>>>>>> refs/remotes/origin/master
 from data_specification.enums import DataType
 from spinn_front_end_common.utilities.utility_objs import ProvenanceDataItem
 
