cd3d29799d62ee063ae0ba207d8ebb5772796e61
basab <basab@manchester.ac.uk>
2019-06-24 17:52:27 +0100

    Added binary of the Conductance based Izhikevich model that can now be used with neuromodulation synapses

--- spynnaker/pyNN/models/neuron/builds/__init__.py
+++ spynnaker/pyNN/models/neuron/builds/__init__.py
@@ -16,10 +16,13 @@ from .if_curr_exp_izhikevich_neuromodulation\
 from .if_curr_exp_semd_base import IFCurrExpSEMDBase
 from .izk_curr_exp_izhikevich_neuromodulation\
     import IZKCurrExpIzhikevichNeuromodulation
+from .izk_cond_exp_izhikevich_neuromodulation\
+    import IZKCondExpIzhikevichNeuromodulation
     
 __all__ = ["EIFConductanceAlphaPopulation", "HHCondExp", "IFCondAlpha",
            "IFCondExpBase", "IFCurrAlpha", "IFCurrDualExpBase",
            "IFCurrExpBase", "IFFacetsConductancePopulation", "IzkCondExpBase",
            "IzkCurrExpBase", "IFCondExpStoc",
            "IFCurrDelta", "IFCurrExpCa2Adaptive",
-           "IFCurrExpIzhikevichNeuromodulation", "IFCurrExpSEMDBase",  "IZKCurrExpIzhikevichNeuromodulation"]
+           "IFCurrExpIzhikevichNeuromodulation", "IFCurrExpSEMDBase",  "IZKCurrExpIzhikevichNeuromodulation",
+           "IZKCondExpIzhikevichNeuromodulation"]
