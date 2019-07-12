7cefa34d89ab4df35657d34847c207c1c940a35a
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2019-07-12 17:21:56 +0100

    Missed a few places

--- spynnaker/pyNN/models/neural_projections/synapse_information.py
+++ spynnaker/pyNN/models/neural_projections/synapse_information.py
@@ -13,6 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+
 class SynapseInformation(object):
     """ Contains the synapse information including the connector, synapse type\
         and synapse dynamics
