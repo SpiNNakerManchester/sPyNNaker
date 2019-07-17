f263e1d35dc213cb23abdd55def5fb6c4b58100c
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2019-07-17 09:23:14 +0100

    Create the buffer

--- neural_modelling/src/neuron/structural_plasticity/synaptogenesis/partner_selection/last_neuron_selection_impl.c
+++ neural_modelling/src/neuron/structural_plasticity/synaptogenesis/partner_selection/last_neuron_selection_impl.c
@@ -7,5 +7,13 @@ uint32_t last_time;
 
 address_t partner_init(address_t data) {
     last_spikes_buffer_size = data[0];
+    for (uint32_t i = 0; i < 2; i++) {
+        last_spikes_buffer[i] = (spike_t *) spin1_malloc(
+            last_spikes_buffer_size * sizeof(spike_t));
+        if (last_spikes_buffer[i] == NULL) {
+            log_error("Out of memory when creating last spikes buffer");
+            rt_error(RTE_SWERR);
+        }
+    }
     return &data[1];
 }
