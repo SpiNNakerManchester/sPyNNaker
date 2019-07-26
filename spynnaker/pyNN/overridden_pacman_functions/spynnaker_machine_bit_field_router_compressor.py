faacb317d4220e00abf4b375d4f7a0cb0321898e
alan-stokes <alan.barry.stokes@gmail.com>
2019-07-26 16:02:14 +0100

    fixes

--- spynnaker/pyNN/overridden_pacman_functions/spynnaker_machine_bit_field_router_compressor.py
+++ spynnaker/pyNN/overridden_pacman_functions/spynnaker_machine_bit_field_router_compressor.py
@@ -41,8 +41,8 @@ class SpynnakerMachineBitFieldRouterCompressor(object):
             produce_report, default_report_folder, target_length,
             routing_infos, time_to_try_for_each_iteration, use_timer_cut_off,
             machine_time_step, time_scale_factor,
-            no_sync_changes, threshold_percentage, executable_targets,
-            compress_only_when_needed=True,
+            no_sync_changes, threshold_percentage,
+            executable_targets, compress_only_when_needed=True,
             compress_as_much_as_possible=False,  provenance_data_objects=None):
         """ entrance for routing table compression with bit field
 
