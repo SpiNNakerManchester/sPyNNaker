7c3e7f64a250fc9fb936abee2a3c770e4331d456
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2019-09-02 09:50:40 +0100

    Change to live connection to make work behind a NAT

--- spynnaker/pyNN/connections/ethernet_control_connection.py
+++ spynnaker/pyNN/connections/ethernet_control_connection.py
@@ -14,50 +14,37 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import logging
-from threading import Thread
 from spinn_front_end_common.utility_models import MultiCastCommand
-from spinnman.connections.udp_packet_connections import EIEIOConnection
 from spinnman.messages.eieio.data_messages import (
     EIEIODataMessage, KeyDataElement, KeyPayloadDataElement)
+from spinn_front_end_common.utilities.connections import LiveEventConnection
 
 logger = logging.getLogger(__name__)
 
 
-class EthernetControlConnection(EIEIOConnection):
+class EthernetControlConnection(LiveEventConnection):
     """ A connection that can translate Ethernet control messages received\
         from a Population
     """
-    # __slots__ = []  # FIXME: Sort out messy class hierarchy
+    __slots__ = ["__translator"]
 
     def __init__(
-            self, translator, local_host=None, local_port=None):
+            self, translator, label, live_packet_gather_label, local_host=None,
+            local_port=None):
         """
         :param translator: The translator of multicast to control commands
         :param local_host: The optional host to listen on
         :param local_port: The optional port to listen on
         """
         super(EthernetControlConnection, self).__init__(
+            live_packet_gather_label, receive_labels=[label],
             local_host=local_host, local_port=local_port)
-        thread = Thread(name="Ethernet Control Connection on {}:{}".format(
-            self.local_ip_address, self.local_port), target=self.run)
-        thread.daemon = True
         self.__translator = translator
-        self.__running = True
-        thread.start()
 
-    def run(self):
-        try:
-            while self.__running:
-                self._step()
-        except Exception:
-            if self.__running:
-                logger.exception("failure processing EIEIO message")
-
-    def _step(self):
-        message = self.receive_eieio_message()
-        if isinstance(message, EIEIODataMessage):
-            while message.is_next_element:
-                self._translate(message.next_element)
+    def _receive_packet_callback(self, packet):
+        if isinstance(packet, EIEIODataMessage):
+            while packet.is_next_element:
+                self._translate(packet.next_element)
 
     def _translate(self, element):
         if isinstance(element, KeyDataElement):
@@ -71,4 +58,5 @@ class EthernetControlConnection(EIEIOConnection):
         """ Close the connection
         """
         self.__running = False
+        self.__receiver.close()
         super(EthernetControlConnection, self).close()
