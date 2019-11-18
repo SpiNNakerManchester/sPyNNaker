2a014d2518114d315b13666d7198bfa969dc4589
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2019-11-18 10:07:18 +0000

    Update version and dependencies for master

--- spynnaker/_version.py
+++ spynnaker/_version.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = "1!5.1.0"
+__version__ = "1!5.1.1"
 __version_month__ = "November"
 __version_year__ = "2019"
 __version_day__ = "15"
