a50f6fc7ef8dd4b9b95d1dc3214f9b94b61188c5
Andrew Gait <andrew.gait@manchester.ac.uk>
2019-12-09 15:32:45 +0000

    Add neuromodulation capability to LIF conductance neuron

--- spynnaker/pyNN/models/neuron/builds/if_cond_exp_izhikevich_neuromodulation.py
+++ spynnaker/pyNN/models/neuron/builds/if_cond_exp_izhikevich_neuromodulation.py
@@ -17,32 +17,33 @@ from spynnaker.pyNN.models.neuron import AbstractPyNNNeuronModelStandard
 from spynnaker.pyNN.models.defaults import default_initial_values
 from spynnaker.pyNN.models.neuron.neuron_models import (
     NeuronModelLeakyIntegrateAndFire)
+from spynnaker.pyNN.models.neuron.input_types import InputTypeConductance
 from spynnaker.pyNN.models.neuron.synapse_types import (
     SynapseTypeExpIzhikevichNeuromodulated)
-from spynnaker.pyNN.models.neuron.input_types import InputTypeCurrent
 from spynnaker.pyNN.models.neuron.threshold_types import ThresholdTypeStatic
 
 
-class IFCurrExpIzhikevichNeuromodulation(AbstractPyNNNeuronModelStandard):
+class IFCondExpIzhikevichNeuromodulation(AbstractPyNNNeuronModelStandard):
     """ Leaky integrate and fire neuron with an exponentially decaying \
-        current input and neuromodulated synapses
+        conductance input and neuromodulated synapses
     """
 
     @default_initial_values({"v", "isyn_exc", "isyn_inh"})
     def __init__(
             self, tau_m=20.0, cm=1.0, v_rest=-65.0, v_reset=-65.0,
             v_thresh=-50.0, tau_syn_E=5.0, tau_syn_I=5.0, tau_refrac=0.1,
-            i_offset=0.0, v=-65.0, isyn_exc=0.0, isyn_inh=0.0):
+            i_offset=0.0, e_rev_E=0.0, e_rev_I=-70.0, v=-65.0,
+            isyn_exc=0.0, isyn_inh=0.0):
         # pylint: disable=too-many-arguments, too-many-locals
         neuron_model = NeuronModelLeakyIntegrateAndFire(
             v, v_rest, tau_m, cm, i_offset, v_reset, tau_refrac)
         synapse_type = SynapseTypeExpIzhikevichNeuromodulated(
             tau_syn_E, tau_syn_I, isyn_exc, isyn_inh)
-        input_type = InputTypeCurrent()
+        input_type = InputTypeConductance(e_rev_E, e_rev_I)
         threshold_type = ThresholdTypeStatic(v_thresh)
 
-        super(IFCurrExpIzhikevichNeuromodulation, self).__init__(
-            model_name="IF_curr_exp_stdp_izhikevich_neuromodulation",
-            binary="IF_curr_exp_stdp_izhikevich_neuromodulation.aplx",
+        super(IFCondExpIzhikevichNeuromodulation, self).__init__(
+            model_name="IF_cond_exp_stdp_izhikevich_neuromodulation",
+            binary="IF_cond_exp_stdp_izhikevich_neuromodulation.aplx",
             neuron_model=neuron_model, input_type=input_type,
             synapse_type=synapse_type, threshold_type=threshold_type)
