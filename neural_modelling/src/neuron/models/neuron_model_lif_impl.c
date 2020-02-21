c7c73fc85abbf036d1e25708bdc34c8f54b5f86a
j43277jw <j43277jw@cspc265.cs.man.ac.uk>
2020-02-21 15:08:02 +0000

    fixed build

--- neural_modelling/src/neuron/models/neuron_model_lif_impl.c
+++ neural_modelling/src/neuron/models/neuron_model_lif_impl.c
@@ -63,7 +63,7 @@ state_t neuron_model_state_update(
         // Get the input in nA
         input_t input_this_timestep =
                 //total_exc - total_inh + external_bias + neuron->I_offset;
-                total_exc - total_inh + external_bias + neuron->I_dynamic[idx];
+                total_exc - total_inh + external_bias + I_dynamic[idx];
 
         lif_neuron_closed_form(
                 neuron, neuron->V_membrane, input_this_timestep);
