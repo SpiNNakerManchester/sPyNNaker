a24e6083f444428210cab3aa5c95d5330aba2ffd
Andrew Gait <andrew.gait@manchester.ac.uk>
2020-03-19 12:09:12 +0000

    satisfy the rat

--- unittests/model_tests/neuron/test_get_max_synapses.py
+++ unittests/model_tests/neuron/test_get_max_synapses.py
@@ -1,10 +1,25 @@
+# Copyright (c) 2017-2019 The University of Manchester
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import pytest
 from spynnaker.pyNN.models.neuron.synapse_dynamics import SynapseDynamicsSTDP
 from spynnaker.pyNN.models.neuron.plasticity.stdp.timing_dependence import (
     TimingDependenceSpikePair)
 from spynnaker.pyNN.models.neuron.plasticity.stdp.weight_dependence import (
     WeightDependenceAdditive)
 from unittests.mocks import MockSimulator
-import pytest
 
 
 @pytest.mark.timeout(1)
