020e79a4d3709136248f0498ded4291efd04eded
Andrew Gait <andrew.gait@manchester.ac.uk>
2020-03-24 15:19:25 +0000

    Report back the number of successful rewires rather than attempts

--- neural_modelling/src/neuron/c_main.c
+++ neural_modelling/src/neuron/c_main.c
@@ -96,8 +96,8 @@ int32_t rewiring_period = 0;
 //! Flag representing whether rewiring is enabled
 bool rewiring = false;
 
-//! For counting the number of rewires to report back
-uint32_t count_rewires = 0;
+//! DEBGGING: for counting the number of rewiring attempts
+uint32_t count_rewire_attempts = 0;
 
 //! The number of neurons on the core
 static uint32_t n_neurons;
@@ -114,7 +114,7 @@ void c_main_store_provenance_data(address_t provenance_region) {
     prov->current_timer_tick = time;
     prov->n_plastic_synaptic_weight_saturations =
             synapse_dynamics_get_plastic_saturation_count();
-    prov->n_rewires = count_rewires;
+    prov->n_rewires = spike_processing_get_successful_rewires();
     log_debug("finished other provenance data");
 }
 
@@ -274,7 +274,7 @@ void timer_callback(uint timer_count, uint unused) {
         // run
         time--;
 
-        log_debug("Rewire tries = %d", count_rewires);
+        log_debug("Rewire tries = %d", count_rewire_attempts);
         simulation_ready_to_read();
         return;
     }
@@ -290,7 +290,7 @@ void timer_callback(uint timer_count, uint unused) {
         } else {
             spike_processing_do_rewiring(1);
         }
-        count_rewires++;
+        count_rewire_attempts++;
     }
 
     // Now do synapse and neuron time step updates
