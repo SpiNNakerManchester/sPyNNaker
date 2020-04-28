2a7365ee1f387770ded16e09368f644ef812b608
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-28 09:27:04 +0000

    Deploy SpiNNakerManchester/sPyNNaker to github.com/SpiNNakerManchester/sPyNNaker.git:gh-pages

--- c/search/functions_8.js
+++ c/search/functions_8.js
@@ -1,6 +1,16 @@
 var searchData=
 [
+  ['in_5fspikes_5fadd_5fspike',['in_spikes_add_spike',['../in__spikes_8h.html#af50c3c392fbfb0d59e24eb90eb06e9f4',1,'in_spikes.h']]],
+  ['in_5fspikes_5fget_5fn_5fbuffer_5foverflows',['in_spikes_get_n_buffer_overflows',['../in__spikes_8h.html#ae405867606c4cf942b7507b9ca5baf4a',1,'in_spikes.h']]],
+  ['in_5fspikes_5fget_5fn_5fbuffer_5funderflows',['in_spikes_get_n_buffer_underflows',['../in__spikes_8h.html#ae98f5c1851e73d640276add2c7793df1',1,'in_spikes.h']]],
+  ['in_5fspikes_5fget_5fnext_5fspike',['in_spikes_get_next_spike',['../in__spikes_8h.html#a27d508007a6c99c3d80caed473318b08',1,'in_spikes.h']]],
   ['in_5fspikes_5finitialize_5fspike_5fbuffer',['in_spikes_initialize_spike_buffer',['../in__spikes_8h.html#a9cc56cd2e4bbe79b5a722357f1959185',1,'in_spikes.h']]],
+  ['in_5fspikes_5finput_5findex',['in_spikes_input_index',['../in__spikes_8h.html#a7437cf899a2be1ed04cb5cd2c445051d',1,'in_spikes.h']]],
+  ['in_5fspikes_5fis_5fnext_5fspike_5fequal',['in_spikes_is_next_spike_equal',['../in__spikes_8h.html#a2ae3e298f1e13b8804dc826a229c6669',1,'in_spikes.h']]],
+  ['in_5fspikes_5foutput_5findex',['in_spikes_output_index',['../in__spikes_8h.html#a06eb057099ca98c17d7aca3cf1754636',1,'in_spikes.h']]],
+  ['in_5fspikes_5fprint_5fbuffer',['in_spikes_print_buffer',['../in__spikes_8h.html#af2c8052da82bc8ce24310a278994ef33',1,'in_spikes.h']]],
+  ['in_5fspikes_5freal_5fsize',['in_spikes_real_size',['../in__spikes_8h.html#a4fdfc400d8b3a5f4456b56de6e1b42a1',1,'in_spikes.h']]],
+  ['in_5fspikes_5fvalue_5fat_5findex',['in_spikes_value_at_index',['../in__spikes_8h.html#a645f3cede41ec753b8d9cae272b37dda',1,'in_spikes.h']]],
   ['incoming_5fspike_5fcallback',['incoming_spike_callback',['../robot__motor__control_8c.html#a6b818e9883e19c8fc320e4aa2f800e47',1,'robot_motor_control.c']]],
   ['initialise',['initialise',['../c__main_8c.html#abc8ec4992e18193766cc267a4968f1d7',1,'c_main.c']]],
   ['initialise_5frecording',['initialise_recording',['../spike__source__poisson_8c.html#a25d26d2a2c3ec75e4013c7a5e42bc5a7',1,'spike_source_poisson.c']]],
