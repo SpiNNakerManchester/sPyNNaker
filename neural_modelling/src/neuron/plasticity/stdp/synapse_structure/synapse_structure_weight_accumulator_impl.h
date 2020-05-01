0590c15beafc87311bd70fc0fe284636c8951cd6
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2020-05-01 16:01:16 +0100

    Document synaptic structure

--- neural_modelling/src/neuron/plasticity/stdp/synapse_structure/synapse_structure_weight_accumulator_impl.h
+++ neural_modelling/src/neuron/plasticity/stdp/synapse_structure/synapse_structure_weight_accumulator_impl.h
@@ -25,26 +25,34 @@
 //---------------------------------------
 //! Plastic synapse contains normal 16-bit weight and an accumulator
 typedef struct plastic_synapse_t {
+    //! The state
     weight_t weight;
-
+    //! The accumulator
     int16_t accumulator;
 } plastic_synapse_t;
 
 //! The update state is a weight state with 32-bit ARM-friendly version of the
 //! accumulator
 typedef struct update_state_t {
+    //! The weight staet
     weight_state_t weight_state;
-
+    //! The accumulator (in ARM-friendly format)
     int32_t accumulator;
 } update_state_t;
 
+//! Final states are actually directly what is stored
 typedef plastic_synapse_t final_state_t;
 
 #include "synapse_structure.h"
 
-//! Create update state, using weight-dependence to initialise the weight
-//! state, and copying other parameters from the synaptic word into 32-bit
-//! form.
+//! \brief Get the update state from the synapse structure
+//! \param[in] synaptic_word: The plastic synapse data
+//! \param[in] synapse_type: What (supported) type of synapse is this?
+//! \return The update state
+//! \details
+//!     Creates the update state, using weight-dependence to initialise the
+//!     weight state, and copying other parameters from the synaptic word into
+//!     32-bit form.
 static inline update_state_t synapse_structure_get_update_state(
         plastic_synapse_t synaptic_word, index_t synapse_type) {
     update_state_t update_state;
@@ -55,6 +63,9 @@ static inline update_state_t synapse_structure_get_update_state(
 }
 
 //---------------------------------------
+//! \brief Get the final state from the update state.
+//! \param[in] state: the update state
+//! \return the final state
 static inline final_state_t synapse_structure_get_final_state(
         update_state_t state) {
     // Get weight from state
@@ -68,18 +79,27 @@ static inline final_state_t synapse_structure_get_final_state(
 }
 
 //---------------------------------------
+//! \brief Get the final weight from the final state
+//! \param[in] final_state: the final state
+//! \return the final weight
 static inline weight_t synapse_structure_get_final_weight(
         final_state_t final_state) {
     return final_state.weight;
 }
 
 //---------------------------------------
+//! \brief Get the final plastic synapse data from the final state
+//! \param[in] final_state: the final state
+//! \return the final plastic synapse data, ready to be stored
 static inline plastic_synapse_t synapse_structure_get_final_synaptic_word(
         final_state_t final_state) {
     return final_state;
 }
 
 //---------------------------------------
+//! \brief Create the initial plastic synapse data
+//! \param[in] weight: the initial synaptic weight
+//! \return the plastic synapse data
 static inline plastic_synapse_t synapse_structure_create_synapse(
         weight_t weight) {
     return (plastic_synapse_t) {
@@ -89,6 +109,9 @@ static inline plastic_synapse_t synapse_structure_create_synapse(
 }
 
 //---------------------------------------
+//! \brief Get the current synaptic weight from the plastic synapse data
+//! \param[in] synaptic_word: the plastic synapse data
+//! \return the current synaptic weight
 static inline weight_t synapse_structure_get_weight(
         plastic_synapse_t synaptic_word) {
     return synaptic_word.weight;
--- neural_modelling/src/neuron/plasticity/stdp/synapse_structure/synapse_structure_weight_impl.h
+++ neural_modelling/src/neuron/plasticity/stdp/synapse_structure/synapse_structure_weight_impl.h
@@ -36,36 +36,55 @@ typedef weight_t final_state_t;
 //---------------------------------------
 // Synapse interface functions
 //---------------------------------------
+//! \brief Get the update state from the synapse structure
+//! \param[in] synaptic_word: The plastic synapse data
+//! \param[in] synapse_type: What (supported) type of synapse is this?
+//! \return The update state
 static inline update_state_t synapse_structure_get_update_state(
         plastic_synapse_t synaptic_word, index_t synapse_type) {
     return weight_get_initial(synaptic_word, synapse_type);
 }
 
 //---------------------------------------
+//! \brief Get the final state from the update state.
+//! \param[in] state: the update state
+//! \return the final state
 static inline final_state_t synapse_structure_get_final_state(
         update_state_t state) {
     return weight_get_final(state);
 }
 
 //---------------------------------------
+//! \brief Get the final weight from the final state
+//! \param[in] final_state: the final state
+//! \return the final weight
 static inline weight_t synapse_structure_get_final_weight(
         final_state_t final_state) {
     return final_state;
 }
 
 //---------------------------------------
+//! \brief Get the final plastic synapse data from the final state
+//! \param[in] final_state: the final state
+//! \return the final plastic synapse data, ready to be stored
 static inline plastic_synapse_t synapse_structure_get_final_synaptic_word(
         final_state_t final_state) {
     return final_state;
 }
 
 //---------------------------------------
+//! \brief Create the initial plastic synapse data
+//! \param[in] weight: the initial synaptic weight
+//! \return the plastic synapse data
 static inline plastic_synapse_t synapse_structure_create_synapse(
         weight_t weight) {
     return weight;
 }
 
 //---------------------------------------
+//! \brief Get the current synaptic weight from the plastic synapse data
+//! \param[in] synaptic_word: the plastic synapse data
+//! \return the current synaptic weight
 static inline weight_t synapse_structure_get_weight(
         plastic_synapse_t synaptic_word) {
     return synaptic_word;
