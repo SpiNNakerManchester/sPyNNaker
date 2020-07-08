f8a2e401912580cb1b3e24bc86e15462b8841533
christian-B <christian.brenninkmeijer@manchester.ac.uk>
2020-07-08 16:10:10 +0100

    untangle progress bars while adding progress for cores state check

--- spynnaker/pyNN/overridden_pacman_functions/on_chip_bit_field_generator.py
+++ spynnaker/pyNN/overridden_pacman_functions/on_chip_bit_field_generator.py
@@ -97,7 +97,7 @@ class OnChipBitFieldGenerator(object):
 
         # progress bar
         progress = ProgressBar(
-            len(app_graph.vertices) + 2,
+            len(app_graph.vertices) * 2,
             "Running bitfield generation on chip")
 
         # get data
@@ -114,7 +114,7 @@ class OnChipBitFieldGenerator(object):
             provenance_file_path, executable_finder,
             read_bit_field_generator_iobuf, self._check_for_success,
             None, [CPUState.FINISHED], False, 0,
-            "bit_field_expander_on_{}_{}_{}.txt")
+            "bit_field_expander_on_{}_{}_{}.txt",progress_bar=progress)
         # update progress bar
         progress.end()
 
