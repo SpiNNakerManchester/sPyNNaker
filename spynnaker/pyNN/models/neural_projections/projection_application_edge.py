53e1c9cdc33732d442614563d8e8d4466a6f3056
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2020-07-23 13:43:06 +0100

    Structual Plasticity can learn connection not originally specified

--- spynnaker/pyNN/models/neural_projections/projection_application_edge.py
+++ spynnaker/pyNN/models/neural_projections/projection_application_edge.py
@@ -104,7 +104,7 @@ class ProjectionApplicationEdge(ApplicationEdge, AbstractSlicesConnect):
     @overrides(AbstractSlicesConnect.could_connect)
     def could_connect(self, pre_slice, post_slice):
         for synapse_info in self.__synapse_information:
-            # https://github.com/SpiNNakerManchester/sPyNNaker/issues/848
+            # Structual Plasticity can learn connection not originally specified 
             if _are_dynamics_structural(synapse_info.synapse_dynamics):
                 return True
             if synapse_info.connector.could_connect(
