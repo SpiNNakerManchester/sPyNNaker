5f379d8fa3259592b5cc66ab9f6c79a2d120869f
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2020-08-03 13:35:31 +0100

    Remove unused import

--- spynnaker/pyNN/extra_algorithms/synapse_expander.py
+++ spynnaker/pyNN/extra_algorithms/synapse_expander.py
@@ -12,7 +12,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-import functools
 import logging
 
 from spinn_front_end_common.utilities.system_control_logic import \
