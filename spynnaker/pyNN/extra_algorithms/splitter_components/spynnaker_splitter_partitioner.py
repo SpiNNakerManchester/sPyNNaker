4a640a23ace4bf313c1c4437978cbc0f4fc8bb8b
alan-stokes <alan.barry.stokes@gmail.com>
2020-09-16 16:49:24 +0100

    spynnaker partitioner

---
+++ spynnaker/pyNN/extra_algorithms/splitter_components/spynnaker_splitter_partitioner.py
@@ -0,0 +1,48 @@
+# Copyright (c) 2020-2021 The University of Manchester
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+from pacman.operations.partition_algorithms import SplitterPartitioner
+from spinn_utilities.overrides import overrides
+
+
+class SpynnakerSplitterPartitioner(SplitterPartitioner):
+    """ a splitter partitioner that's bespoke for spynnaker vertices.
+    """
+
+    @overrides(SplitterPartitioner.create_machine_edge)
+    def create_machine_edge(
+            self, src_machine_vertex, dest_machine_vertex,
+            common_edge_type, app_edge, machine_graph,
+            app_outgoing_edge_partition):
+        """ overridable method for creating the machine edges
+
+        :param MachineVertex src_machine_vertex:
+        :param MachineVertex dest_machine_vertex:
+        :param MachineEdge common_edge_type:
+        :param ApplicationEdge app_edge:
+        :param MachineGraph machine_graph:
+        :param OutgoingEdgePartition app_outgoing_edge_partition:
+        :rtype: None
+        """
+
+        # todo add code for handling delays
+
+
+        # build edge and add to machine graph
+        machine_edge = common_edge_type(
+            src_machine_vertex, dest_machine_vertex,
+            app_edge=app_edge)
+        machine_graph.add_edge(
+            machine_edge,
+            app_outgoing_edge_partition.identifier)
