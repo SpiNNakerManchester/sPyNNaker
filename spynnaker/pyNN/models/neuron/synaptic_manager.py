04782bc5a433cb87dbcaf5338928987bdaa91949
Andrew Gait <andrew.gait@manchester.ac.uk>
2020-10-20 14:38:21 +0100

    Seriously...

--- spynnaker/pyNN/models/neuron/synaptic_manager.py
+++ spynnaker/pyNN/models/neuron/synaptic_manager.py
@@ -13,11 +13,11 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from __future__ import division
+
 import struct
 import numpy
 import sys
-
-from __future__ import division
 
 from collections import defaultdict
 from six import itervalues
