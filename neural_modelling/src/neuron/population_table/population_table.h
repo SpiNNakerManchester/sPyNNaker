b7abf569eaf851cf4f621d3ca02ebc78cfaf0797
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2020-10-27 08:55:09 +0000

    Reorder loading of bitfields to avoid memory overrun

--- neural_modelling/src/neuron/population_table/population_table.h
+++ neural_modelling/src/neuron/population_table/population_table.h
@@ -45,14 +45,17 @@ extern uint32_t bit_field_filtered_packets;
 //!                                  data
 //! \param[in] direct_rows_address: The address of the start of the direct
 //!                                 synapse data
-//! \param[in] bitfield_address: The address of the start of the bitfield data
 //! \param[out] row_max_n_words: Updated with the maximum length of any row in
 //!                              the table in words
 //! \return True if the table was initialised successfully, False otherwise
 bool population_table_initialise(
         address_t table_address, address_t synapse_rows_address,
-        address_t direct_rows_address, filter_region_t *bitfield_address,
-        uint32_t *row_max_n_words);
+        address_t direct_rows_address, uint32_t *row_max_n_words);
+
+//! \brief Initialise the bitfield filtering system.
+//! \param[in] filter_region: Where the bitfield configuration is
+//! \return True on success
+bool population_table_load_bitfields(filter_region_t *filter_region);
 
 //! \brief Get the first row data for the given input spike
 //! \param[in] spike: The spike received
