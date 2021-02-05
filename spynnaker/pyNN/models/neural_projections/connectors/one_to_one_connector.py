f3f1aa1c7b016894bd73cb3a68c1a548ed67f62e
Andrew Gait <andrew.gait@manchester.ac.uk>
2021-02-05 10:19:55 +0000

    missed import

--- spynnaker/pyNN/models/neural_projections/connectors/one_to_one_connector.py
+++ spynnaker/pyNN/models/neural_projections/connectors/one_to_one_connector.py
@@ -14,6 +14,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import numpy
+from six import string_types
 from pyNN.random import RandomDistribution
 from pyNN.connectors import OneToOneConnector as PyNNOneToOneConnector
 from spinn_utilities.overrides import overrides
