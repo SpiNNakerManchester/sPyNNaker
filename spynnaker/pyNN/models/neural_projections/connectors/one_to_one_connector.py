71c6eb58d744b8c771165ab1b6ff7a6ac57fdcee
Andrew Gait <andrew.gait@manchester.ac.uk>
2021-02-05 10:27:08 +0000

    another missing import

--- spynnaker/pyNN/models/neural_projections/connectors/one_to_one_connector.py
+++ spynnaker/pyNN/models/neural_projections/connectors/one_to_one_connector.py
@@ -14,6 +14,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import numpy
+import math
 from six import string_types
 from pyNN.random import RandomDistribution
 from pyNN.connectors import OneToOneConnector as PyNNOneToOneConnector
