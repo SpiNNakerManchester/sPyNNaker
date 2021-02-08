be42f02468f753d147dbd0c8507485a8348bd28f
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2021-02-08 08:56:33 +0000

    Try even longer

--- neural_modelling/src/spike_source/poisson/spike_source_poisson.c
+++ neural_modelling/src/spike_source/poisson/spike_source_poisson.c
@@ -717,7 +717,7 @@ static void timer_callback(uint timer_count, UNUSED uint unused) {
         simulation_ready_to_read();
         return;
     }
-    spin1_delay_us(5);
+    spin1_delay_us(100);
 
     // Loop through spike sources
     tdma_processing_reset_phase();
