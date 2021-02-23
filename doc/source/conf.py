52c4ba3b971601bcda5fce097c561103f05cf97a
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2021-02-23 14:07:48 +0000

    Sphinx config tweak

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -388,7 +388,7 @@ for f in os.listdir("."):
 
 # We want to document __call__ when encountered
 autodoc_default_options = {
-    "members": True,
+    "members": None,
     "special-members": "__call__"
 }
 
