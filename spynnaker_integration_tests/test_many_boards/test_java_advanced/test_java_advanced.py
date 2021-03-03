523cfccd7cd7476d80db0093ec1ce9d13fac8068
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2021-03-03 13:09:44 +0000

    more import fixes

--- spynnaker_integration_tests/test_many_boards/test_java_advanced/test_java_advanced.py
+++ spynnaker_integration_tests/test_many_boards/test_java_advanced/test_java_advanced.py
@@ -13,8 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from spynnaker_integration_tests.test_many_boards.manyBoards import (
-    ManyBoards)
+from spynnaker_integration_tests.test_many_boards import ManyBoards
 
 
 class TestJavaAdvanced(ManyBoards):
