f9f73f83df65a976d668c66c18f57ba6517bd63b
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2021-04-27 07:58:17 +0100

    find and check all get_config calls

---
+++ unittests/test_cfg_checker.py
@@ -0,0 +1,33 @@
+# Copyright (c) 2017-2019 The University of Manchester
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import unittest
+from pacman.config_holder import check_python_file
+
+
+class TestCfgChecker(unittest.TestCase):
+
+    def test_import_all(self):
+        # This imports AbstractSpiNNakerCommon which calls set_cfg_files
+        module = __import__("spynnaker8")
+        module = __import__("spynnaker")
+        path = module.__file__
+        directory = os.path.dirname(path)
+        for root, dirs, files in os.walk(directory):
+            for file_name in files:
+                if file_name.endswith(".py"):
+                    py_path = os.path.join(root, file_name)
+                    check_python_file(py_path)
