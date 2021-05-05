de0ba21d1e967872459f3df7349dc0deae8f5678
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2021-05-05 11:37:02 +0100

    cfg and config checker

--- unittests/test_cfg_checker.py
+++ unittests/test_cfg_checker.py
@@ -13,29 +13,18 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import os
 import unittest
-from spinn_utilities.config_holder import (
-    check_python_file, find_double_defaults)
+from spinn_utilities.config_holder import run_config_checks
 
 
 class TestCfgChecker(unittest.TestCase):
 
-    def test_cfg_checker(self):
+    def test_config_checks(self):
         # This imports AbstractSpiNNakerCommon which calls set_cfg_files
         module = __import__("spynnaker8")
-        module = __import__("spynnaker")
-        path = module.__file__
-        directory = os.path.dirname(path)
-        for root, dirs, files in os.walk(directory):
-            for file_name in files:
-                if file_name.endswith(".py"):
-                    py_path = os.path.join(root, file_name)
-                    check_python_file(py_path)
-
-    def test_double_defaults(self):
-        find_double_defaults(repeaters=[
+        repeaters = [
             "application_to_machine_graph_algorithms",
             "machine_graph_to_machine_algorithms",
             "machine_graph_to_virtual_machine_algorithms",
-            "loading_algorithms"])
+            "loading_algorithms"]
+        run_config_checks("spynnaker", repeaters=repeaters)
