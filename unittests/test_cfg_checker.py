7612b57b8e9d381bd7d99d0272569b8dea927dbb
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2021-05-06 10:22:12 +0100

    run_config_checks based on list of dirs

--- unittests/test_cfg_checker.py
+++ unittests/test_cfg_checker.py
@@ -13,18 +13,37 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
 import unittest
 from spinn_utilities.config_holder import run_config_checks
 
 
 class TestCfgChecker(unittest.TestCase):
 
-    def test_config_checks(self):
+    @classmethod
+    def setUpClass(cls):
         # This imports AbstractSpiNNakerCommon which calls set_cfg_files
         module = __import__("spynnaker8")
+
+    def test_config_checks(self):
         repeaters = [
             "application_to_machine_graph_algorithms",
             "machine_graph_to_machine_algorithms",
             "machine_graph_to_virtual_machine_algorithms",
             "loading_algorithms"]
         run_config_checks("spynnaker", repeaters=repeaters)
+
+    def test_config_checks(self):
+        unittests = os.path.dirname(__file__)
+        parent = os.path.dirname(unittests)
+        spynnaker = os.path.join(parent, "spynnaker")
+        spynnaker8 = os.path.join(parent, "spynnaker8")
+        spynnaker_it = os.path.join(parent, "spynnaker_integration_tests")
+        repeaters = [
+            "application_to_machine_graph_algorithms",
+            "machine_graph_to_machine_algorithms",
+            "machine_graph_to_virtual_machine_algorithms",
+            "loading_algorithms"]
+        run_config_checks(
+            directories=[spynnaker, spynnaker8, spynnaker_it, unittests],
+            repeaters=repeaters)
