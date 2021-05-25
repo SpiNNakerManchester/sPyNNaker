2d214906a0a535568860b22e26dc729026cbd253
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2021-05-25 10:48:35 +0100

    log issue that this is untested

--- spynnaker/pyNN/extra_algorithms/spynnaker_neuron_network_specification_report.py
+++ spynnaker/pyNN/extra_algorithms/spynnaker_neuron_network_specification_report.py
@@ -13,10 +13,14 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import logging
 import os
+from spinn_utilities.log import FormatAdapter
 from spinn_utilities.progress_bar import ProgressBar
 from spynnaker.pyNN.exceptions import SpynnakerException
 from spynnaker.pyNN.models.neural_projections import ProjectionApplicationEdge
+
+logger = FormatAdapter(logging.getLogger(__name__))
 
 
 class SpYNNakerNeuronGraphNetworkSpecificationReport(object):
@@ -38,6 +42,9 @@ class SpYNNakerNeuronGraphNetworkSpecificationReport(object):
         try:
             import graphviz
         except ImportError as e:
+            logger.warning(
+                "Currently not tested due to "
+                "https://github.com/SpiNNakerManchester/sPyNNaker/issues/1078")
             raise SpynnakerException(
                 "graphviz is required to use this report. "
                 "Please install graphviz if you want to use this report."
