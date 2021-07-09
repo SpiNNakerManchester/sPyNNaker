d19e569dbbb13c92bab05e63d859127e685f5b06
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2021-07-09 12:42:19 +0100

    Numpy no longer required here

--- spynnaker/pyNN/models/neuron/population_machine_neurons.py
+++ spynnaker/pyNN/models/neuron/population_machine_neurons.py
@@ -13,7 +13,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import ctypes
-import numpy
 from collections import namedtuple
 
 from spinn_utilities.abstract_base import abstractproperty, abstractmethod
