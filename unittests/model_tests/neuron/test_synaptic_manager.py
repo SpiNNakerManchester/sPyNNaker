99df1e06c3a64e26b5404948e3d82957536c27d7
Andrew Gait <andrew.gait@manchester.ac.uk>
2021-11-24 12:13:57 +0000

    Add rb_left_shifts argument in tests

--- unittests/model_tests/neuron/test_synaptic_manager.py
+++ unittests/model_tests/neuron/test_synaptic_manager.py
@@ -218,7 +218,8 @@ def test_set_synapse_dynamics():
     post_app_vertex = post_app_model.create_vertex(
         n_neurons=10, label="post", constraints=None, spikes_per_second=None,
         ring_buffer_sigma=None, incoming_spike_buffer_size=None,
-        n_steps_per_timestep=1, drop_late_spikes=True, splitter=None)
+        n_steps_per_timestep=1, drop_late_spikes=True, splitter=None,
+        rb_left_shifts=None)
 
     static = SynapseDynamicsStatic()
     stdp = SynapseDynamicsSTDP(
@@ -328,7 +329,8 @@ def test_set_synapse_dynamics():
     post_app_vertex = post_app_model.create_vertex(
         n_neurons=10, label="post", constraints=None, spikes_per_second=None,
         ring_buffer_sigma=None, incoming_spike_buffer_size=None,
-        n_steps_per_timestep=1, drop_late_spikes=True, splitter=None)
+        n_steps_per_timestep=1, drop_late_spikes=True, splitter=None,
+        rb_left_shifts=None)
 
     # STDP followed by structural STDP should result in Structural STDP
     post_app_vertex.synapse_dynamics = stdp
@@ -350,7 +352,8 @@ def test_set_synapse_dynamics():
     post_app_vertex = post_app_model.create_vertex(
         n_neurons=10, label="post", constraints=None, spikes_per_second=None,
         ring_buffer_sigma=None, incoming_spike_buffer_size=None,
-        n_steps_per_timestep=1, drop_late_spikes=True, splitter=None)
+        n_steps_per_timestep=1, drop_late_spikes=True, splitter=None,
+        rb_left_shifts=None)
 
     # Static followed by static structural should result in static
     # structural
@@ -387,7 +390,8 @@ def test_set_synapse_dynamics():
     post_app_vertex = post_app_model.create_vertex(
         n_neurons=10, label="post", constraints=None, spikes_per_second=None,
         ring_buffer_sigma=None, incoming_spike_buffer_size=None,
-        n_steps_per_timestep=1, drop_late_spikes=True, splitter=None)
+        n_steps_per_timestep=1, drop_late_spikes=True, splitter=None,
+        rb_left_shifts=None)
     post_app_vertex.synapse_dynamics = static_struct
     post_app_vertex.synapse_dynamics = stdp_struct
 
