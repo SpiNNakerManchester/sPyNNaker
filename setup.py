afbc82ba57556510fcf7a8bc140c65f770230825
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2022-01-12 14:25:25 +0000

    test and support python 3.10 3.6 for curiosity only

--- setup.py
+++ setup.py
@@ -104,10 +104,10 @@ setup(
 
         "Programming Language :: C",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
 
         "Topic :: Scientific/Engineering",
         "Topic :: Scientific/Engineering :: Neuroscience",
