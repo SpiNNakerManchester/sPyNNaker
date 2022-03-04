fc7019724c3422b4e64c9d0b3ec6b5515c96e5cf
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2022-03-04 10:30:27 +0000

    always hard reset if user has accessed the machine

--- spynnaker_integration_tests/test_various/test_get_machine.py
+++ spynnaker_integration_tests/test_various/test_get_machine.py
@@ -30,49 +30,55 @@ class MachineTest(BaseTestCase):
         sim.run(1)
         # HACK to avoid detecting we have a machine
         # DO NOT COPY AS UNSUPPORTED
-        id1 = id(SpynnakerDataView._MachineDataView__data._machine)
+        machine1 = SpynnakerDataView._MachineDataView__data._machine
         sim.run(2)
 
         # Same machine after second run
         # HACK to avoid detecting we have a machine
         # DO NOT COPY AS UNSUPPORTED
-        self.assertEqual(
-            id1, id(SpynnakerDataView._MachineDataView__data._machine), "run2")
+        machine = SpynnakerDataView._MachineDataView__data._machine
+        self.assertEqual(id(machine), id(machine1), "run 2")
 
         sim.reset()  # soft as no get_machine detected
-        # HACK to avoid detecting we have a machine
+        # HACK to avoid creating a new machine
         # DO NOT COPY AS UNSUPPORTED
-        self.assertEqual(
-            id1, id(SpynnakerDataView._MachineDataView__data._machine), "run2")
+        machine = SpynnakerDataView._MachineDataView__data._machine
+        self.assertEqual(id(machine), id(machine1), "reset 1")
         sim.run(3)
-        self.assertEqual(id1, id(sim.get_machine()), "run3")
-
-        sim.reset()  # hard due to get_machine
-        id2 = id(sim.get_machine())
-        self.assertNotEqual(id1, id2, "hard reset")
-        sim.run(4)
-        self.assertEqual(id2, id(sim.get_machine()), "run4")
+        # Normal detected get machine
+        machine = SpynnakerDataView.get_machine()
+        self.assertEqual(id(machine), id(machine1), "run 3")
 
         sim.reset()  # hard due to get_machine
         # HACK to avoid detecting we have a machine
         # DO NOT COPY AS UNSUPPORTED
-        id3 = id(SpynnakerDataView._MachineDataView__data._machine)
+        self.assertIsNone(SpynnakerDataView._MachineDataView__data._machine)
+        sim.run(4)
+        # HACK to avoid detecting we have a machine
+        # DO NOT COPY AS UNSUPPORTED
+        machine2 = SpynnakerDataView._MachineDataView__data._machine
+        print(f"cyab 1:{id(machine1)} 2:{id(machine2)}")
+        self.assertNotEqual(id(machine2), id(machine1), "run 4")
+
+        sim.reset()  # soft reset as no new detectable get machine
+        # HACK to avoid detecting we have a machine
+        # DO NOT COPY AS UNSUPPORTED
+        machine = SpynnakerDataView._MachineDataView__data._machine
+        self.assertEqual(id(machine), id(machine2))
         sim.run(5)
-        self.assertEqual(
-            id3, id(SpynnakerDataView._MachineDataView__data._machine), "run5")
+        machine = SpynnakerDataView._MachineDataView__data._machine
+        self.assertEqual(id(machine), id(machine2), "run 5")
 
         sim.reset()  # Will become hard due to new pop
         # Hard reset not yet done
         # HACK to avoid detecting we have a machine
         # DO NOT COPY AS UNSUPPORTED
-        self.assertEqual(
-            id3, id(SpynnakerDataView._MachineDataView__data._machine),
-            "not hard yet")
+        machine = SpynnakerDataView._MachineDataView__data._machine
+        self.assertEqual(id(machine), id(machine2), "reset 3")
         sim.Population(3, sim.IF_curr_exp(), label="pop_2")
         sim.run(6)
-        id4 = id(SpynnakerDataView._MachineDataView__data._machine)
-        self.assertNotEqual(id3, id4, "run6")
-        sim.end()
+        machine3 = SpynnakerDataView._MachineDataView__data._machine
+        self.assertNotEqual(id(machine3), id(machine2), "run 6")
 
     def test_run(self):
         self.runsafe(self.do_run)
