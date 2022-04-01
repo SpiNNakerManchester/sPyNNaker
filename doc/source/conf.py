3d775db68c0afd48a29446dda8ef9cd1617ada1b
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2022-04-01 11:22:23 +0100

    exclude spynnaker8 from docs

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -390,7 +390,7 @@ if _on_rtd:
 # Automatically called by sphinx at startup
 def setup(app):
     # NB: extra dot at end is deliberate!
-    trim = ("spynnaker.", "spynnaker8.", "spinn_front_end_common.", "pacman.",
+    trim = ("spynnaker.", "spinn_front_end_common.", "pacman.",
             "spinnman.", "spinn_machine.", "data_specification.",
             "spinn_utilities.")
 
@@ -438,6 +438,5 @@ os.chdir("../..")  # WARNING! RELATIVE FILENAMES CHANGE MEANING HERE!
 apidoc.main([
     '-o', _output_dir, ".",
     # Exclude test and setup code
-    "spynnaker_integration_tests/*", "unittests/*", "setup.py",
-    *filtered_files("spynnaker", _unfiltered_files),
-    *filtered_files("spynnaker8", _unfiltered_files)])
+    "spynnaker_integration_tests/*", "unittests/*", "setup.py", "spynnaker8"
+    *filtered_files("spynnaker", _unfiltered_files)])
