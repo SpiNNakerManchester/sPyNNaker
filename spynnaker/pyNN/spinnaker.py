005f340ea64ae12cd5646c876f837226cd9d3d5d
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2022-06-27 14:00:51 +0100

    pylint: disable=attribute-defined-outside-init

--- spynnaker/pyNN/spinnaker.py
+++ spynnaker/pyNN/spinnaker.py
@@ -464,6 +464,7 @@ class SpiNNaker(AbstractSpinnakerBase, pynn_control.BaseState):
             if timer.skip_if_virtual_board():
                 return
             spynnaker_machine_bitfield_ordered_covering_compressor()
+            # pylint: disable=attribute-defined-outside-init
             self._multicast_routes_loaded = True
             return None
 
@@ -474,6 +475,7 @@ class SpiNNaker(AbstractSpinnakerBase, pynn_control.BaseState):
             if timer.skip_if_virtual_board():
                 return
             spynnaker_machine_bitField_pair_router_compressor()
+            # pylint: disable=attribute-defined-outside-init
             self._multicast_routes_loaded = True
             return None
 
