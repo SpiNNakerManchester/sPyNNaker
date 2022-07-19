fe0197352cb864a8dc0817d110fe5312037547c5
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2022-07-19 10:06:34 +0100

    track new vertices and edges

--- spynnaker/pyNN/extra_algorithms/delay_support_adder.py
+++ spynnaker/pyNN/extra_algorithms/delay_support_adder.py
@@ -50,12 +50,15 @@ class _DelaySupportAdder(object):
     __slots__ = [
         "_app_to_delay_map",
         "_delay_post_edge_map",
-        "_delay_pre_edges"]
+        "_new_edges",
+        "_new_vertices"
+        ]
 
     def __init__(self):
         self._app_to_delay_map = dict()
         self._delay_post_edge_map = dict()
-        self._delay_pre_edges = list()
+        self._new_edges = list()
+        self._new_vertices = list()
 
     def _run(self):
         """ adds the delay extensions to the app graph, now that all the\
@@ -91,8 +94,7 @@ class _DelaySupportAdder(object):
                         delay_app_vertex = (
                             self._create_delay_app_vertex_and_pre_edge(
                                 app_outgoing_edge_partition, app_edge,
-                                delay_steps_per_stage, app_graph,
-                                n_delay_stages))
+                                delay_steps_per_stage, n_delay_stages))
 
                         # add the edge from the delay extension to the
                         # dest vertex
@@ -100,19 +102,18 @@ class _DelaySupportAdder(object):
                             delay_app_vertex, app_edge)
 
         # avoids mutating the list of outgoing partitions. add them afterwards
-        self._add_new_app_edges(app_graph)
+        self._add_to_graph(app_graph)
 
-    def _add_new_app_edges(self, app_graph):
+    def _add_to_graph(self, app_graph):
         """ adds new edges to the app graph. avoids mutating the arrays being\
             iterated over previously.
 
         :param ApplicationGraph app_graph: app graph
         :rtype: None
         """
-        for key in self._delay_post_edge_map:
-            delay_edge = self._delay_post_edge_map[key]
-            app_graph.add_edge(delay_edge, constants.SPIKE_PARTITION_ID)
-        for edge in self._delay_pre_edges:
+        for vertex in self._new_vertices:
+            app_graph.add_vertex(vertex)
+        for edge in self._new_edges:
             app_graph.add_edge(edge, constants.SPIKE_PARTITION_ID)
 
     def _create_post_delay_edge(self, delay_app_vertex, app_edge):
@@ -135,12 +136,13 @@ class _DelaySupportAdder(object):
                 undelayed_edge=app_edge)
             self._delay_post_edge_map[
                 (delay_app_vertex, app_edge.post_vertex)] = delay_edge
+            self._new_edges.append(delay_edge)
             app_edge.delay_edge = delay_edge
             delay_app_vertex.add_outgoing_edge(delay_edge)
 
     def _create_delay_app_vertex_and_pre_edge(
             self, app_outgoing_edge_partition, app_edge, delay_per_stage,
-            app_graph, n_delay_stages):
+            n_delay_stages):
         """ creates the delay extension app vertex and the edge from the src\
             vertex to this delay extension. Adds to the graph, as safe to do\
             so.
@@ -150,7 +152,6 @@ class _DelaySupportAdder(object):
         :param AppEdge app_edge: the undelayed app edge.
         :param int delay_per_stage: delay for each delay stage
         :param int n_delay_stages: the number of delay stages needed
-        :param ApplicationGraph app_graph: the app graph.
         :return: the DelayExtensionAppVertex
         """
 
@@ -166,7 +167,7 @@ class _DelaySupportAdder(object):
 
             # set trackers
             delay_app_vertex.splitter = SplitterDelayVertexSlice()
-            app_graph.add_vertex(delay_app_vertex)
+            self._new_vertices.append(delay_app_vertex)
             self._app_to_delay_map[app_outgoing_edge_partition] = (
                 delay_app_vertex)
 
@@ -175,7 +176,7 @@ class _DelaySupportAdder(object):
                 app_edge.pre_vertex, delay_app_vertex,
                 label="{}_to_DelayExtension".format(
                     app_edge.pre_vertex.label))
-            self._delay_pre_edges.append(delay_pre_edge)
+            self._new_edges.append(delay_pre_edge)
         else:
             delay_app_vertex.set_new_n_delay_stages_and_delay_per_stage(
                 n_delay_stages, delay_per_stage)
