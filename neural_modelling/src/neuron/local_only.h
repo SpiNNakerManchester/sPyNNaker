84acbdaf5a054464e1dd8aef01d7694d588ad847
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2022-09-07 16:18:36 +0100

    Copyright date

--- neural_modelling/src/neuron/local_only.h
+++ neural_modelling/src/neuron/local_only.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 The University of Manchester
+ * Copyright (c) 2021-2022 The University of Manchester
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- spynnaker/pyNN/models/neuron/synapse_dynamics/abstract_supports_signed_weights.py
+++ spynnaker/pyNN/models/neuron/synapse_dynamics/abstract_supports_signed_weights.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2021 The University of Manchester
+# Copyright (c) 2021-2022 The University of Manchester
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
