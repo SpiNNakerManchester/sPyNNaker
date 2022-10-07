6c2f1ee579c0e85fe3959bcc018d69eb4693642d
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2022-10-07 07:51:52 +0100

    Move timer to avoid circular dependency

--- spynnaker/pyNN/spinnaker.py
+++ spynnaker/pyNN/spinnaker.py
@@ -29,8 +29,7 @@ from spinn_utilities.overrides import overrides
 from spinn_front_end_common.interface.abstract_spinnaker_base import (
     AbstractSpinnakerBase)
 from spinn_front_end_common.interface.provenance import (
-    LOADING, MAPPING, ProvenanceWriter, RUN_LOOP)
-from spinn_front_end_common.data import FecTimer
+    LOADING, MAPPING, ProvenanceWriter, FecTimer, RUN_LOOP)
 from spinn_front_end_common.utilities.constants import (
     MICRO_TO_MILLISECOND_CONVERSION)
 from spinn_front_end_common.utilities.exceptions import ConfigurationException
