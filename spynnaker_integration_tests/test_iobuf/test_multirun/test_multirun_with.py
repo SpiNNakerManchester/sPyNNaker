421af8c3d54b99a87ead1f49b48e36fa503a05d4
Christian <christian.brenninkmeijer@manchester.ac.uk>
2022-10-12 11:45:26 +0200

    update tests that after soft reset new app_provenance_data

--- spynnaker_integration_tests/test_iobuf/test_multirun/test_multirun_with.py
+++ spynnaker_integration_tests/test_iobuf/test_multirun/test_multirun_with.py
@@ -30,46 +30,63 @@ class TestIobuffMultirun(BaseTestCase):
 
     def do_run(self):
         sim.setup(timestep=1.0, min_delay=1.0)
-        prov_path = SpynnakerDataView.get_app_provenance_dir_path()
+        prov_path1 = SpynnakerDataView.get_app_provenance_dir_path()
         pop = sim.Population(10, sim.IF_curr_exp(), label='pop_1')
         sim.run(50)
 
         machine_verts = list(pop._vertex.machine_vertices)
         placement = SpynnakerDataView.get_placement_of_vertex(machine_verts[0])
 
-        size1 = self.check_size(prov_path, placement)
+        size1 = self.check_size(prov_path1, placement)
         sim.run(50)
-        size2 = self.check_size(prov_path, placement)
+        prov_path2 = SpynnakerDataView.get_app_provenance_dir_path()
+        self.assertEqual(prov_path2, prov_path1)
+        size2 = self.check_size(prov_path2, placement)
         self.assertGreater(size2, size1)
         sim.run(50)
-        size3 = self.check_size(prov_path, placement)
+        prov_path3 = SpynnakerDataView.get_app_provenance_dir_path()
+        self.assertEqual(prov_path3, prov_path1)
+        size3 = self.check_size(prov_path3, placement)
         self.assertGreater(size3, size2)
 
-        # Soft reset so same provenance
+        # Soft reset new provenance but not full reset
         sim.reset()
         sim.run(50)
-        size4 = self.check_size(prov_path, placement)
-        self.assertGreater(size4, size3)
+        prov_path4 = SpynnakerDataView.get_app_provenance_dir_path()
+        self.assertNotEqual(prov_path4, prov_path1)
+        size4 = self.check_size(prov_path4, placement)
+        # Less as startup after soft reset different than original
+        self.assertLess(size4, size1)
         sim.run(50)
-        size5 = self.check_size(prov_path, placement)
+        prov_path5 = SpynnakerDataView.get_app_provenance_dir_path()
+        self.assertEqual(prov_path4, prov_path5)
+        size5 = self.check_size(prov_path5, placement)
         self.assertGreater(size5, size4)
 
         # hard reset so new provenance
         sim.reset()
         sim.Population(10, sim.IF_curr_exp(), label='pop_1')
         sim.run(50)
-        prov_patha = SpynnakerDataView.get_app_provenance_dir_path()
-        self.assertNotEqual(prov_path, prov_patha)
-        size6 = self.check_size(prov_patha, placement)
-        # Should write the same thing again
+        prov_path6 = SpynnakerDataView.get_app_provenance_dir_path()
+        self.assertNotEqual(prov_path6, prov_path1)
+        self.assertNotEqual(prov_path6, prov_path4)
+        machine_verts = list(pop._vertex.machine_vertices)
+        placement = SpynnakerDataView.get_placement_of_vertex(machine_verts[0])
+        size6 = self.check_size(prov_path6, placement)
+        # Should write the same thing as original again
         self.assertEqual(size1, size6)
         sim.end()
 
         # Should not add anything on end.
-        size7 = self.check_size(prov_path, placement)
-        self.assertEqual(size5, size7)
-        size8 = self.check_size(prov_patha, placement)
-        self.assertEqual(size8, size6)
+        # Before reset
+        size3a = self.check_size(prov_path3, placement)
+        self.assertEqual(size3, size3a)
+        # Soft reset
+        size5a = self.check_size(prov_path5, placement)
+        self.assertEqual(size5a, size5)
+        # Hard reset
+        size6a = self.check_size(prov_path6, placement)
+        self.assertEqual(size6a, size6)
 
     def test_do_run(self):
         self.runsafe(self.do_run)
