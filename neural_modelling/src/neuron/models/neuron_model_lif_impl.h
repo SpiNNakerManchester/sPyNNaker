1204c5576c9f569fcae5e0cfe9787870ce84caa8
Andrew Gait <andrew.gait@manchester.ac.uk>
2022-12-01 17:47:38 +0000

    Seems like this UFRACT is the culprit

--- neural_modelling/src/neuron/models/neuron_model_lif_impl.h
+++ neural_modelling/src/neuron/models/neuron_model_lif_impl.h
@@ -68,7 +68,7 @@ struct neuron_t {
     //! 'fixed' computation parameter - time constant multiplier for
     //! closed-form solution
     //! exp(-(machine time step in ms)/(R * C)) [.]
-    UFRACT   exp_TC;
+    REAL     exp_TC;
 
     //! offset current [nA]
     REAL     I_offset;
