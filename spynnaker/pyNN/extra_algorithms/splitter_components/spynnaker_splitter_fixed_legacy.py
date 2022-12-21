9d6ccd8aa8efe28682b500f876549f430471abf4
Christian <christian.brenninkmeijer@manchester.ac.uk>
2022-12-21 11:24:46 +0100

    removed "Useless super() delegation in method '__init__'"

--- spynnaker/pyNN/extra_algorithms/splitter_components/spynnaker_splitter_fixed_legacy.py
+++ spynnaker/pyNN/extra_algorithms/splitter_components/spynnaker_splitter_fixed_legacy.py
@@ -19,6 +19,3 @@ from .abstract_spynnaker_splitter_delay import AbstractSpynnakerSplitterDelay
 
 class SpynnakerSplitterFixedLegacy(
         SplitterFixedLegacy, AbstractSpynnakerSplitterDelay):
-
-    def __init__(self):
-        super().__init__()
