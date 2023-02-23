99a7bf84b8c7c39efd74811d8bd252de0ab62c85
Christian <christian.brenninkmeijer@manchester.ac.uk>
2023-02-23 13:33:02 +0000

    fix debug

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -448,5 +448,5 @@ for semantic_sugar_file in semantic_sugar_files:
     with open(semantic_sugar_file, "a",  encoding="utf-8") as f:
         f.write(noindex_line)
     print(semantic_sugar_file)
+    with open(semantic_sugar_file, 'r') as f:
         print(f.read())
-        with open('filename.txt') as f:
