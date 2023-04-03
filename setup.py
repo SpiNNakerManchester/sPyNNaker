b7e9bb5ee33bce6b619472d61e3027c857732d03
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2023-04-03 14:15:17 +0100

    cleanup setup

--- setup.py
+++ setup.py
@@ -12,6 +12,22 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import distutils.dir_util
 from setuptools import setup
+import os
+import sys
+
+
 if __name__ == '__main__':
+    # Repeated installs assume files have not changed
+    # https://github.com/pypa/setuptools/issues/3236
+    if len(sys.argv) > 0 and sys.argv[1] == 'egg_info':
+        # on the first call to setpy.py remove files left by previous install
+        this_dir = os.path.dirname(os.path.abspath(__file__))
+        build_dir = os.path.join(this_dir, "build")
+        if os.path.isdir(build_dir):
+            distutils.dir_util.remove_tree(build_dir)
+        egg_dir = os.path.join(this_dir, "sPyNNaker.egg-info")
+        if os.path.isdir(egg_dir):
+            distutils.dir_util.remove_tree(egg_dir)
     setup()
