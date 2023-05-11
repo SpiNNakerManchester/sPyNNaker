5b8fa99f6e846452c6597e4416dc8151a34fa921
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2023-05-11 06:11:04 +0100

    refactor reset_num_str as load_run_str

--- spynnaker_integration_tests/test_debug_mode/check_debug.py
+++ spynnaker_integration_tests/test_debug_mode/check_debug.py
@@ -42,13 +42,30 @@ import pyNN.spiNNaker as sim
 
 class CheckDebug(BaseTestCase):
 
-    def _reports(self, reset_number):
+    def _load_reports(self, run_number):
+        return [
+            reports_names._C_ROUTING_TABLE_DIR.format(run_number),
+            reports_names._COMPARED_FILENAME.format(run_number),
+            # write_routing_tables_from_machine_report
+            routing_tables_from_machine_report.format(run_number),
+            # write_memory_map_report
+            memory_map_on_host_report.format(run_number),
+            "ds{}.sqlite3".format(run_number),
+        ]
+
+    def _run_reports(self, run_number):
+        return [
+            # write_drift_report_end or start
+            CLOCK_DRIFT_REPORT.format(run_number),
+            "data{}.sqlite3".format(run_number),
+            ]
+
+    """
+    that it does not crash in debug mode. All reports on.
+    """
+    def debug(self):
+        # pylint: disable=protected-access
         reports = [
-            # write_energy_report
-            # EnergyReport._DETAILED_FILENAME,
-            # EnergyReport._SUMMARY_FILENAME,
-            # write_text_specs = False
-            "data_spec_text_files",
             # write_router_reports
             reports_names._ROUTING_FILENAME,
             # write_partitioner_reports
@@ -62,55 +79,44 @@ class CheckDebug(BaseTestCase):
             reports_names._VIRTKEY_FILENAME,
             # write_routing_table_reports
             reports_names._ROUTING_TABLE_DIR,
-            reports_names._C_ROUTING_TABLE_DIR.format(reset_number),
-            reports_names._COMPARED_FILENAME.format(reset_number),
             # write_routing_tables_from_machine_report
-            routing_tables_from_machine_report.format(reset_number),
-            # write_memory_map_report
-            memory_map_on_host_report.format(reset_number),
             # write_network_specification_report
             network_specification_file_name,
             # write_provenance_data
             "provenance_data",
             # write_tag_allocation_reports
             reports_names._TAGS_FILENAME,
-            # write_drift_report_end or start
-            CLOCK_DRIFT_REPORT.format(reset_number),
             # write_board_chip_report
             AREA_CODE_REPORT_NAME,
             _GRAPH_NAME,
             _GRAPH_NAME + "." +
             _GRAPH_FORMAT,
             ]
+
+        sim.setup(1.0)
         if (get_config_bool("Machine", "enable_advanced_monitor_support")
                 and not get_config_bool("Java", "use_java")):
             # write_data_speed_up_report
             reports.append(
                 DataSpeedUpPacketGatherMachineVertex.OUT_REPORT_NAME)
             reports.append(DataSpeedUpPacketGatherMachineVertex.IN_REPORT_NAME)
-        return reports
 
-    """
-    that it does not crash in debug mode. All reports on.
-    """
-    def debug(self):
-        # pylint: disable=protected-access
-
-        sim.setup(1.0)
         pop = sim.Population(100, sim.IF_curr_exp, {}, label="pop")
         pop.record("v")
         inp = sim.Population(1, sim.SpikeSourceArray(
             spike_times=[0]), label="input")
         sim.Projection(inp, pop, sim.AllToAllConnector(),
                        synapse_type=sim.StaticSynapse(weight=5))
-        sim.run(0)
+        sim.run(0)  # first run
         pop.get_data("v")
         run0 = SpynnakerDataView.get_run_dir_path()
         found = os.listdir(run0)
-        for report in self._reports(""):
+        for report in reports:
             self.assertIn(report, found)
-        self.assertIn("data.sqlite3", found)
-        self.assertIn("ds.sqlite3", found)
+        for report in self._load_reports(""):
+            self.assertIn(report, found)
+        for report in self._run_reports(""):
+            self.assertIn(report, found)
 
         sim.run(10)  # second run
         pop.get_data("v")
@@ -120,19 +126,21 @@ class CheckDebug(BaseTestCase):
         sim.reset()  # Soft
         # check get works directly after a reset
         pop.get_data("v")
-        sim.run(10)
+        sim.run(10)  # third run
         found = os.listdir(SpynnakerDataView.get_run_dir_path())
-        self.assertIn("data1.sqlite3", found)
-        self.assertNotIn("ds1.sqlite3", found)
+        for report in self._run_reports("3"):
+            self.assertIn(report, found)
 
         sim.reset()  # soft with dsg
         SpynnakerDataView.set_requires_data_generation()
-        sim.run(10)
+        sim.run(10)  # fourth run
         pop.get_data("v")
         self.assertEqual(run0, SpynnakerDataView.get_run_dir_path())
         found = os.listdir(run0)
-        self.assertIn("data2.sqlite3", found)
-        self.assertIn("ds2.sqlite3", found)
+        for report in self._load_reports("4"):
+            self.assertIn(report, found)
+        for report in self._run_reports("4"):
+            self.assertIn(report, found)
         # No point in checking files they are already there
 
         sim.reset()  # hard
@@ -141,9 +149,11 @@ class CheckDebug(BaseTestCase):
         pop.get_data("v")
         self.assertNotEqual(run0, SpynnakerDataView.get_run_dir_path())
         found = os.listdir(SpynnakerDataView.get_run_dir_path())
-        for report in self._reports("3"):
+        for report in reports:
             self.assertIn(report, found)
-        self.assertIn("data3.sqlite3", found)
-        self.assertIn("ds3.sqlite3", found)
+        for report in self._load_reports(""):
+            self.assertIn(report, found)
+        for report in self._run_reports(""):
+            self.assertIn(report, found)
 
         sim.end()
