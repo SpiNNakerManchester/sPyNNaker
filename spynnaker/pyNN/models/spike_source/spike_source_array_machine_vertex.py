261d6b3aa9a4f1035e4c7cbe2a338c8bf30560a4
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2023-05-24 09:04:47 +0100

    Flake8

--- spynnaker/pyNN/models/spike_source/spike_source_array_machine_vertex.py
+++ spynnaker/pyNN/models/spike_source/spike_source_array_machine_vertex.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import numpy
 from spinn_utilities.overrides import overrides
 from pacman.utilities.utility_calls import get_keys
 from spinn_front_end_common.utility_models import (
