bc6a99f4e8269124d0ad7878cfe4b6576b9031cf
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2023-05-25 12:11:07 +0100

    debug2

--- spynnaker/pyNN/models/neural_projections/connectors/from_list_connector.py
+++ spynnaker/pyNN/models/neural_projections/connectors/from_list_connector.py
@@ -146,8 +146,12 @@ class FromListConnector(AbstractConnector, AbstractGenerateConnectorOnHost):
         sort_indices = numpy.argsort(post_indices)
 
         # Split the sort order in to groups of connection indices
+        print(sort_indices)
+        print(index_count)
         a = numpy.cumsum(index_count)
+        print(a)
         b = numpy.split(sort_indices, numpy.cumsum(index_count))
+        print(b)
         c = numpy.array(numpy.split(sort_indices, numpy.cumsum(index_count)))
         split_indices = numpy.array(numpy.split(
             sort_indices, numpy.cumsum(index_count)))[:-1]
