3de286914a8555a9fe7a64b2537fb97e5ae3acd8
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2023-06-21 11:08:16 +0100

    renamed sdg_target ds_database

--- unittests/model_tests/neuron/test_synaptic_manager.py
+++ unittests/model_tests/neuron/test_synaptic_manager.py
@@ -29,7 +29,7 @@ from pacman.operations.partition_algorithms import splitter_partitioner
 from spinn_front_end_common.interface.ds import (
     DataSpecificationGenerator, DsSqlliteDatabase)
 from spinn_front_end_common.interface.interface_functions import (
-    execute_application_data_specs)
+    load_application_data_specs)
 from spynnaker.pyNN.data.spynnaker_data_writer import SpynnakerDataWriter
 from spynnaker.pyNN.models.neuron.synaptic_matrices import SynapticMatrices,\
     SynapseRegions
@@ -165,10 +165,10 @@ def test_write_data_spec():
     ds_db = DsSqlliteDatabase()
     spec = DataSpecificationGenerator(0, 0, 3, post_vertex, ds_db)
     synaptic_matrices.write_synaptic_data(spec, post_vertex_slice, references)
-    writer.set_dsg_targets(ds_db)
+    writer.set_ds_database(ds_db)
 
     writer.set_transceiver(_MockTransceiverinOut())
-    execute_application_data_specs()
+    load_application_data_specs()
 
     report_folder = mkdtemp()
     try:
