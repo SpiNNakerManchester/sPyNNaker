7a6771ed0dde05a7fb040b42025c037a2e51663d
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2023-07-07 16:33:50 +0100

    remove unused import

--- unittests/model_tests/neuron/test_synaptic_manager.py
+++ unittests/model_tests/neuron/test_synaptic_manager.py
@@ -19,7 +19,7 @@ import numpy
 import pytest
 
 from spinn_utilities.overrides import overrides
-from spinn_utilities.config_holder import load_config, set_config
+from spinn_utilities.config_holder import set_config
 from spinnman.model import CPUInfo
 from spinnman.transceiver import Transceiver
 from pacman.model.placements import Placement
