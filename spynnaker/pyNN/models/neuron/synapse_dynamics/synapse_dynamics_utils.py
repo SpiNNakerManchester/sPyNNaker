6cfe9e56100a9d046d79bf0a16e8e05b6c10684b
Andrew Gait <andrew.gait@manchester.ac.uk>
2023-07-26 16:15:15 +0100

    flake8

--- spynnaker/pyNN/models/neuron/synapse_dynamics/synapse_dynamics_utils.py
+++ spynnaker/pyNN/models/neuron/synapse_dynamics/synapse_dynamics_utils.py
@@ -65,7 +65,7 @@ def calculate_spike_pair_additive_stdp_weight(
     depressions = a_minus * numpy.exp(
         (depression_times / tau_minus))
 
-    print("Potentiations: ", potentation_times, potentiations)
+    print("Potentiations: ", potentiation_times, potentiations)
     print("Depressions: ", depression_times, depressions)
     return initial_weight + numpy.sum(potentiations) - numpy.sum(depressions)
 
