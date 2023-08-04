8fec3243660b52b4646f4aba9fdf5f72c0f5d2f9
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2023-08-04 16:35:24 +0100

    flake8

--- unittests/model_tests/neuron/test_synaptic_manager.py
+++ unittests/model_tests/neuron/test_synaptic_manager.py
@@ -32,8 +32,8 @@ from spinn_front_end_common.interface.ds import (
 from spinn_front_end_common.interface.interface_functions import (
     load_application_data_specs)
 from spynnaker.pyNN.data.spynnaker_data_writer import SpynnakerDataWriter
-from spynnaker.pyNN.models.neuron.synaptic_matrices import SynapticMatrices,\
-    SynapseRegions
+from spynnaker.pyNN.models.neuron.synaptic_matrices import (
+    SynapticMatrices, SynapseRegions)
 from spynnaker.pyNN.models.neuron.synapse_dynamics import (
     SynapseDynamicsStatic, SynapseDynamicsStructuralSTDP,
     SynapseDynamicsSTDP, SynapseDynamicsStructuralStatic,
@@ -90,7 +90,6 @@ class _MockTransceiverinOut(MockableTransceiver):
     def read_word(self, x, y, base_address, cpu=0):
         datum, = struct.unpack("<I", self.read_memory(x, y, base_address, 4))
         return datum
-
 
 
 def say_false(self, weights, delays):
