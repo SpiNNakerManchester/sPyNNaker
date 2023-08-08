2b09447313c4dafca0188c91cb7a88906ebc9210
Donal K. Fellows <donal.k.fellows@manchester.ac.uk>
2023-08-08 16:05:05 +0100

    Ick

--- unittests/model_tests/neuron/test_synaptic_manager.py
+++ unittests/model_tests/neuron/test_synaptic_manager.py
@@ -20,8 +20,7 @@ import pytest
 
 from spinn_utilities.overrides import overrides
 from spinn_utilities.config_holder import set_config
-from spinnman.model import CPUInfo
-from spinnman.processes.get_cpu_info_process import _INFO_PATTERN
+from spinnman.model.cpu_info import CPUInfo, _VCPU_PATTERN
 from spinnman.transceiver import Transceiver
 from pacman.model.placements import Placement
 from pacman.operations.routing_info_allocator_algorithms import (
@@ -81,7 +80,7 @@ class _MockTransceiverinOut(Transceiver):
     @overrides(Transceiver.get_cpu_information_from_core)
     def get_cpu_information_from_core(self, x, y, p):
         bs = bytearray(128)
-        cpu_data = _INFO_PATTERN.unpack_from(bytes(bs), 0)
+        cpu_data = _VCPU_PATTERN.unpack_from(bytes(bs), 0)
         return CPUInfo(x=1, y=2, p=3, cpu_data=cpu_data)
 
     @overrides(Transceiver.read_memory)
