33ed7b7838bf3b00d3fb67d32c2747754c7b2d21
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2023-08-17 10:39:16 +0100

    get_region_base_address

--- unittests/model_tests/neuron/test_synaptic_manager.py
+++ unittests/model_tests/neuron/test_synaptic_manager.py
@@ -78,11 +78,9 @@ class _MockTransceiverinOut(Transceiver):
             data = struct.Struct("<I").pack(data)
         self._data_to_read[base_address:base_address + len(data)] = data
 
-    @overrides(Transceiver.get_cpu_information_from_core)
-    def get_cpu_information_from_core(self, x, y, p):
-        bs = bytearray(128)
-        cpu_data = _INFO_PATTERN.unpack_from(bytes(bs), 0)
-        return CPUInfo(x=1, y=2, p=3, cpu_data=cpu_data)
+    @overrides(Transceiver.get_region_base_address)
+    def get_region_base_address(self, x, y, p):
+        return 0
 
     @overrides(Transceiver.read_memory)
     def read_memory(self, x, y, base_address, length, cpu=0):
