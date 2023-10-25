537e67a1aeebb3e9bcf1b605824989b2c354437d
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2023-10-25 15:09:10 +0100

    remove unused import

--- spynnaker/pyNN/models/abstract_models/receives_synaptic_inputs_over_sdram.py
+++ spynnaker/pyNN/models/abstract_models/receives_synaptic_inputs_over_sdram.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from six import add_metaclass
-from typing import TYPE_CHECKING
 from spinn_utilities.abstract_base import (
     AbstractBase, abstractmethod, abstractproperty)
 from spinn_utilities.overrides import overrides
--- spynnaker/pyNN/models/abstract_models/sends_synaptic_inputs_over_sdram.py
+++ spynnaker/pyNN/models/abstract_models/sends_synaptic_inputs_over_sdram.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import TYPE_CHECKING
 from spinn_utilities.abstract_base import abstractmethod
 from spinn_utilities.overrides import overrides
 from pacman.model.graphs import AbstractSupportsSDRAMEdges
