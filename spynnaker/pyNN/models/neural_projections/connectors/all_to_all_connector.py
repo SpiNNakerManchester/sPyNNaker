4c6074036102b6dd60d34f9528a9f2447a536b4a
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2023-11-03 07:25:21 +0000

    Fix self connection here too

--- spynnaker/pyNN/models/neural_projections/connectors/all_to_all_connector.py
+++ spynnaker/pyNN/models/neural_projections/connectors/all_to_all_connector.py
@@ -157,8 +157,9 @@ class AllToAllConnector(AbstractGenerateConnectorOnMachine,
 
     @overrides(AbstractGenerateConnectorOnMachine.gen_connector_params)
     def gen_connector_params(self):
-        return numpy.array([
-            int(self.__allow_self_connections)], dtype="uint32")
+        allow_self = (self.__allow_self_connections or
+            synapse_info.pre_population != synapse_info.post_population)
+        return numpy.array([int(allow_self)], dtype="uint32")
 
     @property
     @overrides(
