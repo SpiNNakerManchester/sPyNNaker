2f74f4c82dfc1d4a8e564b2fb9d28e8ae958d7ba
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2023-11-30 14:34:18 +0000

    Try with a timeout and hope it works!

--- test_whole_board/test_whole_board.py
+++ test_whole_board/test_whole_board.py
@@ -207,7 +207,7 @@ def test_run(x, y, b):
         triad=(x, y, b), machine_name=SPALLOC_MACHINE)
     with job:
         job.launch_keepalive_task()
-        job.wait_until_ready(None, 3)
+        job.wait_until_ready(31, 3)
         state = job.get_state()
         # If queued or destroyed skip test
         if state == SpallocState.QUEUED:
