c49c1fb1c10df58c307a5c7ffd902fce57765164
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2024-03-14 10:54:06 +0000

    Add TSP evaluator vertex

--- spynnaker/pyNN/extra_models/__init__.py
+++ spynnaker/pyNN/extra_models/__init__.py
@@ -31,6 +31,7 @@ from spynnaker.pyNN.models.neuron.builds import (
     IFCurrExpSEMDBase as IF_curr_exp_sEMD,
     IFCurrDeltaCa2Adaptive, StocExp, StocExpStable,  StocSigma, IFTruncDelta,
     IFCurrDeltaFixedProb)
+from spynnaker.pyNN.models.utility_models.tsp_eval import TSPEvalVertex
 
 # Variable rate poisson
 from spynnaker.pyNN.models.spike_source import SpikeSourcePoissonVariable
@@ -57,5 +58,5 @@ __all__ = [
     'StocExp', 'StocExpStable', 'StocSigma', 'IFCurrDeltaFixedProb',
 
     # Special
-    'IFTruncDelta'
+    'IFTruncDelta', 'TSPEvalVertex'
     ]
