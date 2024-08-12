be69a7b92495c7e23e197182b8b4ceee3c333662
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2024-08-12 12:59:16 +0100

    info for release 7.3.0

--- spynnaker/_version.py
+++ spynnaker/_version.py
@@ -12,10 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "1!7.1.1"
+__version__ = "1!7.3.0"
 __version_month__ = "TBD"
 __version_year__ = "TBD"
 __version_day__ = "TBD"
-__version_name__ = "Post_HBP"
+__version_name__ = "TBD"
 
 _NAME = f"sPyNNaker_under_version({__version__}-{__version_name__})"
