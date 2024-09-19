96578f5925d7b65d8ec6b353da11876d2899aa1a
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2024-09-19 14:58:15 +0100

    remove error that should be unreachable

--- spynnaker/pyNN/utilities/neo_buffer_database.py
+++ spynnaker/pyNN/utilities/neo_buffer_database.py
@@ -188,11 +188,6 @@ class NeoBufferDatabase(BufferDatabase, NeoCsv):
                 t_stop = row[self._T_STOP]
             return (row[self._SEGMENT_NUMBER], time, t_stop, row[self._DT],
                     self._string(row[self._SIMULATOR]))
-        if (SpynnakerDataView.is_shutdown() and
-                not get_config_bool("Reports","keep_data_database")):
-            raise ConfigurationException(
-                "Unable to get data after shutdown as "
-                "cfg:Reports:keep_data_database is false")
         raise ConfigurationException(
             "No recorded data. Did the simulation run?")
 
