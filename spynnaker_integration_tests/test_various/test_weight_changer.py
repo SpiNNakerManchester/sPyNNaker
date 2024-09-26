b77f61bf3f3e3d14bd65fc3a913886a2b0ab9f14
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2024-09-26 12:11:12 +0100

    Better tests

--- spynnaker_integration_tests/test_various/test_weight_changer.py
+++ spynnaker_integration_tests/test_various/test_weight_changer.py
@@ -13,61 +13,167 @@
 # limitations under the License.
 
 import pyNN.spiNNaker as sim
+import numpy
 
 
-def test_weight_changer():
+def test_weight_changer_limits():
     sim.setup(timestep=1.0)
+    n_neurons = 5
+    n_changes = 6
+    time_diff = 4
+    n_cycles = n_neurons * time_diff
 
-    # Set up so that there is a pre-spike after each change and that each
+    # Set up so that there is a pre-spike after each change set and that each
     # change happens to each neuron separately
-    pre_times = [[j * 5 + i + 0.25 for j in range(5)] for i in range(1, 6)]
-    change_times = [[j * 5 + i for j in range(5)] for i in range(1, 6)]
+    changes_1 = [[j * n_cycles + i * time_diff + 0
+                  for j in range(n_changes)] for i in range(n_neurons)]
+    changes_2 = [[j * n_cycles + i * time_diff + 1
+                  for j in range(n_changes)] for i in range(n_neurons)]
+    pre_times = [[j * n_cycles + i * time_diff + 2
+                  for j in range(n_changes)] for i in range(n_neurons)]
 
-    print(change_times)
+    print(changes_1)
+    print(changes_2)
+    print(pre_times)
 
     pre = sim.Population(5, sim.SpikeSourceArray(spike_times=pre_times))
-    post_1 = sim.Population(5, sim.IF_curr_exp())
-    post_2 = sim.Population(5, sim.IF_curr_exp())
-    changer = sim.Population(5, sim.SpikeSourceArray(spike_times=change_times))
+    post = sim.Population(5, sim.IF_curr_exp())
+    changer_1 = sim.Population(
+        5, sim.SpikeSourceArray(spike_times=changes_1))
+    changer_2 = sim.Population(
+        5, sim.SpikeSourceArray(spike_times=changes_2))
 
     changable_proj_1 = sim.Projection(
-        pre, post_1, sim.OneToOneConnector(),
-        sim.extra_models.WeightChangeable(1.0, 3.0, weight=2.0, delay=1.0))
+        pre, post, sim.OneToOneConnector(),
+        sim.extra_models.WeightChangeable(0.25, 4.5, weight=2.0, delay=1.0))
     changable_proj_2 = sim.Projection(
-        pre, post_2, sim.OneToOneConnector(),
-        sim.extra_models.WeightChangeable(0.25, 2.0, weight=1.0, delay=1.0))
+        pre, post, sim.OneToOneConnector(),
+        sim.extra_models.WeightChangeable(0.25, 4.5, weight=1.5, delay=1.0))
 
     change_proj_1 = sim.Projection(
-        changer, post_1, sim.OneToOneConnector(),
+        changer_1, post, sim.OneToOneConnector(),
         sim.extra_models.WeightChanger(
             weight_change=0.5, projection=changable_proj_1))
     change_proj_2 = sim.Projection(
-        changer, post_2, sim.OneToOneConnector(),
+        changer_2, post, sim.OneToOneConnector(),
         sim.extra_models.WeightChanger(
             weight_change=-0.25, projection=changable_proj_2))
 
     weights_1 = []
     weights_2 = []
 
-    sim.run(0.5)
+    sim.run(0)
 
     print(change_proj_1.get('weight', 'list'))
     print(change_proj_2.get('weight', 'list'))
 
-    for i in range(26):
-        sim.run(1)
+    # Get weights at start
+    weights_1.append(
+        changable_proj_1.get('weight', 'list', with_address=False))
+    weights_2.append(
+        changable_proj_2.get('weight', 'list', with_address=False))
 
-        weights_1.append(changable_proj_1.get('weight', 'list'))
-        weights_2.append(changable_proj_2.get('weight', 'list'))
+    # Do initial run to get past first changes and pre-spike
+    sim.run(time_diff - 1)
+
+    # Do the rest of the runs (already done one)
+    for i in range((n_neurons * n_changes) - 1):
+        weights_1.append(
+            changable_proj_1.get('weight', 'list', with_address=False))
+        weights_2.append(
+            changable_proj_2.get('weight', 'list', with_address=False))
+
+        sim.run(time_diff)
+
+    # Get final weights
+    weights_1.append(
+        changable_proj_1.get('weight', 'list', with_address=False))
+    weights_2.append(
+        changable_proj_2.get('weight', 'list', with_address=False))
 
     sim.end()
 
+    next_weights = [2.0 for _ in range(n_neurons)]
+    change = 0
     for w in weights_1:
-        print(w)
-    print()
+        assert numpy.array_equal(w, next_weights)
+        if next_weights[change] < 4.5:
+            next_weights[change] += 0.5
+        change = (change + 1) % n_neurons
+
+    next_weights = [1.5 for _ in range(n_neurons)]
+    change = 0
     for w in weights_2:
-        print(w)
+        assert numpy.array_equal(w, next_weights)
+        if next_weights[change] > 0.25:
+            next_weights[change] -= 0.25
+        change = (change + 1) % n_neurons
+
+
+def test_weight_changer_diffs():
+    sim.setup(timestep=1.0)
+
+    # Pre spikes at 5ms intervals
+    pre_times = [5, 10, 15]
+    # For change 1, do one in segment 1, none in segment 2, two in segment 3
+    changes_1 = [1, 12, 13]
+    # For change 2, do none in segment 1, two in segment 2, one in segment 3
+    changes_2 = [7, 8, 12]
+
+    pre = sim.Population(1, sim.SpikeSourceArray(spike_times=pre_times))
+    post = sim.Population(1, sim.IF_curr_exp())
+    changer_1 = sim.Population(
+        1, sim.SpikeSourceArray(spike_times=changes_1))
+    changer_2 = sim.Population(
+        1, sim.SpikeSourceArray(spike_times=changes_2))
+
+    changable_proj_1 = sim.Projection(
+        pre, post, sim.OneToOneConnector(),
+        sim.extra_models.WeightChangeable(0, 5, weight=2.5, delay=1.0))
+    changable_proj_2 = sim.Projection(
+        pre, post, sim.OneToOneConnector(),
+        sim.extra_models.WeightChangeable(0, 5, weight=3.0, delay=1.0),
+        receptor_type='inhibitory')
+
+    change_proj_1 = sim.Projection(
+        changer_1, post, sim.OneToOneConnector(),
+        sim.extra_models.WeightChanger(
+            weight_change=-0.75, projection=changable_proj_1))
+    change_proj_2 = sim.Projection(
+        changer_2, post, sim.OneToOneConnector(),
+        sim.extra_models.WeightChanger(
+            weight_change=0.25, projection=changable_proj_2),
+        receptor_type='inhibitory')
+
+    weights_1 = []
+    weights_2 = []
+
+    sim.run(0)
+
+    print(change_proj_1.get('weight', 'list'))
+    print(change_proj_2.get('weight', 'list'))
+
+    # Get weights at start
+    weights_1.append(
+        changable_proj_1.get('weight', 'list', with_address=False)[0])
+    weights_2.append(
+        changable_proj_2.get('weight', 'list', with_address=False)[0])
+
+    last_time = 0
+    for pre_time in pre_times:
+        sim.run((pre_time - last_time) + 1)
+        last_time = pre_time + 1
+
+        weights_1.append(
+            changable_proj_1.get('weight', 'list', with_address=False)[0])
+        weights_2.append(
+            changable_proj_2.get('weight', 'list', with_address=False)[0])
+
+    sim.end()
+
+    assert numpy.array_equal(weights_1, [2.5, 1.75, 1.75, 0.25])
+    assert numpy.array_equal(weights_2, [3.0, 3.0, 3.50, 3.75])
 
 
 if __name__ == "__main__":
-    test_weight_changer()
+    test_weight_changer_diffs()
