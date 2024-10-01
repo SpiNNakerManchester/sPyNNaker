851c76f6dad70f5b7861bd3d7c4de201e1a6a04a
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2024-10-01 12:12:34 +0100

    info for release 7.3.0

--- spynnaker/pyNN/_version.py
+++ spynnaker/pyNN/_version.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "1!7.2.2"
-__version_month__ = "TBD"
-__version_year__ = "TBD"
-__version_day__ = "TBD"
-__version_name__ = "TO DO"
+__version__ = "1!7.3.0"
+__version_month__ = "October"
+__version_year__ = "2024"
+__version_day__ = "01"
+__version_name__ = "Control Surface"
