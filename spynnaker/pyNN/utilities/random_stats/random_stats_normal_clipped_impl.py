b0e9ee51424bd52968facae655d7cba3acc3fbb1
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2025-02-06 11:59:00 +0000

    typing

--- spynnaker/pyNN/utilities/random_stats/random_stats_normal_clipped_impl.py
+++ spynnaker/pyNN/utilities/random_stats/random_stats_normal_clipped_impl.py
@@ -12,7 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from typing import List, Optional
 from scipy.stats import truncnorm
+from spinn_utilities.overrides import overrides
+from pyNN.random import RandomDistribution
 from spynnaker.pyNN.utilities.random_stats import AbstractRandomStats
 
 
@@ -22,7 +25,7 @@ class RandomStatsNormalClippedImpl(AbstractRandomStats):
     are clipped to a boundary (redrawn).
     """
 
-    def _get_params(self, dist):
+    def _get_params(self, dist: RandomDistribution) -> List[float]:
         low = ((dist.parameters['low'] - dist.parameters['mu']) /
                dist.parameters['sigma'])
         high = ((dist.parameters['high'] - dist.parameters['mu']) /
@@ -30,23 +33,30 @@ class RandomStatsNormalClippedImpl(AbstractRandomStats):
         return [low, high,
                 dist.parameters['mu'], dist.parameters['sigma']]
 
-    def cdf(self, dist, v):
+    @overrides(AbstractRandomStats.cdf)
+    def cdf(self, dist: RandomDistribution, v: float) -> float:
         return truncnorm.cdf(v, *self._get_params(dist))
 
-    def ppf(self, dist, p):
+    @overrides(AbstractRandomStats.ppf)
+    def ppf(self, dist: RandomDistribution, p: float) -> float:
         return truncnorm.ppf(p, *self._get_params(dist))
 
-    def mean(self, dist):
+    @overrides(AbstractRandomStats.mean)
+    def mean(self, dist: RandomDistribution) -> float:
         return truncnorm.mean(*self._get_params(dist))
 
-    def std(self, dist):
+    @overrides(AbstractRandomStats.std)
+    def std(self, dist: RandomDistribution) -> float:
         return truncnorm.std(*self._get_params(dist))
 
-    def var(self, dist):
+    @overrides(AbstractRandomStats.var)
+    def var(self, dist: RandomDistribution) -> float:
         return truncnorm.var(*self._get_params(dist))
 
-    def high(self, distribution):
+    @overrides(AbstractRandomStats.high)
+    def high(self, distribution: RandomDistribution) -> Optional[float]:
         return distribution.parameters['high']
 
-    def low(self, distribution):
+    @overrides(AbstractRandomStats.low)
+    def low(self, distribution: RandomDistribution) -> Optional[float]:
         return distribution.parameters['low']
