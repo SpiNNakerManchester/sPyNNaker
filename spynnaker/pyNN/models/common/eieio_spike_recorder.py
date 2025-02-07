dbd8b804463914f5c1945d748e7f8133cb4304ec
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2025-02-07 10:29:50 +0000

    removed unused set_recording

--- spynnaker/pyNN/models/common/eieio_spike_recorder.py
+++ spynnaker/pyNN/models/common/eieio_spike_recorder.py
@@ -42,13 +42,3 @@ class EIEIOSpikeRecorder(object):
         Old method assumed to be spikes.
         """
         self.__record = bool(new_state)
-
-    def set_recording(self, new_state: bool, sampling_interval=None):
-        """
-        :param bool new_state:
-        :param None sampling_interval: not supported functionality
-        """
-        if sampling_interval is not None:
-            logger.warning("Sampling interval currently not supported for "
-                           "SpikeSourceArray so being ignored")
-        self.__record = bool(new_state)
