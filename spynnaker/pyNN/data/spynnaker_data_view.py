17d9af5cde583fcd22cdac5728497c7a8942a60d
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2025-04-03 10:37:30 +0100

    changing max atoms should trigger mapping

--- spynnaker/pyNN/data/spynnaker_data_view.py
+++ spynnaker/pyNN/data/spynnaker_data_view.py
@@ -251,6 +251,7 @@ class SpynnakerDataView(FecDataView):
 
         neuron_type.set_model_max_atoms_per_dimension_per_core(max_permitted)
         cls.__spy_data._neurons_per_core_set.add(neuron_type)
+        cls.set_requires_mapping()
 
     @classmethod
     def get_sim_name(cls) -> str:
