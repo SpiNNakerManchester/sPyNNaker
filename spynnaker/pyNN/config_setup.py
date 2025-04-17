3c3b857601ab27996d3c6c0eb554c570290ad81d
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2025-04-17 14:26:45 +0100

    print_configs

--- spynnaker/pyNN/config_setup.py
+++ spynnaker/pyNN/config_setup.py
@@ -16,7 +16,9 @@ import os
 from typing import Set
 
 from spinn_utilities.config_holder import (
-    clear_cfg_files, set_cfg_files, print_configs)
+    clear_cfg_files, get_default_cfgs, set_cfg_files)
+from spinn_utilities.configs.camel_case_config_parser import (
+    CamelCaseConfigParser, TypedConfigParser)
 from spinn_front_end_common.interface.config_setup import (
     add_default_cfg, add_spinnaker_cfg)
 from spinn_front_end_common.interface.config_setup import (
@@ -76,3 +78,40 @@ def cfg_paths_skipped() -> Set[str]:
     Assuming mode = Debug
     """
     return fec_cfg_paths_skipped()
+
+
+def print_configs() -> None:
+    """
+    To BE MOVED
+    :return:
+    """
+    config1 = CamelCaseConfigParser()
+    config1.read(get_default_cfgs())
+    for section in config1:
+        if section == "DEFAULT":
+            continue
+        print(section)
+        for option in config1.options(section):
+            if option.startswith("path_"):
+                continue
+            if option.startswith("@"):
+                continue
+            print("\t", option)
+            value = config1.get(section, option)
+            print("\t\tdefault:", value)
+            if option.startswith("write"):
+                path = "path" + option[5:]
+                if config1.has_option(section, path):
+                    value = config1.get(section, path)
+                    print("\t\tpath:", value)
+            comment = "@" + option
+            if config1.has_option(section, comment):
+                value = config1.get(section, comment)
+                value = value.replace("\n", "\n\t\t\t")
+                print("\t\t", value)
+
+
+
+if __name__ == '__main__':
+    setup_configs()
+    print_configs()
