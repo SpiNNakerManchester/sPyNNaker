9872ec5d189ecc58099e96e909e679de2f54222b
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2025-11-24 08:08:43 +0000

    Working local build

--- neural_modelling/make/neural_support.mk
+++ neural_modelling/make/neural_support.mk
@@ -1,7 +1,3 @@
-# THIS FILE WAS USED IN AN OLDER MAKE STYLE
-# Make now has to convert c files before building
-# SEE https://spinnakermanchester.github.io/latest/Makefiles.html
-
 # Copyright (c) 2017 The University of Manchester
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,4 +12,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(error inclusion of sPyNNaker/neural_modelling/src/paths.mk)
+# If FEC_INSTALL_DIR is not defined, this is an error!
+ifndef FEC_INSTALL_DIR
+    $(error FEC_INSTALL_DIR is not set.  Please define FEC_INSTALL_DIR (possibly by running "source setup" in the spinnaker package folder))
+endif
+
+MAKEFILE_PATH := $(abspath $(lastword $(MAKEFILE_LIST)))
+NEURAL_MODELLING_DIRS := $(abspath $(dir $(MAKEFILE_PATH))/../)/
+
+# Add the neural modelling src directory to the source directories
+SRC_DIR := $(NEURAL_MODELLING_DIRS)/src/
+SOURCE_DIRS += $(SRC_DIR)
+
+include $(FEC_INSTALL_DIR)/make/fec.mk
--- neural_modelling/makefiles/neural_support.mk
+++ neural_modelling/makefiles/neural_support.mk
@@ -12,32 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# If SPINN_DIRS is not defined, this is an error!
-ifndef SPINN_DIRS
-    $(error SPINN_DIRS is not set.  Please define SPINN_DIRS (possibly by running "source setup" in the spinnaker package folder))
-endif
-# If NEURAL_MODELLING_DIRS is not defined, this is an error!
-ifndef NEURAL_MODELLING_DIRS
-    $(error NEURAL_MODELLING_DIRS is not set.  Please define NEURAL_MODELLING_DIRS (possibly by running "source setup" in the sPyNNaker folder))
-endif
-#Check NEURAL_MODELLING_DIRS
+# Makefile for building neural modelling C code for sPyNNaker specifically
+
 MAKEFILE_PATH := $(abspath $(lastword $(MAKEFILE_LIST)))
-CHECK_PATH := $(NEURAL_MODELLING_DIRS)/makefiles/neural_support.mk
-ifneq ($(CHECK_PATH), $(MAKEFILE_PATH))
-    $(error Please check NEURAL_MODELLING_DIRS as based on that this file is at $(CHECK_PATH) when it is actually at $(MAKEFILE_PATH))
-endif
+SPYNNAKER_DIR := $(abspath $(dir $(MAKEFILE_PATH))/../../)/
 
-# APP name for a and dict files
-ifndef APP
-    $(error APP is not set.  Please define APP)
-endif
+BUILD_DIR := $(SPYNNAKER_DIR)neural_modelling/builds/$(APP)/
+APP_OUTPUT_DIR := $(SPYNNAKER_DIR)spynnaker/pyNN/model_binaries/
+MODIFIED_DIR := $(SPYNNAKER_DIR)neural_modelling/modified_src/
 
-# Define the directories
-SRC_DIR := $(NEURAL_MODELLING_DIRS)/src/
-SOURCE_DIRS += $(SRC_DIR)
-MODIFIED_DIR := $(NEURAL_MODELLING_DIRS)/modified_src/
-BUILD_DIR := $(NEURAL_MODELLING_DIRS)/builds/$(APP)/
-APP_OUTPUT_DIR :=  $(abspath $(dir $(MAKEFILE_PATH))../../spynnaker/pyNN/model_binaries)/
-
-include $(SPINN_DIRS)/make/local.mk
-
+include $(SPYNNAKER_DIR)neural_modelling/make/neural_support.mk
