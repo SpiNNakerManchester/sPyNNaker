6ffc802389fdf8b0acb26411428b759e5cdef09c
christian-B <christian.brenninkmeijer@manchester.ac.uk>
2026-07-20 15:31:04 +0100

    venv for mypy

--- mypyd.bash
+++ mypyd.bash
@@ -14,7 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 # This bash assumes that other repositories are installed in parallel
 
 if [ "$#" -eq  "0" ]
