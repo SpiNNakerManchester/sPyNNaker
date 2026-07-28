a3b642a937189e2e3efb91ed3b395f6c87e741e1
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2026-07-28 14:00:39 +0100

    Add testing of the sample profiling

---
+++ spynnaker_integration_tests/test_sample_profile/test_sample_profile.py
@@ -0,0 +1,65 @@
+# Copyright (c) 2017 The University of Manchester
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+import os
+import numpy
+import pyNN.spiNNaker as p
+
+from spinn_utilities.config_holder import get_report_path
+
+from spinnaker_testbase import BaseTestCase
+
+from spynnaker_integration_tests.scripts import SynfireRunner
+
+n_neurons = 200  # number of neurons in each population
+neurons_per_core = int(n_neurons / 2)
+run_times = [10, 20, 30]
+# parameters for population 1 first run
+input_class = p.SpikeSourcePoisson
+start_time = 0
+duration = 5000.0
+rate = 2.0
+synfire_run = SynfireRunner()
+
+
+class TestSampleProfiling(BaseTestCase):
+
+    def assert_report(self) -> None:
+        path = get_report_path("path_sample_profile_report")
+        if not os.path.exists(path):
+            raise AssertionError("Unable to find report")
+
+        with open(path) as report:
+            report_contents = json.load(report)
+            assert isinstance(report_contents["simulation_timestep_us"], int)
+            assert isinstance(report_contents["hardware_timestep_us"], int)
+
+    def do_run(self) -> None:
+        synfire_run.do_run(n_neurons, neurons_per_core=neurons_per_core,
+                           run_times=run_times, input_class=input_class,
+                           start_time=start_time, duration=duration, rate=rate,
+                           seed=12345)
+        spikes = synfire_run.get_output_pop_spikes_numpy()
+        self.assertIsNotNone(spikes, "must have some spikes")
+        # Check spikes increase in second half by at least a factor of ten
+        hist = numpy.histogram(spikes[:, 1], bins=[0, 5000, 10000])
+        self.assertIsNotNone(hist, "must have a histogram")
+
+        # Did we build the report file like we asked for in config file?
+        self.assert_report()
+
+
+    def test_sample_profiling(self) -> None:
+        self.runsafe(self.do_run)
