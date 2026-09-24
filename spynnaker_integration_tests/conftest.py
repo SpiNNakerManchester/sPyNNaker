3ccdf4ba64c23094ea87d41d5881b721fa798a08
Christian-B <christian.brenninkmeijer@manchester.ac.uk>
2026-09-24 12:04:42 +0100

    raise better Exception

--- spynnaker_integration_tests/conftest.py
+++ spynnaker_integration_tests/conftest.py
@@ -34,4 +34,4 @@ def check_end_is_called(request: SubRequest) -> Iterator[None]:
     except Exception:  # NOQA
         # Ignore anything that comes from this
         pass
-    raise Exception(f"Simulation has not been stopped in {request.function}!")
+    raise AssertionError(f"Simulation has not been stopped in {request.function}!")
